e15ff8f1f73df8372a921d10ee509823970a6b95
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2007-08-13 06:55:36 +0000

    Bug fix with respect to wchar_t

--- quex/input/setup.py
+++ quex/input/setup.py
@@ -16,7 +16,7 @@ FLAG = -2
 SETUP_INFO = {         
     # [Name in Setup]                 [ Flags ]                              [Default / Type]
     "begin_of_stream_code":           [["--begin-of-stream"],                0x19],
-    "bytes_per_ucs_code_point":       [["--bytes-per-ucs-code-point", "-b"], 1],
+    "bytes_per_ucs_code_point":       [["--bytes-per-ucs-code-point", "-b"], "1"],
     "dos_carriage_return_newline_f":  [["--DOS"],                            FLAG],
     "end_of_stream_code":             [["--end-of-stream"],                  0x1A],
     "enable_iconv_f":                 [["--iconv"],                          FLAG],
@@ -49,11 +49,11 @@ DEPRECATED = {
   "input_pattern_file": 
      ("Write a 'pattern { ... }' section inside the mode files instead.\n" + \
       "Syntax of the 'pattern { ... }' section and the previous file syntax\n" + \
-      "are backward compatible.", "0.9.x"),	       
+      "are backward compatible.", "0.9.x"),        
   "input_token_id_db":
      ("Write a 'token { ... }' section inside the mode files instead.\n" + \
       "Syntax of the 'token { ... }' section and the previous file syntax\n" + \
-      "are backward compatible.", "0.9.x"),	       
+      "are backward compatible.", "0.9.x"),        
   "yywrap_is_ok_f":
      ("Since the mentioned version, the flex core engine is no longer supported. The\n" + \
       "flag makes only sense for flex core engines.", "0.13.1"),
@@ -77,33 +77,33 @@ def do(argv):
     if command_line.search("--version", "-v"):
         print "Quex - A Mode Oriented Lexical Analyser"
         print "Version " + QUEX_VERSION
-	print "(C) 2006, 2007 Frank-Rene Schaefer"
+        print "(C) 2006, 2007 Frank-Rene Schaefer"
         sys.exit(0)
 
     if command_line.search("--help", "-h"):
         print "Quex - A Mode Oriented Lexical Analyser"
         print "Please, consult the quex documentation for further help, or"
-	print "(C) 2006, 2007 Frank-Rene Schaefer"
+        print "(C) 2006, 2007 Frank-Rene Schaefer"
         print "visit http://quex.sourceforge.net."
         sys.exit(0)
 
 
     for name, info in DEPRECATED.items():
-	command_line_options = SETUP_INFO[name][0]
-	comment                   = info[0]
-	depreciated_since_version = info[1]
-	for option in command_line_options:
-	    if command_line.search(option):
-		error_msg("Command line option '%s' is ignored.\n" % option + \
-			  "Last version of Quex supporting this option is version %s. Please, visit\n" % version + \
-			  "http://quex.sourceforge.net for download---Or use a more advanced approach.\n" + \
-			  comment)
-			  
+        command_line_options = SETUP_INFO[name][0]
+        comment                   = info[0]
+        depreciated_since_version = info[1]
+        for option in command_line_options:
+            if command_line.search(option):
+                error_msg("Command line option '%s' is ignored.\n" % option + \
+                          "Last version of Quex supporting this option is version %s. Please, visit\n" % version + \
+                          "http://quex.sourceforge.net for download---Or use a more advanced approach.\n" + \
+                          comment)
+                          
 
     for variable_name, info in SETUP_INFO.items():
-	if info[1]   == LIST:
+        if info[1]   == LIST:
             setup.__dict__[variable_name] = command_line.nominus_followers(info[0])        
-	elif info[1] == FLAG:
+        elif info[1] == FLAG:
             setup.__dict__[variable_name] = command_line.search(info[0])        
         else:
             setup.__dict__[variable_name] = command_line.follow(info[1], info[0])
@@ -115,7 +115,7 @@ def do(argv):
     if setup.input_derived_class_name != "" and \
        setup.input_derived_class_file == "":
             print "error: specified derived class '%s' on command line, but it was not" % \
-		  setup.input_derived_class_name
+                  setup.input_derived_class_name
             print "error: specified which file contains the definition of it."
             print "error: use command line option '--derived-class-file'."
             sys.exit(-1)
@@ -126,37 +126,41 @@ def do(argv):
     setup.output_code_file     = setup.output_engine_name + ".cpp"
 
     def __get_integer(code, option_name):
-	try:
-	    if   type(code) == int: return code
-	    elif len(code) > 2:
-		if   code[:2] == "0x": return int(code, 16)
-		elif code[:2] == "0o": return int(code, 8)
-		else:                  return int(code)
-	except:
-	    pass
-	print "error: cannot convert %s into an intege for '%s'" % (code, option_name)
-	sys.exit(-1)
+        try:
+            if   type(code) == int: return code
+            elif len(code) > 2:
+                if   code[:2] == "0x": return int(code, 16)
+                elif code[:2] == "0o": return int(code, 8)
+                else:                  return int(code)
+        except:
+            pass
+        print "error: cannot convert %s into an intege for '%s'" % (code, option_name)
+        sys.exit(-1)
 
     setup.begin_of_stream_code = __get_integer(setup.begin_of_stream_code, "--begin-of-stream")
     setup.end_of_stream_code   = __get_integer(setup.end_of_stream_code, "--end-of-stream")
 
     if setup.begin_of_stream_code == 0 or setup.end_of_stream_code == 0:
-	print "error: code for begin/end of stream cannot be zero!"
-	sys.exit(-1)
+        print "error: code for begin/end of stream cannot be zero!"
+        sys.exit(-1)
 
     # check validity
-    if setup.bytes_per_ucs_code_point not in ["whar_t", 1, 2, 4]:
-	print "error: choice for --bytes-per-ucs-code-point: %i" % setup.bytes_per_ucs_code_point
-	print "error: quex only supports 1, 2, or 4 bytes per character in internal engine"
-	sys.exit(-1)
+    if setup.bytes_per_ucs_code_point != "wchar_t":
+        if setup.bytes_per_ucs_code_point not in [1, 2, 4]:
+            print "error: choice for --bytes-per-ucs-code-point: %i" % setup.bytes_per_ucs_code_point
+            print "error: quex only supports 1, 2, or 4 bytes per character in internal engine"
+            sys.exit(-1)
+        else:
+            setup.bytes_per_ucs_code_point = int(setup.bytes_per_ucs_code_point)
+
 
     if setup.byte_order == "<system>": 
-	setup.byte_order = sys.byteorder 
+        setup.byte_order = sys.byteorder 
     elif setup.byte_order not in ["<system>", "little", "big"]:
-	print "error: Byte order (option --endian) must be 'little' or 'big'."
-	print "error: Note, that this option is only interesting for cross plattform development."
-	print "error: By default, quex automatically chooses the endian type of your system."
-	sys.exit(-1)
+        print "error: Byte order (option --endian) must be 'little' or 'big'."
+        print "error: Note, that this option is only interesting for cross plattform development."
+        print "error: By default, quex automatically chooses the endian type of your system."
+        sys.exit(-1)
         
     # (*) Check for 'Straying' Options ___________________________________________________
     options = []
