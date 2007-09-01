801ebfb07ebcf3ae5423cf69352a1bb7e07e0a98
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2007-09-01 17:17:10 +0000

    Moved to trunk

--- demo/TEST/000.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-if [[ $1 == "--hwut-info" ]]; then
-    echo "demo/000: Single Mode Example"
-    echo "CHOICES:  NDEBUG, DEBUG"
-    exit
-fi
-source core.sh 000 $1
--- quex/core_engine/DOC/README.txt
+++
@@ -1,1 +0,0 @@
-The content of this directory has been moved to quex/DOC/inside-quex
