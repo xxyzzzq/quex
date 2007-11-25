f29309b94f5bd2f8e663998c85cb01193068106d
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2007-11-25 09:13:55 +0000

    steps towards solving the pseudo-ambiguous post-conditions.

---
+++ trunk/quex/core_engine/generator/input_position_backward_detector.py
@@ -0,0 +1,16 @@
+# PURPOSE: Where there is a pseudo ambiguous post-condition, there
+#          cannot be made an immediate decision about the input position after
+#          an acceptance state is reached. Instead, it is necessary to 
+#          go backwards and detect the start of the post-condition 
+#          a-posteriori. This function produces code of the backward
+#          detector inside a function.
+#
+# (C) 2007 Frank-Rene Schaefer
+#
+################################################################################
+
+
+def do(sm, LanguageDB):
+    pass
+
+
