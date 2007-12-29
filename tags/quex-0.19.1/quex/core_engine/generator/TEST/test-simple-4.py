833e73bed24908184b43e0f5a70a0665a9297c49
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2007-12-29 22:39:45 +0000

    Release 0.19.1

---
+++ tags/quex-0.19.1/quex/core_engine/generator/TEST/test-simple-4.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+import sys
+import generator_test
+from generator_test import action
+import quex.core_engine.regular_expression.core as regex
+
+if "--hwut-info" in sys.argv:
+    print "Simple: The 'Nothing is Fine' / EOF Problem"
+    sys.exit(0)
+
+pattern_action_pair_list = [
+    # pre-conditioned expressions need to preceed same (non-preoconditioned) expressions,
+    # otherwise, the un-conditional expressions gain precedence and the un-conditional
+    # pattern is never matched.
+    ('x*',      "X*"),
+]
+test_str = "aber-hallo"
+
+try:
+#    generator_test.do(pattern_action_pair_list, test_str, {}, choice, QuexBufferSize=5)    
+    pass
+except str, x:
+    print dir(x)
+
+pattern_action_pair_list = [
+    # pre-conditioned expressions need to preceed same (non-preoconditioned) expressions,
+    # otherwise, the un-conditional expressions gain precedence and the un-conditional
+    # pattern is never matched.
+    ('<<EOF>>',  "EOF STOP"),
+    ('x*',       "X*"),
+]
+test_str = "a"
+
+generator_test.do(pattern_action_pair_list, test_str, {}, QuexBufferSize=5)    
