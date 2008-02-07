2a9b1e2759905d2c4c969689f8e8536a726aae8e
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-07 23:13:51 +0000

    administration

--- trunk/TEST/adapt-lexer.sh
+++
@@ -1,4 +0,0 @@
-cat lexer.cpp | \
-awk -v input_file=$1input.txt -v header_file=$1header \
-'{ txt = $0; sub("INPUT-FILE", input_file, txt); sub("HEADER", header_file, txt); print txt; }' > $1lexer.cpp
-
