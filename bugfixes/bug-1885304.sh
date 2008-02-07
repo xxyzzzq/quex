13b1ce506a2b0fdc85243c4df131fdbb5d457001
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-07 23:24:51 +0000

    admin

---
+++ bugfixes/bug-1885304.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+if [[ $1 == "--hwut-info" ]]; then
+    echo "1885304 by 'sphericalcow': Problems with files with DOS CR/LF line endings"
+    exit
+fi
+
+tmp=`pwd`
+cd 1885304/ 
+quex -i dos_lf_2.qx --engine Simple
+cat Simple Simple-core-engine.cpp  Simple.cpp  Simple-token_ids
+cd $tmp
