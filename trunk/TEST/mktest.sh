21b396332f0637dad86ab02ece35ddb454963084
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-09 16:17:07 +0000

    bugfixes

--- trunk/TEST/mktest.sh
+++ trunk/TEST/mktest.sh
@@ -1,5 +1,5 @@
 mkdir $1
-cp bug-1887163.sh bug-$1.sh
+cat bug-template.sh | sed -e "s/BUG\_ID/$1/g" | sed -e "s/SUBMITTER/$2/g" | sed -s "s/TITLE/$3/g" > bug-$1.sh
 chmod u+x bug-$1.sh
 svn add bug-$1.sh
 svn add $1
