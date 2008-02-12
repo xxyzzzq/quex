14c6cbed2934ab4f3104e4ff4f9a299143ab81f3
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-12 21:17:42 +0000

    Release 0.20.3

---
+++ tags/quex-0.20.3/quex/code_base/buffer/TEST/misc-core.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <sstream>
+
+#include <test-core.h>
+
+using namespace quex;
+using namespace std;
+
+template <class Buffer> inline void
+test_this(Buffer& x)
+{
+    
+    cout << "<" << read_to_end_of_file(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "<" << read_to_begin_of_file(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "<" << read_forward_until_end_of_buffer(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "\n(*) load_forward:\n";
+    x.load_forward();
+    x.x_show_content();
+
+    cout << "<" << read_forward_until_end_of_buffer(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "\n(*) load_forward:\n";
+    x.load_forward();
+    x.x_show_content();
+
+    cout << "<" << read_to_end_of_file(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "<" << read_backward_until_begin_of_buffer(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "\n(*) load_backward:\n";
+    x.load_backward();
+    x.x_show_content();
+
+    cout << "<" << read_backward_until_begin_of_buffer(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "\n(*) load_backward:\n";
+    x.load_backward();
+    x.x_show_content();
+
+    cout << "<" << read_to_begin_of_file(x) << ">" << endl;
+    x.x_show_content();
+
+    cout << "<" << read_to_end_of_file(x) << ">" << endl;
+    x.x_show_content();
+}
+
+void
+test_istream(int ContentSz, int BackupSz)
+{
+    istringstream ifs("Im Wald steht ein Baum.");
+
+    // '+2' because of two positions for begin and end of buffer
+    quex::basic_buffer<input_strategy<std::istream*, char> >  x(&ifs, ContentSz + 2, BackupSz); 
+
+    cout << "-(total size = " << ContentSz << ", border = " << BackupSz << ")-istream-----------------------------------\n";
+    test_this(x);
+}
+
+void
+test_stdio(int ContentSz, int BackupSz)
+{
+    FILE*       fh = fopen("test.txt", "w");
+    fprintf(fh, "Im Wald steht ein Baum.");
+    fclose(fh);
+    fh = fopen("test.txt", "r");
+    setvbuf(fh, 0x0, _IONBF, 0);   // disable buffering
+    assert(fh != 0x0);
+    //
+    // '+2' because of two positions for begin and end of buffer
+    quex::basic_buffer<input_strategy<FILE*, char> >  x(fh, ContentSz + 2, BackupSz); 
+
+    cout << "-(total size = " << ContentSz << ", border = " << BackupSz << ")-stdio-------------------------------------\n";
+
+    test_this(x);
+}
+
+
