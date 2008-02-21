3f3a294215417ba05c4f2611813fc06c20f3ddf4
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-21 06:29:16 +0000

    bugfix

--- trunk/TEST/bug-1898125.sh
+++ trunk/TEST/bug-1898125.sh
@@ -7,7 +7,13 @@ fi
 
 tmp=`pwd`
 cd $bug/ 
-quex -i error.qx -o Simple
+quex -i error.qx -o Simple --token-offset 0x100
+echo
+quex -i error.qx -o Simple --token-offset 0x10f
+echo
+quex -i error.qx -o Simple --token-offset foo
+echo
+
 
 # cleansening
 rm -f Simple Simple-core-engine.cpp Simple.cpp Simple-token-ids Simplism
