db1b2b6df55c5b46bbdaf7a5ba93a7c864e811bb
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-23 08:11:27 +0000

    towards graphical display of lexer

--- trunk/quex/output/graphviz/interface.py
+++ trunk/quex/output/graphviz/interface.py
@@ -1,48 +1,69 @@
 import os
 import subprocess
+import tempfile
 
-from frs_py.file_in   import error_msg
-from quex.DEFINITIONS import QUEX_INSTALLATION_DIR
+from quex.frs_py.file_in             import error_msg
+from quex.DEFINITIONS                import QUEX_INSTALLATION_DIR
+from quex.core_engine.generator.core import GeneratorBase
 
 
-def do(StateMachine, OutputFormat):
+def do(PatternActionPairList, StateMachineName, GraphicFormat):
     """Prepare output in the 'dot' language, that graphviz uses."""
     __assert_graphviz_installed()
 
-    supported_format_list = __get_supported_graphic_formats()
+    supported_format_list = get_supported_graphic_formats()
     if OutputFormat not in supported_format_list:
-        error_msg("Graphic format '%s' not supported. Please, use one of: " + \
-                  repr(supported_formats)[1:-1])
+        error_msg("Graphic format '%s' not supported.\n" + \
+                  get_supported_graphic_format_description())
 
-    dot_code
+    generator = GeneratorBase(PatternActionPairList, StateMachineName)
+
+    __call_dot(TestCode, "fig", test_filename + ".fig")
     
-def report_supported_graphic_formats():
-    __assert_graphviz_installed()
+def get_supported_graphic_formats():
+    reply_str = __call_dot(TestCode, "xxxx", "", GetStdErrF=True)
 
+    list_start_i = reply_str.rfind(":")
+    if list_start_i == -1 or list_start_i == len(reply_str)-1:
+        error_msg("Graphviz was asked to report supported graphic formats, but\n" + \
+                  "reply was incomprehensible.")
 
-def __get_supported_graphic_formats():
-    return ["fig"]
+    return reply_str[list_start_i+1:].split()
 
+def get_supported_graphic_format_description():
+    txt = "SUPPORTED GRAPHIC FORMATS:\n"
+    i = -1
+    for format in get_supported_graphic_formats():
+        i += 1
+        txt += format + ", "
+        if i > 10: txt += "\n"; i = -1
 
-transition_template = '$1$ -> $2$ [ label = "$3$"]'
-def __call_dot(Code, OutputFormat, OutputFile):
+    
+    return txt[:-2] + "."
+
+def __call_dot(Code, OutputFormat, OutputFile, GetStdErrF=False):
     # (*) initiate call to the graphviz utility ('dot') and use a sample file
     #     for reference.
-    try:
-        fd, input_file_name = tempfile.mkstemp(".quex.dot", "TMP")
-        fh = os.fdopen(fd, "w")
-        fh.write(Code)
-        fh.close()
+    # try:
+    fd, input_file_name = tempfile.mkstemp(".quex.dot", "TMP")
+    fh = os.fdopen(fd, "w")
+    fh.write(Code)
+    fh.close()
 
-        fd, error_report = tempfile.mkstemp(".quex.err", "TMP")
-        fh_err = os.fopen(fd, "e")
-        fh_out = open(os.devnull, "w")
+    fd_err, error_file = tempfile.mkstemp(".quex.err", "TMP")
+    fd_out, out_file   = tempfile.mkstemp(".quex.out", "TMP")
+    fh_err = os.fdopen(fd_err, "w")
+    fh_out = os.fdopen(fd_out, "w")
 
-    except:
-        error_msg("File access error while preparing graphviz code.")
+    # except:
+    #    error_msg("File access error while preparing graphviz code.")
 
-    try:    subprocess.call(["dot", input_file_name, "-T%s" % OutputFormat, "-o%s" % OutputFile], 
-                            stdout=fh_out, stderr=fh_err)
+    try:    
+        if OutputFile != "": output_str = "-o%s" % OutputFile 
+        else:                output_str = ""
+
+        subprocess.call(["dot", input_file_name, "-T%s" % OutputFormat, output_str],
+                        stdout=fh_out, stderr=fh_err)
     except: 
         error_msg("Graphviz seems not to be installed on this system. Please, visit www.graphviz.org\n",
                   "and download the package. This package is necessary for for plotting\n"
@@ -51,6 +72,27 @@ def __call_dot(Code, OutputFormat, OutputFile):
     fh_out.close()
     fh_err.close()
 
+    result = None
+    if GetStdErrF:
+        try:    result = open(error_file).read()
+        except: error_msg("Output of graphviz is unaccessible.")
+
+    os.remove(error_file)
+    os.remove(out_file)
+
+    return result
+
+TestCode = \
+"""
+digraph Kraichgau {
+	rankdir=LR;
+	size="10,5"
+	node [shape = doublecircle]; Pforzheim;
+	node [shape = circle];
+	Ispringen -> Pforzheim [ label = "5 km" ];
+	Ispringen -> Ersingen [ label = "1 km" ];
+}
+"""
 
 def __assert_graphviz_installed():
     """This function checks whether the graphviz utility has been installed."""
@@ -58,18 +100,7 @@ def __assert_graphviz_installed():
     # (*) initiate call to the graphviz utility ('dot') and use a sample file
     #     for reference.
     test_filename = QUEX_INSTALLATION_DIR + "/output/graphviz/files/test.dot"
-    fh_out        = open(test_filename + ".fig", "w")
-    fh_err        = open(test_filename + ".err", "w")
-
-    try:    
-        subprocess.call(["dot", test_filename, "-Tfig"], stdout=fh_out, stderr=fh_err)
-    except: 
-        error_msg("Graphviz is not installed on this system. Please, visit www.graphviz.org\n",
-                  "and download the package. This package is necessary for for plotting\n"
-                  "transition graphs.")
-
-    fh_out.close()
-    fh_err.close()
+    __call_dot(TestCode, "fig", test_filename + ".fig")
 
     try:    fh = open(test_filename + ".fig")
     except: return False
