6dfaf01477bdee8eb30a2c7d8d85afcfcbd1b55c
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-25 22:21:25 +0000

    unit test

--- demo/TEST/other-compilers-intel.sh
+++ demo/TEST/other-compilers-intel.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 if [[ $1 == "--hwut-info" ]]; then
     echo "Other Compilers: Intel's C++"
-    echo "CHOICES:  000, 001, 002, 003, 004, 005, 006"
+    echo "CHOICES:  000, 001, 002, 003, 005, 006"
     exit
 fi
 source /opt/intel/cc/10.1.012/bin/iccvars.sh
