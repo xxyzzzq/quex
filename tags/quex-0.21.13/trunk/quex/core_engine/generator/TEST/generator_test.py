8173425168bafe850db36c8684ccddcfce21b693
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-27 06:28:44 +0000

    Release 0.21.13

---
+++ tags/quex-0.21.13/trunk/quex/core_engine/generator/TEST/generator_test.py
@@ -0,0 +1,204 @@
+#! /usr/bin/env python
+import sys
+import os
+from StringIO import StringIO
+from tempfile import mkstemp
+sys.path.insert(0, os.environ["QUEX_PATH"])
+#
+from quex.frs_py.string_handling import blue_print
+from quex.exception              import RegularExpressionException
+from quex.lexer_mode             import PatternShorthand
+#
+from   quex.core_engine.generator.action_info   import ActionInfo
+import quex.core_engine.generator.core          as generator
+import quex.core_engine.regular_expression.core as regex
+
+def action(PatternName): 
+    # txt = 'fprintf(stderr, "%19s  \'%%s\'\\n", Lexeme);\n' % PatternName # DEBUG
+    txt = 'printf("%19s  \'%%s\'\\n", Lexeme);\n' % PatternName
+
+    if   "->1" in PatternName: txt += "me->__current_mode_analyser_function_p = analyser_do;\n"
+    elif "->2" in PatternName: txt += "me->__current_mode_analyser_function_p = analyser_do_2;\n"
+
+    if "CONTINUE" in PatternName: txt += ""
+    elif "STOP" in PatternName:   txt += "return 0;"
+    else:                         txt += "return 1;"
+
+    return txt
+    
+
+test_program_common_declarations = """
+struct QUEX_CORE_ANALYSER_STRUCT;
+static int    analyser_do(QUEX_CORE_ANALYSER_STRUCT* me);
+static int    analyser_do_2(QUEX_CORE_ANALYSER_STRUCT* me);
+"""
+
+test_program_common = """
+int main(int, char**)
+{
+    using namespace std;
+
+    analyser   lexer_state;
+    //
+    int    success_f = 0;
+    //
+    %%BUFFER_SPECIFIC_SETUP%%
+    //
+    printf("(*) test string: \\n'%%TEST_STRING%%'\\n");
+    printf("(*) result:\\n");
+    do {
+        success_f = lexer_state.__current_mode_analyser_function_p(&lexer_state);
+    } while ( success_f );      
+    printf("  ''\\n");
+}\n"""
+
+quex_buffer_based_test_program = """
+    istringstream   istr("%%TEST_STRING%%");
+    quex::buffer    buf(&istr, $$BUFFER_SIZE$$, $$BUFFER_FALLBACK_N$$);
+
+    analyser_init(&lexer_state, 0, &buf, analyser_do);
+"""
+
+plain_memory_based_test_program = """
+    char   tmp[] = "\\0%%TEST_STRING%%";  // introduce first '0' for safe backward lexing
+
+    analyser_init(&lexer_state, &(tmp[1]), analyser_do);
+"""
+
+def create_main_function(BufferType, TestStr, QuexBufferSize, QuexBufferFallbackN):
+    global plain_memory_based_test_program
+    global quex_buffer_based_test_program
+    global test_program_common
+
+    txt = test_program_common
+    
+    if BufferType=="QuexBuffer": 
+        if QuexBufferFallbackN == -1: QuexBufferFallbackN = QuexBufferSize - 3
+        txt = "#include <sstream>\n" + txt
+
+        buffer_specific_str = quex_buffer_based_test_program.replace("$$BUFFER_SIZE$$", repr(QuexBufferSize))
+        buffer_specific_str = buffer_specific_str.replace("$$BUFFER_FALLBACK_N$$", repr(QuexBufferFallbackN))
+        core_engine_definition_file = "quex/code_base/core_engine/definitions-quex-buffer.h"
+    else:                        
+        buffer_specific_str = plain_memory_based_test_program
+        core_engine_definition_file = "quex/code_base/core_engine/definitions-plain-memory.h"
+
+    test_str = TestStr.replace("\"", "\\\"")
+    test_str = test_str.replace("\n", "\\n\"\n\"")
+
+    txt = blue_print(txt,
+                     [["%%TEST_STRING%%",           test_str],
+                      ["%%BUFFER_SPECIFIC_SETUP%%", buffer_specific_str]])
+
+    return txt, core_engine_definition_file
+
+
+def create_state_machine_function(PatternActionPairList, PatternDictionary, 
+                                  BeginOfFile_Code, EndOfFile_Code, 
+                                  core_engine_definition_file, SecondModeF=False):
+    default_action = "return 0;"
+
+    # -- produce some visible output about the setup
+    print "(*) Lexical Analyser Patterns:"
+    for pair in PatternActionPairList:
+        print "%20s --> %s" % (pair[0], pair[1])
+    # -- create default action that prints the name and the content of the token
+    try:
+        PatternActionPairList = map(lambda x: 
+                                    ActionInfo(regex.do(x[0], PatternDictionary, 
+                                               BeginOfFile_Code, EndOfFile_Code), 
+                                               action(x[1])),
+                                    PatternActionPairList)
+    except RegularExpressionException, x:
+        print "Regular expression parsing:\n" + x.message
+        sys.exit(0)
+
+    print "## (1) code generation"    
+    txt  = "#include<stdio.h>\n"
+    txt += "#define  __QUEX_OPTION_UNIT_TEST\n"
+
+    txt += generator.do(PatternActionPairList, default_action, PrintStateMachineF=True,
+                        AnalyserStateClassName         = "analyser",
+                        StandAloneAnalyserF            = True, 
+                        QuexEngineHeaderDefinitionFile = core_engine_definition_file,
+                        EndOfFile_Code                 = EndOfFile_Code)   
+
+    if SecondModeF: txt = txt.replace("analyser_do(", "analyser_do_2(")
+
+    return txt
+
+
+def do(PatternActionPairList, TestStr, PatternDictionary={}, BufferType="PlainMemory", QuexBufferSize=15,
+       SecondPatternActionPairList=[], QuexBufferFallbackN=-1, ShowBufferLoadsF=False,
+       NDEBUG_str=""):    
+
+    try:
+        adapted_dict = {}
+        for key, regular_expression in PatternDictionary.items():
+            string_stream = StringIO(regular_expression)
+            state_machine = regex.do(string_stream, adapted_dict)
+            # It is ESSENTIAL that the state machines of defined patterns do not 
+            # have origins! Actually, there are not more than patterns waiting
+            # to be applied in regular expressions. The regular expressions 
+            # can later be origins.
+            state_machine.delete_state_origins()
+
+            adapted_dict[key] = PatternShorthand(key, state_machine)
+    except RegularExpressionException, x:
+        print "Dictionary Creation:\n" + repr(x)
+    
+    test_program, core_engine_definition_file = create_main_function(BufferType, TestStr,
+                                                                     QuexBufferSize, QuexBufferFallbackN)
+
+    if BufferType=="QuexBuffer": BeginOfFile_Code = 0x19; EndOfFile_Code = 0x1A
+    else:                        BeginOfFile_Code = 0;    EndOfFile_Code = 0
+
+    state_machine_code = create_state_machine_function(PatternActionPairList, 
+                                                       adapted_dict, 
+                                                       BeginOfFile_Code, EndOfFile_Code, 
+                                                       core_engine_definition_file)
+
+    if SecondPatternActionPairList != []:
+        state_machine_code += create_state_machine_function(SecondPatternActionPairList, 
+                                                            PatternDictionary, 
+                                                            BeginOfFile_Code, EndOfFile_Code, 
+                                                            core_engine_definition_file, 
+                                                            SecondModeF=True)
+
+
+    if ShowBufferLoadsF:
+        state_machine_code = "#define __QUEX_OPTION_UNIT_TEST_QUEX_BUFFER_LOADS\n" + \
+                             "#define __QUEX_OPTION_UNIT_TEST\n" + \
+                             "#define __QUEX_OPTION_UNIT_TEST_QUEX_BUFFER\n" + \
+                             state_machine_code
+
+    fd, filename_tmp = mkstemp(".cpp", "tmp-", dir=os.getcwd())
+    os.write(fd, test_program_common_declarations)
+    os.write(fd, state_machine_code)
+    os.write(fd, test_program)    
+    os.close(fd)    
+
+    os.system("mv -f %s tmp.cpp" % filename_tmp); filename_tmp = "./tmp.cpp" # DEBUG
+
+    print "## (2) compiling generated engine code and test"    
+    compile_str = "g++ %s %s " % (NDEBUG_str, filename_tmp) + \
+                  "-I./. -I$QUEX_PATH " + \
+                  "-o %s.exe " % filename_tmp + \
+                  "-D__QUEX_OPTION_UNIT_TEST_ISOLATED_CODE_GENERATION " + \
+                  "-ggdb"    
+                  # "-D__QUEX_OPTION_DEBUG_STATE_TRANSITION_REPORTS" + \
+                  # "-D__QUEX_OPTION_UNIT_TEST_QUEX_BUFFER_LOADS " + \
+
+    # print compile_str # DEBUG
+    os.system(compile_str)
+
+    print "## (3) running the test"
+    try:
+        os.system("%s.exe" % filename_tmp)
+        os.remove("%s.exe" % filename_tmp)
+    except:
+        print "<<compilation failed>>"
+    print "## (4) cleaning up"
+    # os.remove(filename_tmp)
+
+
---
+++ tags/quex-0.21.13/trunk/quex/core_engine/regular_expression/traditional_character_set.py
@@ -0,0 +1,123 @@
+import sys
+import StringIO
+import quex.core_engine.utf8 as utf8
+import quex.core_engine.regular_expression.snap_backslashed_character as snap_backslashed_character
+from quex.core_engine.interval_handling  import *
+from quex.exception                      import RegularExpressionException
+
+
+
+class Tracker:
+    def __init__(self):
+        self.direct_match_set = NumberSet()
+        self.inverse_match_set  = NumberSet()
+        self.negation_f = False
+        # NOTE: each incoming letter is written into 'last_letter' because 
+        #       a minus sign might occur to span a range from this character
+        #       to the following character. (see: consider_letter(...)).
+        self.last_letter = -1 
+ 
+    def __consider_interval(self, Begin, End):
+        """... the core of the matter"""
+        if End == None: End = Begin + 1
+        #
+        if Begin > End:
+            # In case that the 'end' is lesser than the begin, on canot simply switch
+            # end and begin. End points to the first element beyond the valid interval.
+            begin = End - 1
+            end = Begin
+        else:
+            begin = Begin
+            end   = End
+
+        interval = Interval(begin, end)
+        if self.negation_f == False:  self.direct_match_set.add_interval(interval)
+        else:                         self.inverse_match_set.add_interval(interval)
+
+    def consider_interval(self, x0, x1 = None):
+        # flush last letter if it exists
+        self.consider_letter(-1)
+        # flush interval
+        self.__consider_interval(x0, x1)
+
+    def consider_letter(self, x):
+        # if previous character was a letter, than add it to the set of trigger letters
+        if self.last_letter != -1: self.__consider_interval(self.last_letter, self.last_letter+1)
+        # store currently incoming letter code in 'self.last_letter' (maybee, its used for a 
+        # character range).
+        self.last_letter = x
+ 
+    def negate(self):
+         # flush last letter if it exists
+        self.consider_letter(-1)
+
+        if self.negation_f: self.negation_f = False
+        else:               self.negation_f = True 
+
+def do_string(UTF8_String):
+    return do(StringIO.StringIO(UTF8_String))
+
+def do(sh):
+    """Transforms an expression of the form [a-z0-9A-Z] into a NumberSet of
+       code points that corresponds to the characters and character ranges mentioned.
+    """
+    assert     sh.__class__.__name__ == "StringIO" \
+            or sh.__class__.__name__ == "file"
+
+    tracker   = Tracker()
+    char_code = None
+    while char_code != 0xFF:
+        char_code = utf8.__read_one_utf8_code_from_stream(sh)
+
+        if char_code == ord(']'): break
+
+        if char_code == ord('-'):
+            #_________________________________________________________________________
+            # character range:  'character0' '-' 'character1'
+            #
+            if tracker.last_letter == -1:
+                raise RegularExpressionException("Character range: '-' requires a preceding character, e.g. 'a-z'")
+
+            char_code = utf8.__read_one_utf8_code_from_stream(sh)
+            if char_code == 0xFF or char_code == ord(']'):
+                raise RegularExpressionException("Character range: '-' requires a character following '-'.")
+
+            # value denotes 'end', i.e first character outside the interval
+            if char_code == ord("\\"): value = snap_backslashed_character.do(sh) + 1 
+            else:                      value = char_code + 1
+
+            tracker.consider_interval(tracker.last_letter, value)
+
+        elif char_code == ord("\\"):
+            #_________________________________________________________________________
+            # Escape Character / Sophisticated Code Point Definition
+            #
+            value = snap_backslashed_character.do(sh)
+            tracker.consider_letter(value)
+
+        elif char_code == ord("^"):
+            #_________________________________________________________________________
+            # Negation
+            #
+            # any characterange or interval that is specified is now
+            # negated, i.e. ^0-5 means: 'anything but 0-5'
+            tracker.negate()
+            tracker.consider_letter(-1)  # '-1' => there is no preceeding character
+
+        else:
+            #_________________________________________________________________________
+            # Normal character
+            #
+            tracker.consider_letter(char_code)
+    
+    # flush last letter if it exists
+    tracker.consider_letter(-1)
+
+    # inverse_match_set: if a 'event' appears that is != to any of them, then trigger!
+    # direct_match_set:  if a 'event' appears that is == to any of them, then trigger!
+    # the whole set:     combine inverse and direct set with 'or', i.e. union
+    result = tracker.direct_match_set
+    result = result.intersection(tracker.inverse_match_set.inverse())
+    return result
+
+
