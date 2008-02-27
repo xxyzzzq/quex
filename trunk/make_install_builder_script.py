39be7fd1be4893c76ff9a925a4ab75a779af9532
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-02-27 07:14:55 +0000

    install scripts

--- trunk/make_install_builder_script.py
+++ trunk/make_install_builder_script.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python
 import sys
+import os
+sys.path.insert(0, os.environ["QUEX_PATH"])
+from   quex.frs_py.string_handling import blue_print
+
+DistributionFileDB = sys.argv[1]
+Version            = sys.argv[2]
+
+
+template_str = open(os.environ["QUEX_PATH"] + "/adm/install-builder-template.xml").read()
 
 file_str = \
 """<distributionFile>
@@ -12,4 +21,13 @@ for line in open(sys.argv[1]).readlines():
     for file in line.split():
         file_list_str += file_str.replace("$$FILE$$", file)
 
-print file_list_str
+file_list_str = file_list_str.replace("\n", "\n                    ")
+
+if file_list_str[-1] == "\n": file_list_str = file_list_str[:-1]
+
+result = blue_print(template_str,
+                    [["$$VERSION$$", Version ],
+                     ["$$FILE_LIST$$", file_list_str],
+                    ])
+
+open("install-builder.xml", "w").write(result)
