ad3e7191ecf5078fc541521a87bfa39237351026
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-03-02 11:03:36 +0000

    Release 0.22.5

---
+++ tags/quex-0.22.5/quex/input/regular_expression.py
@@ -0,0 +1,52 @@
+from StringIO import StringIO
+from   quex.frs_py.file_in          import EndOfStreamException, error_msg
+from   quex.exception               import RegularExpressionException
+import quex.lexer_mode              as lexer_mode
+import quex.core_engine.regular_expression.core as regex
+import quex.core_engine.regular_expression.character_set_expression as charset_expression
+
+def parse(fh, Setup):
+
+    start_position = fh.tell()
+    try:
+        # -- parse regular expression, build state machine
+        pattern_state_machine = regex.do(fh, lexer_mode.shorthand_db, 
+                                         Setup.begin_of_stream_code, Setup.end_of_stream_code,
+                                         DOS_CarriageReturnNewlineF=Setup.dos_carriage_return_newline_f)
+
+        if pattern_state_machine == None:
+            error_msg("No valid regular expression detected.", fh)
+
+    except RegularExpressionException, x:
+        fh.seek(start_position)
+        error_msg("Regular expression parsing:\n" + x.message, fh)
+
+    except EndOfStreamException:
+        fh.seek(start_position)
+        error_msg("End of file reached while parsing regular expression.", fh)
+
+
+    end_position = fh.tell()
+
+    fh.seek(start_position)
+    regular_expression = fh.read(end_position - start_position)
+
+    return regular_expression, pattern_state_machine
+
+
+def parse_character_set(Txt):
+
+    try:
+        # -- parse regular expression, build state machine
+        character_set = charset_expression.snap_set_expression(StringIO("[:" + Txt + ":]"))
+
+        if character_set == None:
+            error_msg("No valid regular character set expression detected.")
+
+    except RegularExpressionException, x:
+        error_msg("Regular expression parsing:\n" + x.message)
+
+    except EndOfStreamException:
+        error_msg("End of character set expression reached while parsing.")
+
+    return character_set
