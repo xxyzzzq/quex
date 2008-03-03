6d0be2ba08e9ac3deeae0b28ac3fd8824991f5a7
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-03-03 07:18:07 +0000

    Interval Handling

--- quex/core_engine/TEST/test-NumberSet-difference.py
+++ quex/core_engine/TEST/test-NumberSet-difference.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 import test_NumberSet_base
 import sys
+import os
+from copy import deepcopy
+sys.path.append(os.environ["QUEX_PATH"])
+from quex.core_engine.interval_handling import Interval, NumberSet
 
 if "--hwut-info" in sys.argv:
     print "NumberSet: Difference"
@@ -15,3 +19,6 @@ def the_difference(Comment, A, B):
 
 test_NumberSet_base.do("DIFFERENCE", the_difference)    
 
+# some more tests:
+X = deepcopy(test_NumberSet_base.A6)
+the_difference("Special Case 10", X, NumberSet([Interval(0, 1), Interval(1000, 1001)]))
