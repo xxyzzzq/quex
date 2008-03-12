0e466d23d494b202bb790f5fbb8e7bd5bccd0d98
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-03-12 04:00:51 +0000

    hopcroft minimization

--- quex/core_engine/state_machine/hopcroft_minimization.py
+++ quex/core_engine/state_machine/hopcroft_minimization.py
@@ -4,12 +4,14 @@ from   quex.core_engine.state_machine.index import map_state_combination_to_inde
 
 class StateSet_List:
     def __init__(self, StateMachine):
-        self.sm             = StateMachine
-        self.state_set_list = self.__initial_split()
-        self.size           = len(self.state_set_list)
-
-        ## print "##", self.sm.get_string()
-        ## print "##", self.state_set_list
+        self.sm = StateMachine
+        #
+        # -- map: [state index]  -->  [index of the state set that contains it]
+        self.map = {} 
+        #
+        # -- create: self.state_set_list by initial split of all states.
+        self.__initial_split()
+        self.size = len(self.state_set_list)
 
     def get(self, Index):
         return self.state_set_list[Index]
@@ -20,83 +22,100 @@ class StateSet_List:
            acceptance states are also returned.
 
         """   
-        acceptance_state_list     = []
-        non_acceptance_state_list = []
-        for state_idx, state in self.sm.states.items():
-            if state.is_acceptance(): acceptance_state_list.append(state_idx)
-            else:                     non_acceptance_state_list.append(state_idx)
+        self.state_set_list = []
 
-        # map: set of original states ---> state indices that are of this origin
+        # (1) Split according to acceptance and non-acceptance
+        self.state_set_list.append([])  # state set '0': non-acceptance states
+        acceptance_state_set = []       # acceptance states
+        for state_index, state in self.sm.states.items():
+            if state.is_acceptance(): 
+                acceptance_state_set.append(state_index)
+            else:                     
+                self.state_set_list[0].append(state_index)  # put state into state set 0
+                self.map[state_index] = 0                   # note, that it is stored in state set '0'
+
+        # NOTE: Under normal conditions, there **must** be at least one non-acceptance state,
+        #       which happens to be the initial state (otherwise nothing would be acceptable).
+        #       But, for unit tests etc. we need to live with the possibility that the there 
+        #       might be no non-acceptance states.
+        if len(self.state_set_list[0]) == 0: del self.state_set_list[0]
+
+        # (2) Split the acceptance states according to their origin. An acceptance
+        #     state maching the, for example, an identifier is not equivalent an 
+        #     acceptance state thate that matches a number.
         db = {}   
-        def db_add(key, list_element):
-            if db.has_key(key): db[key].append(list_element)
-            else:               db[key] = [ list_element ]                             
+        def db_add(key, state_index):
+            if db.has_key(key): db[key].append(state_index)
+            else:               db[key] = [ state_index ]                             
 
-        for state_index in acceptance_state_list:
-            origin_state_machine_ids = map(lambda origin: 
-                                           origin.state_machine_id, 
-                                           self.sm.states[state_index].get_origin_list())
+        for state_index in acceptance_state_set:
+            state = self.sm.states[state_index]
+            origin_state_machine_ids = map(lambda origin: origin.state_machine_id, 
+                                           state.get_origin_list())
             state_combination_id = map_state_combination_to_index(origin_state_machine_ids) 
             db_add(state_combination_id, state_index)
 
-        # each 'value' (belonging to a key) represents the set of states that have the
-        # same combination of original states
-        result = db.values()
-            
-        if non_acceptance_state_list != []: 
-            return result + [ non_acceptance_state_list ]
-                
-        return result
+        # (2b) Enter the splitted acceptance state sets.
+        i = len(self.state_set_list) - 1           # See 'NOTE' above.
+        for state_set in db.values():
+            i += 1
+            self.state_set_list.append(state_set)  # -- enter the state set.
+            for state_index in state_set:          # -- mark for each state in which state
+                self.map[state_index] = i          #    set it is stored.
 
-    def split(self, StateSet):
+
+
+    def split(self, StateSetIndex):
         """RETURNS:  False   if StateSet does not need to be split up any further.
                      True    if the state set requires a split.
         """
-        N = len(StateSet)
+        state_set = self.state_set_list[StateSetIndex]
+        #
+        N         = len(state_set)
         assert N != 0, "State set of size '0'. List = " + repr(state_set_list)
         # only one state in state set => no change possible
         if N == 1: return False    
 
-        prototype = self.sm.states[StateSet[0]]
+        # -- choose one arbitrary state (for example state 0) as a prototype
+        #    which is compared against the remaining states in the state set.
+        prototype_index      = state_set[0]
+        prototype            = self.sm.states[prototype_index]
+        equivalent_state_set = [ prototype_index ] 
 
-        # loop over all auxiliary states from state set
-        non_equivalent_state_list = []
-        i = 1
-        while i < N:
-            state = self.sm.states[StateSet[i]]
+        # -- loop over all remaining states from state set
+        i         = 1   # state_set[i] = state index
+        element_n = N   # remaining number of elements in state set
+        for state_index in state_set[1:]:
+            state = self.sm.states[state_index]
             if self.check_equivalence(prototype, state): 
-                i += 1             # the next please, ...
-            else:
-                non_equivalent_state_list.append(state)
-                del state_set[i]   # don't increase i since the next canditate shifts to position i
-                N -= 1
+                equivalent_state_set.append(state_index)
 
-        if non_equivalent_state_list == []: return False
+        # -- Are all states equivalent?
+        if len(equivalent_state_set) == N: return False  # no split! 
 
-        # replace state set with the result of the split
-        # the old state_set has been adapted by 'split()', only 
-        # append the new state set
-        self.state_set_list.append(non_equivalent_state_list)
+        # -- States that are not equivalent (probably most likely) remain in the 
+        #    original state set and the ones that are equivalent are put into a new
+        #    set at the end of the state set list.
+        #    
+        #    Delete equivalent states from the original state set
+        for state_index in equivalent_state_set:
+            i = state_set.index(state_index)
+            del state_set[i]
+
+        #    Create the new state set at the end of the list
+        self.state_set_list.append(equivalent_state_set)
+        # -- Mark in the map the states that have moved to the new state set at the end.
+        for state_index in equivalent_state_set:
+            self.map[state_index] = self.size
+        # -- increase the size counter
         self.size += 1 
 
         return True
-
-    def get_index_of_state_set_containing_state(self, StateIdx):
-        """Returns the index of the state set that contains the given 
-           StateIdx. It returns -1  if no state set contains that index.
-        """
-
-        i = -1L   
-        for state_set in self.state_set_list:
-            i += 1L
-            if StateIdx in state_set: return i
-        return -1L  
 
     def check_equivalence(self, This, That):
         """Do state 'This' and state 'That' trigger on the same triggers to the
            same target state?
         """
-
         transition_list_0 = This.get_transition_list()
         transition_list_1 = This.get_transition_list()
 
@@ -110,15 +129,13 @@ class StateSet_List:
                 # no trigger set found in 'That' that corresponds to 'This' => not equivalent
                 return False
 
-            target_0 = self.get_index_of_state_set_containing_state(t0.target_state_index)
-            target_1 = self.get_index_of_state_set_containing_state(t1.target_state_index)
+            target_0 = self.map[t0.target_state_index]
+            target_1 = self.map[t1.target_state_index]
 
             # do both states trigger on the same trigger set to the same target state?
             if target_0 != target_1: return False
 
         return True
-
-          
             
 
 def do(SM):
@@ -148,17 +165,12 @@ def do(SM):
     while state_set_list_changed_f:
         # Loop over all sets in state set
         # by default the next state set list is the same
-        i                        = 0
+        i                        = 0              # -- loop index of the state set
         state_set_list_changed_f = False
-        while i < state_set_list.size:
-            state_set = state_set_list.get(i)
-            
-            if state_set_list.split(state_set) == False:  # -- State set remains. 
-                i += 1                                    #    The next, please!
-            else:                                 # -- No 'i += 1' since deletion shifted 
-                state_set_list_changed_f = True   #    the next at the place of i.
-
-            ## print "##", state_set_list.state_set_list
+        while i < state_set_list.size - 1:
+            i += 1
+            if state_set_list.split(i):           
+                state_set_list_changed_f = True   # -- a split happend, the state sets changed ...  
 
     # If all states in the state sets trigger equivalently, then the state set remains
     # nothing has to be done to the new state_set list, because its by default setup that way 
@@ -169,7 +181,7 @@ def create_state_machine(SM, StateSetList):
     # state set triggers to a different state set on the same trigger. the state sets can
     # become a new state machine. the state set that contains the initial state becomes 
     # the initial state of the new state machine.   
-    state_set_containing_initial_state_i = StateSetList.get_index_of_state_set_containing_state(SM.init_state_index)
+    state_set_containing_initial_state_i = StateSetList.map[SM.init_state_index]
     map_new_state_index = {}
     def create_state_index(StateSetIndex):
         if not map_new_state_index.has_key(StateSetIndex):
@@ -206,7 +218,7 @@ def create_state_machine(SM, StateSetList):
         result.set_acceptance(start_index, prototype.is_acceptance())
 
         for t in prototype.get_transition_list():
-            target_index = StateSetList.get_index_of_state_set_containing_state(t.target_state_index)
+            target_index = StateSetList.map[t.target_state_index]
             result.add_transition(start_index, 
                                   t.trigger_set, 
                                   create_state_index(target_index))
