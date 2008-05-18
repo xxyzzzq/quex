62da0a48e8f2b726055405e63dd54aec6b6dcd86
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-05-18 10:55:25 +0000

    Benchmark

--- trunk/demo/004/lexer.cpp
+++ trunk/demo/004/lexer.cpp
@@ -4,55 +4,81 @@
 // (*) include lexical analyser header
 #include "tiny_lexer"
 #include <ctime>
+#include <cstdlib>
 
 
 int 
 main(int argc, char** argv) 
 {        
     using namespace std;
-    clock_t  start_time = (clock_t)0;
-    clock_t  end_time   = (clock_t)0;
+    const clock_t  MinExperimentTime = 5 * CLOCKS_PER_SEC;
+    clock_t        start_time = (clock_t)0;
+    int            checksum = 0;
+    int            checksum_ref = -1;
 
     // (*) create token
     quex::token        Token;
     // (*) create the lexical analyser
     //     if no command line argument is specified user file 'example.txt'
     quex::tiny_lexer*  qlex = new quex::tiny_lexer(argc == 1 ? "example.txt" : argv[1]);
+    // -- repeat the experiment, so that it takes at least 5 seconds
 
     // (*) print the version 
     // cout << qlex->version() << endl << endl;
 
-#ifndef QUEX_BENCHMARK_SERIOUS
-    cout << ",------------------------------------------------------------------------------------\n";
-    cout << "| [START]\n";
-    int number_of_tokens = 0;
-#endif
-
     // (*) loop until the 'termination' token arrives
     start_time = clock();
-    do {
-        // (*) get next token from the token stream
-        qlex->get_token(&Token);
+    float repetition_n = 0.0;
+    while( clock() < MinExperimentTime ) { 
+        repetition_n += 1.0f;
+#       ifndef QUEX_BENCHMARK_SERIOUS
+        cout << ",------------------------------------------------------------------------------------\n";
+        cout << "| [START]\n";
+        int number_of_tokens = 0;
+#       endif
+        
+        do {
+            // (*) get next token from the token stream
+            qlex->get_token(&Token);
 
-        // (*) print out token information
-        //     -- name of the token
-#ifndef QUEX_BENCHMARK_SERIOUS
-        cout << Token.type_id_name() << endl;
+            checksum = (checksum + Token.type_id()) % 0x1000000; 
 
-        ++number_of_tokens;
-#endif
+            // (*) print out token information
+            //     -- name of the token
+#           ifndef QUEX_BENCHMARK_SERIOUS
+            cout << Token.type_id_name() << endl;
+            ++number_of_tokens;
+#           endif
+            // (*) check against 'termination'
+        } while( Token.type_id() != quex::TKN_TERMINATION );
 
-        // (*) check against 'termination'
-    } while( Token.type_id() != quex::TKN_TERMINATION );
-    end_time = clock();
-    cout << (end_time - start_time) << endl;
-    cout << float(end_time - start_time) / (float)CLOCKS_PER_SEC << endl;
+#       ifndef QUEX_BENCHMARK_SERIOUS
+        cout << "| [END] number of token = " << number_of_tokens << "\n";
+        cout << "`------------------------------------------------------------------------------------\n";
+#       endif
+        if( checksum_ref == -1 ) { 
+            checksum_ref = checksum; 
+        }
+        else if( checksum != checksum_ref ) {
+            cerr << "run:                " << repetition_n << endl;
+            cerr << "checksum-reference: " << checksum_ref << endl;
+            cerr << "checksum:           " << checksum     << endl;
+            throw std::runtime_error("Checksum failure");
+        }
+        checksum = 0;
+        qlex->_reset();
+    }
+    { 
+        const clock_t EndTime    = clock();
+        const float   TimeDiff   = (float)(EndTime - start_time) / (float)CLOCKS_PER_SEC;
+        const float   TimePerRun = TimeDiff / repetition_n;
+
+        cout << "Total Time: " << TimeDiff << " [sec]" << endl;
+        cout << "Runs:       " << (long)repetition_n << " [1]" << endl;
+        cout << "TimePerRun: " << TimePerRun << " [sec]" << endl;
+    }
 
     // cout << Token.type_id_name() << endl;
-#ifndef QUEX_BENCHMARK_SERIOUS
-    cout << "| [END] number of token = " << number_of_tokens << "\n";
-    cout << "`------------------------------------------------------------------------------------\n";
-#endif
 
     return 0;
 }
