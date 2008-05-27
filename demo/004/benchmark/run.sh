3355385075778ab54b6e14845dee33863fb61e02
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-05-27 20:55:42 +0000

    benchmarking

--- demo/004/benchmark/run.sh
+++ demo/004/benchmark/run.sh
@@ -3,17 +3,11 @@ output="result-$stamp.dat"
 cd ..
 make clean; make
 cd benchmark
-# blind run, load application, etc. into memory
-../lexer many-tiny-tokens.c
-# real
-../lexer many-tiny-tokens.c           >  $output
-../lexer single-large-token.c         >> $output
-../lexer linux-2.6.22.17-kernel-dir.c >> $output
-
-# blind run, load application, etc. into memory
-../lexer-lc many-tiny-tokens.c
-# real
-../lexer-lc many-tiny-tokens.c           >> $output
+../lexer-lc many-tiny-tokens.c            > $output
 ../lexer-lc single-large-token.c         >> $output
 ../lexer-lc linux-2.6.22.17-kernel-dir.c >> $output
+
+../lexer many-tiny-tokens.c           >> $output
+../lexer single-large-token.c         >> $output
+../lexer linux-2.6.22.17-kernel-dir.c >> $output
 
