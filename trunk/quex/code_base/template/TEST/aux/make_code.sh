b8df5335bae211a4565a0526f4ede61cda7eccb8
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-05-28 05:34:39 +0000

    unit tests

--- trunk/quex/code_base/template/TEST/aux/make_code.sh
+++ trunk/quex/code_base/template/TEST/aux/make_code.sh
@@ -2,10 +2,10 @@
 # template_file = $1
 # source_file   = $2
 
-sed  's/\$\$LEXER_CLASS_NAME\$\$/my_tester/g' $1 > tmp.txt
 sed  's/inline //g' tmp.txt > tmp2.txt
-echo "#include <my_tester.h>" > $2
-echo "#include <cassert>" >> $2
-echo "#line 1 \"$1\"" >> $2
-cat  tmp2.txt >> $2
+echo "#include <my_tester.h>"  > $2
+echo "#define CLASS my_tester" >> $2
+echo "#include <cassert>"      >> $2
+echo "#line 1 \"$1\""          >> $2
+cat  tmp2.txt                  >> $2
 rm tmp.txt tmp2.txt
