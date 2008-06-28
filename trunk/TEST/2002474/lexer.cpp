b6061742cc65d17851ba5da9c70e6a9b534f91b1
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-06-28 04:13:57 +0000

    Bugfix 202474

--- trunk/TEST/2002474/lexer.cpp
+++ trunk/TEST/2002474/lexer.cpp
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-int 
+    int 
 main(int argc, char** argv) 
 {        
     // (*) create token
     quex::token        Token;
     // (*) create the lexical analyser
     //     if no command line argument is specified user file 'example.txt'
-    quex::Simple * qlex = new quex::Simple(argc == 1 ? "example.dat" : argv[1], "UTF-8");
+    quex::Simple  qlex(argc == 1 ? "example.dat" : argv[1], "UTF-8");
 
     // (*) print the version 
     // cout << qlex->version() << endl << endl;
@@ -24,16 +24,16 @@ main(int argc, char** argv)
     int number_of_tokens = 0;
     // (*) loop until the 'termination' token arrives
     do {
-	// (*) get next token from the token stream
-	qlex->get_token(&Token);
+        // (*) get next token from the token stream
+        qlex.get_token(&Token);
 
-	// (*) print out token information
-	//     -- name of the token
-	cout << string(Token) << endl;
+        // (*) print out token information
+        //     -- name of the token
+        cout << string(Token) << endl;
 
-	++number_of_tokens;
+        ++number_of_tokens;
 
-	// (*) check against 'termination'
+        // (*) check against 'termination'
     } while( Token.type_id() != quex::TKN_TERMINATION );
 
     cout << "| [END] number of token = " << number_of_tokens << "\n";
