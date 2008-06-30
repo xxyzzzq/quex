86b383e183a4fe092502d2c21d67fac0e4f763bd
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-06-30 04:56:12 +0000

    Benchmark for Intel's C++

--- trunk/demo/TEST/other-compilers-intel.sh
+++ trunk/demo/TEST/other-compilers-intel.sh
@@ -4,5 +4,5 @@ if [[ $1 == "--hwut-info" ]]; then
     echo "CHOICES:  000, 001, 002, 003, 005, 006"
     exit
 fi
-source /opt/intel/cc/10.1.012/bin/iccvars.sh
+source /home/compilers/intel-cpp/bin/iccvars.sh
 source core.sh $1 DEBUG COMPILER=icpc
