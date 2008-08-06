49af84105dd4af0849cc972e932fc6b8bd24d466
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-08-06 06:35:06 +0000

    update ...

--- trunk/quex/code_base/buffer/iconv/TEST/n-beat.cpp
+++ trunk/quex/code_base/buffer/iconv/TEST/n-beat.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<fstream>
 
-#include<quex/code_base/buffer/iconv/fixed_size_character_stream>
+#include<quex/code_base/memory/iconv/BufferFiller_IConv>
+#include<quex/code_base/memory/iconv/BufferFiller_IConv.i>
 
 using namespace std;
 
@@ -14,22 +15,28 @@ main(int argc, char** argv)
     }
 
     std::FILE*   fh = fopen("test.txt", "r");
-    uint8_t      raw_buffer[5];
+    uint8_t      raw_memory[5];
     char*        target_charset = (char*)"UCS-4BE";
-    uint8_t      buffer[512];
-    quex::fixed_size_character_stream_iconv<std::FILE, uint32_t>   is(fh, raw_buffer, 5, "UTF8", target_charset);
+    uint8_t      memory[512];
+
+    QuexBuffer               buffer;
+    QuexBufferFiller_IConv   filler;
+
+    BufferFiller_Plain_init(&filler, fh, "UTF8", raw_memory, 5);
+    QuexBuffer_init(&buffer, memory, 12, (QuexBufferFiller*)&filler);
+    quex::fixed_size_character_stream_iconv<std::FILE, uint32_t>   is(fh, raw_memory, 5, "UTF8", target_charset);
 
     if( argc > 1 ) target_charset = argv[1];
 
-    const int LoadedN = is.read_characters((uint32_t*)buffer, 128);
+    const int LoadedN = is.read_characters((uint32_t*)memory, 128);
 
     cout << "character n = " << LoadedN << endl;
  
     for(int i=0; i < LoadedN*4 ; i+=4) {
-        unsigned char b0 = buffer[i+0];
-        unsigned char b1 = buffer[i+1];
-        unsigned char b2 = buffer[i+2];
-        unsigned char b3 = buffer[i+3];
+        unsigned char b0 = memory[i+0];
+        unsigned char b1 = memory[i+1];
+        unsigned char b2 = memory[i+2];
+        unsigned char b3 = memory[i+3];
         printf("%02X.%02X.%02X.%02X\n", (unsigned)b0, (unsigned)b1, (unsigned)b2, (unsigned)b3);
     }
 }
