baf73236f723a0dae55b168430d8785d516871c4
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-09-01 06:03:41 +0000

    Install Builder

--- pack-help.sh
+++ pack-help.sh
@@ -71,6 +71,11 @@ zip -r quex-$1.zip ./quex-$1
 
 # -- create xml file for the install builder
 $QUEX_PATH/make_install_builder_script.py `pwd`/quex-$1 $1
+/opt/installbuilder-5.4.11/bin/builder build ./install-builder.xml windows
+/opt/installbuilder-5.4.11/bin/builder build ./install-builder.xml linux
+/opt/installbuilder-5.4.11/bin/builder build ./install-builder.xml RPM
+/opt/installbuilder-5.4.11/bin/builder build ./install-builder.xml DEB
+
 
 # -- compress the tar file
 echo "-- Further compress .tar --> 7z and gzip"
