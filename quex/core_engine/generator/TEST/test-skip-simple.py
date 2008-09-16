2f79ce6a656fe4eb6b51cab9b10c0ec89a8d6312
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-09-16 06:11:43 +0000

    Very fast range skippers / Unit Tests

--- quex/core_engine/generator/TEST/test-skip-simple.py
+++ quex/core_engine/generator/TEST/test-skip-simple.py
@@ -2,9 +2,8 @@
 import sys
 import os
 sys.path.insert(0, os.environ["QUEX_PATH"])
-import quex.core_engine.generator.skip_code as skip_code
-from   quex.core_engine.generator.languages.core import db
 from   generator_test import create_main_function, \
+                             create_skipper_code, \
                              compile_and_run
 
 if "--hwut-info" in sys.argv:
@@ -13,50 +12,23 @@ if "--hwut-info" in sys.argv:
     print "SAME;"
     sys.exit(0)
 
-Language = sys.argv[1]
-if not (Language in ["ANSI-C-PlainMemory", "ANSI-C", "Cpp"]): 
+if len(sys.argv) < 2 or not (sys.argv[1] in ["ANSI-C-PlainMemory", "ANSI-C", "Cpp"]): 
     print "Language argument not acceptable, use --hwut-info"
     sys.exit(0)
 
+Language = sys.argv[1]
 end_sequence = map(ord, "*/")
-
-def create_code(Language, TestStr, EndSequence, QuexBufferSize=1024):
-    reached_str  = '    printf("next letter: <%c>\\n", (char)(*(me->buffer._input_p + 1)));\n'
-    reached_str += '    return true;\n'
-    end_str      = '    printf("end\\n");'
-    end_str     += '    return false;\n'
-
-    txt  = "#define QUEX_CHARACTER_TYPE uint8_t\n"
-    txt += "#define QUEX_TOKEN_ID_TYPE  bool\n"  
-    if Language != "Cpp": txt += "#define __QUEX_SETTING_PLAIN_C\n"
-    txt += "#include <quex/code_base/template/Analyser>\n"
-    txt += "#include <quex/code_base/template/Analyser.i>\n"
-    txt += "\n"
-    if Language == "Cpp": txt += "using namespace quex;\n"
-    txt += "bool  Mr_UnitTest_analyser_function(QuexAnalyser* me)\n"
-    txt += "{\n"
-    txt += "    QUEX_CHARACTER_POSITION_TYPE* post_context_start_position    = 0x0;\n"
-    txt += "    QUEX_CHARACTER_POSITION_TYPE  last_acceptance_input_position = 0x0;\n"
-    txt += "    QUEX_CHARACTER_TYPE           input                          = 0x0;\n"
-    txt += skip_code.get_range_skipper(end_sequence, db["C++"], 0, end_str)
-    txt += "__REENTRY_PREPARATION:\n"
-    txt += "    " + reached_str
-    txt += "}\n"
-
-    txt += create_main_function(Language, TestStr, QuexBufferSize)
-
-    return txt
 
 TestStr  = "abcdefg*/hijklmnop*/qrstuvw*/xyz*/ok"
 
-compile_and_run(Language, create_code(Language, TestStr, end_sequence))
+compile_and_run(Language, create_skipper_code(Language, TestStr, end_sequence))
 
 TestStr  = "*/hijklmnop*/qrstuvw*/xyz*/"
 
-compile_and_run(Language, create_code(Language, TestStr, end_sequence))
+compile_and_run(Language, create_skipper_code(Language, TestStr, end_sequence))
 
 TestStr  = "a*/h*/*/*/"
 
-compile_and_run(Language, create_code(Language, TestStr, end_sequence))
+compile_and_run(Language, create_skipper_code(Language, TestStr, end_sequence))
 
 
