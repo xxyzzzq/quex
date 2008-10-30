e65c655c2f4ff42990e1aaca84a9a529f0ee2ce4
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-10-30 19:52:14 +0000

    Unit Tests

--- TEST/bug-2209142.sh
+++ TEST/bug-2209142.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 bug=2209142
 if [[ $1 == "--hwut-info" ]]; then
-    echo "fschaef: $bug 0.33.1-pre-release: RE not suited for comment elimination"
+    echo "fschaef: $bug 0.33.1-pre-release: RE not suited for comment elimination;"
+    echo "CHOICES: error, good;"
     exit
 fi
 
 tmp=`pwd`
 cd $bug/ 
-quex -i error.qx -o Simple
-
-# cleansening
-rm -f Simple Simple-core-engine.cpp Simple.cpp Simple-token_ids Simplism
+make $1
+./$1
+make clean
 cd $tmp
