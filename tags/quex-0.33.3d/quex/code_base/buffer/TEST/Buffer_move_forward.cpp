cfbf45360e751fb2e2818726565ade2df18e5e9c
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-11-10 07:11:24 +0000

    Release 0.33.3d

---
+++ tags/quex-0.33.3d/quex/code_base/buffer/TEST/Buffer_move_forward.cpp
@@ -0,0 +1,29 @@
+#include <quex/code_base/buffer/TEST/Buffer_test_common.i>
+
+int
+main(int argc, char** argv)
+{
+    if( cl_has(argc, argv, "--hwut-info") ) {
+        printf("Move by Offset: Forward (BPC=%i);\n", sizeof(QUEX_CHARACTER_TYPE));
+        printf("CHOICES:  1, 2, 3, 4, 5;\n");
+        return 0;
+    }
+
+
+    QuexBuffer           buffer;
+    QUEX_CHARACTER_TYPE  memory[]      = { '|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '|'}; 
+    int                  memory_size   = 12;
+    size_t               fallback_n    = 0;
+    const size_t         StepSize      = atoi(argv[1]);
+
+    assert(QUEX_SETTING_BUFFER_MIN_FALLBACK_N == 5);
+
+    /* Filler = 0x0, otherwise, buffer would start loading content */
+    QuexBuffer_init(&buffer, memory_size, 0x0);
+    QuexBuffer_setup_memory(&buffer, memory, memory_size);
+
+    buffer._input_p = buffer._memory._front + 1;
+
+    test_move_forward(&buffer, StepSize);
+
+}
---
+++ tags/quex-0.33.3d/quex/code_base/buffer/iconv/TEST/Buffer_move_forward.cpp
@@ -0,0 +1,38 @@
+#include <quex/code_base/buffer/TEST/Buffer_test_common.i>
+#include <quex/code_base/buffer/iconv/BufferFiller_IConv.i>
+
+using namespace std;
+using namespace quex;
+
+int
+main(int argc, char** argv)
+{
+    if( cl_has(argc, argv, "--hwut-info") ) {
+        printf("Move by Offset: Forward (BPC=%i);\n", sizeof(QUEX_CHARACTER_TYPE));
+        printf("CHOICES:  1, 2, 3, 4, 5;\n");
+        return 0;
+    }
+
+    QuexBuffer      buffer;
+    const int       RawMemorySize = 6;
+    const size_t    StepSize      = atoi(argv[1]);
+    char*           target_charset = (char*)"UCS-4LE";
+    const uint16_t  test = 0x4711;
+
+    /*
+    if( ((uint8_t*)test)[0] == 47 && ((uint8_t*)test)[1] == 1 ) {
+         target_charset = (char*)"UCS-4BE";
+    }
+    */
+
+    QuexBufferFiller_IConv<FILE> filler;
+    std::FILE*                   fh = fopen("test.txt", "r");
+    assert( fh != 0x0 );
+
+    QuexBufferFiller_IConv_init(&filler, fh, "UTF8", target_charset, RawMemorySize);
+    QuexBuffer_init(&buffer, 5, (QuexBufferFiller*)&filler);
+
+    test_move_forward(&buffer, StepSize); 
+    fclose(fh); /* this deletes the temporary file (see description of 'tmpfile()') */
+}
+
