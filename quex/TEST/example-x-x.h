ef4dc32b174994864b75428ba27caab431de47fc
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-11-12 07:22:15 +0000

    Unit Tests

---
+++ quex/TEST/example-x-x.h
@@ -0,0 +1,6 @@
+/* TKN_in_example_x_comment */
+// TKN_in_example_x_comment_2
+TKN_in_example_x_x
+#   include "example-x.h"
+#   include "example.h"
+#   include "not-found.h"
---
+++ quex/TEST/example.h
@@ -0,0 +1,3 @@
+/* TKN_in_example_comment */
+// TKN_in_example_comment_2
+TKN_in_example
