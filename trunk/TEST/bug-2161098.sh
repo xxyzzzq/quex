8848abc055b62aabf8157a7c05eac532bf1ed42c
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-11-26 22:02:49 +0000

    Unit Tests

--- trunk/TEST/bug-2161098.sh
+++ trunk/TEST/bug-2161098.sh
@@ -2,13 +2,15 @@
 bug=2161098
 if [[ $1 == "--hwut-info" ]]; then
     echo "fschaef: $bug 0.32.1 Range Skipper Line Number Counter"
+    echo "CHOICES: c_comments, shell_comments, funny_comments;"
     exit
 fi
 
 tmp=`pwd`
 cd $bug/ 
-quex -i error.qx -o Simple
+make INPUT=$1
+./lexer $1.txt
 
 # cleansening
-rm -f Simple Simple-core-engine.cpp Simple.cpp Simple-token_ids Simplism
+rm -f Simple Simple-core-engine.cpp Simple.cpp Simple-token_ids 
 cd $tmp
