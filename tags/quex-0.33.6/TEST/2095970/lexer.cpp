bc0ec982edd00b2fd758e343ff6656ff67d07344
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-11-26 22:23:39 +0000

    Release 0.33.6

---
+++ tags/quex-0.33.6/TEST/2095970/lexer.cpp
@@ -0,0 +1,47 @@
+#include<fstream>    
+#include<iostream> 
+
+// (*) include lexical analyser header
+#include "Simple"
+
+
+int 
+main(int argc, char** argv) 
+{        
+    using namespace std;
+
+    // (*) create token
+    quex::Token        Token;
+    QUEX_TOKEN_ID_TYPE token_id = __QUEX_TOKEN_ID_UNINITIALIZED;
+    // (*) create the lexical analyser
+    //     if no command line argument is specified user file 'example.txt'
+    quex::Simple*  qlex = new quex::Simple(argc == 1 ? "example.txt" : argv[1]);
+
+    // (*) print the version 
+    cout << ",------------------------------------------------------------------------------------\n";
+    cout << "| [START]\n";
+
+    int number_of_tokens = 0;
+    // (*) loop until the 'termination' token arrives
+    do {
+#       ifdef QUEX_OPTION_TOKEN_SENDING_VIA_QUEUE
+        qlex->get_token(&Token);
+        cout << Token.type_id_name() << endl;
+        token_id = Token.type_id();
+#       else
+        token_id = qlex->get_token();
+        cout << Token.map_id_to_name(token_id) << endl;
+#       endif
+        ++number_of_tokens;
+
+        // (*) check against 'termination'
+    } while( token_id != __QUEX_TOKEN_ID_TERMINATION );
+
+    // cout << Token.type_id_name() << endl;
+    cout << "| [END] number of token = " << number_of_tokens << "\n";
+    cout << "`------------------------------------------------------------------------------------\n";
+
+    cout << "buffer size = " << QUEX_SETTING_BUFFER_SIZE << endl;
+
+    return 0;
+}
---
+++ tags/quex-0.33.6/TEST/feature-2251359.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+bug=2251359
+if [[ $1 == "--hwut-info" ]]; then
+    echo "marcoantonelli: $bug (feature) Single-character token without name"
+    echo "CHOICES: good, 1, 2, 3, 4, 5, 6;"
+    exit
+fi
+
+tmp=`pwd`
+cd $bug/ 
+if [[ $1 == "good" ]]; then
+    quex -i good.qx -o Simple
+    echo "(*) Core Engine"
+    awk ' /QUEX_TKN_/ { print; } ' Simple-core-engine.cpp 
+    echo "(*) Token IDs"
+    awk ' /QUEX_TKN_/ { print; } ' Simple-token_ids
+else
+    quex -i error-$1.qx -o Simple
+fi
+
+# cleansening
+rm -f Simple Simple-core-engine.cpp Simple.cpp Simple-token_ids
+cd $tmp
