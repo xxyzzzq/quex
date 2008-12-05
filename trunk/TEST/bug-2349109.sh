af908a2388f7cbd69a0a1cb4ae8cc395004e45be
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-12-05 06:50:12 +0000

    Unit Tests

--- trunk/TEST/bug-2349109.sh
+++ trunk/TEST/bug-2349109.sh
@@ -2,7 +2,7 @@
 bug=2349109
 if [[ $1 == "--hwut-info" ]]; then
     echo "fschaef: $bug 0.33.5 Line and Column number counts with Set skippers;"
-    echo "CHOICES: newline-skipper, non-newline-skipper;"
+    echo "CHOICES: newline-skipper, newline-skipper-utf8, non-newline-skipper;"
     exit
 fi
 
