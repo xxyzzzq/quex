8182b893c086a0d02d9b980acce6ba628bef94e9
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-12-05 07:21:36 +0000

    Release 0.34.1

---
+++ tags/quex-0.34.1/quex/code_base/buffer/plain/TEST/BufferFiller_forward_load.cpp
@@ -0,0 +1,42 @@
+#include <quex/code_base/definitions>
+#include <quex/code_base/buffer/Buffer.i>
+#include <quex/code_base/buffer/BufferFiller.i>
+#include <quex/code_base/buffer/plain/BufferFiller_Plain.i>
+
+#include "test-helper.h"
+
+int
+main(int argc, char** argv)
+{
+    using namespace quex;
+    if( cl_has(argc, argv, "--hwut-info") ) {
+        printf("Load Forward: Character=%i Byte(s), Fallback=%i\n", 
+               sizeof(QUEX_CHARACTER_TYPE), (int)QUEX_SETTING_BUFFER_MIN_FALLBACK_N);
+        return 0;
+    }
+    FILE*                         fh = prepare_input();
+    QuexBuffer                    buffer;
+    QuexBufferFiller_Plain<FILE>  filler;
+
+    QuexBufferFiller_Plain_init(&filler, fh);
+    buffer.filler = (quex::__QuexBufferFiller_tag*)&filler;
+    QuexBufferMemory_init(&(buffer._memory), MemoryManager_get_BufferMemory(8), 8);      
+    QuexBuffer_init(&buffer);
+
+    do {
+        printf("------------------------------------------------------------\n");
+        QuexBuffer_show_byte_content(&buffer, 5);
+        printf("     ");
+        QuexBuffer_show_content(&buffer);
+        printf("\n");
+        if( buffer._end_of_file_p != 0x0 ) break;
+        buffer._input_p        = buffer._memory._back;
+        buffer._lexeme_start_p = buffer._memory._back;
+        /**/
+        QuexBufferFiller_load_forward(&buffer);
+        printf("\n");
+    } while( 1 + 1 == 2 );
+
+    fclose(fh); /* this deletes the temporary file (see description of 'tmpfile()') */
+}
+
---
+++ tags/quex-0.34.1/quex/core_engine/generator/TEST/test-position_adaption.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+import sys
+import os
+sys.path.append("../../../../")
+import generator_test
+
+if "--hwut-info" in sys.argv:
+    print "Buffer Reload: Forward Position Adaption"
+    print "CHOICES: Cpp_ASSERTS, Cpp, ANSI-C-ASSERTS, ANSI-C;"
+    print "SAME;"
+    exit(0)
+
+if sys.argv[1].find("ASSERTS") != -1: ASSERT_str = "-DQUEX_OPTION_ASSERTS"
+else:                                 ASSERT_str = ""
+if sys.argv[1].find("ANSI-C") != -1: BufferType = "ANSI-C"
+else:                                BufferType = "Cpp"
+
+pattern_action_pair_list = [
+    # keyword (needs to come before identifier, because otherwise they would be overruled by it.)
+    ('"X"/"XXXXXXXXX"', "1 X / ... X"), 
+    ('"XX"/"XXXXXXXY"', "2 X / ... Y"), 
+    ('"XXX"/"XXXXXXZ"', "3 X / ... Z"), 
+    ('"XXXX"/"XXXXXA"', "4 X / ... A"), 
+    # identifier
+    ('[_a-zA-Z][_a-zA-Z0-9]*',   "IDENTIFIER"),
+    # 
+    ('[ \\t\\n]+',               "WHITESPACE")
+]
+
+# NOTE: Buffer Length in generator_test.py = 15 
+#       => number of X = 10, so that at lease one buffer load has to occur
+test_str = """
+XXXXXXXXXX
+AAAAAAAAAAA
+XXXXXXXXXY
+AAAAAAAAAA
+XXXXXXXXXZ
+AAAAAAAAA
+XXXXXXXXXA
+AAAAAAAA
+XXXXXXXXXX
+AAAAAAA
+XXXXXXXXXY
+AAAAAA
+XXXXXXXXXZ
+AAAAA
+XXXXXXXXXA
+AAAA
+XXXXXXXXXX
+AAA
+XXXXXXXXXY
+AA
+XXXXXXXXXZ
+A
+XXXXXXXXXA
+"""
+
+generator_test.do(pattern_action_pair_list, test_str, {}, BufferType,
+                  AssertsActionvation_str=ASSERT_str)
+    
