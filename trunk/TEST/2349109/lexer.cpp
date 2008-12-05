c5e685100746f3347007e529d58b67c2fe67bdc5
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2008-12-05 05:55:57 +0000

    Daily Bump

--- trunk/TEST/2349109/lexer.cpp
+++ trunk/TEST/2349109/lexer.cpp
@@ -10,7 +10,12 @@ main(int argc, char** argv)
 {        
     // we want to have error outputs in stdout, so that the unit test could see it.
     quex::Token   Token;
+#   if defined(__QUEX_OPTION_UNIT_TEST_UTF8__)
+    quex::Simple  qlex(argc == 1 ? "example.txt" : argv[1], "UTF-8");
+    cout << "## UTF-8 encoding\n";
+#   else
     quex::Simple  qlex(argc == 1 ? "example.txt" : argv[1]);
+#   endif
 
     cout << "## An Assert-Abortion might be an intended element of the experiment.\n";
     do {
