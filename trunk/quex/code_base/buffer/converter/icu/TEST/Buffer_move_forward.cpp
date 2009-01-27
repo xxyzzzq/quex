34425db736304ac97d01110f372870f7693e4f4b
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-01-27 21:57:50 +0000

    Update

---
+++ trunk/quex/code_base/buffer/converter/icu/TEST/Buffer_move_forward.cpp
@@ -0,0 +1,40 @@
+#include <quex/code_base/buffer/TEST/Buffer_test_common.i>
+#include <quex/code_base/buffer/converter/icu/BufferFiller_ICU.i>
+
+using namespace std;
+using namespace quex;
+
+int
+main(int argc, char** argv)
+{
+    if( cl_has(argc, argv, "--hwut-info") ) {
+        printf("Move by Offset: Forward (BPC=%i);\n", sizeof(QUEX_CHARACTER_TYPE));
+        printf("CHOICES:  1, 2, 3, 4, 5;\n");
+        return 0;
+    }
+
+    QuexBuffer      buffer;
+    const int       RawMemorySize = 6;
+    const size_t    StepSize      = atoi(argv[1]);
+    char*           target_charset = (char*)"UCS-4LE";
+
+    /*
+    const uint16_t  test = 0x4711;
+    if( ((uint8_t*)test)[0] == 47 && ((uint8_t*)test)[1] == 1 ) {
+         target_charset = (char*)"UCS-4BE";
+    }
+    */
+
+    QuexBufferFiller_Converter<FILE> filler;
+    std::FILE*                   fh = fopen("test.txt", "r");
+    assert( fh != 0x0 );
+
+    QuexBufferFiller_Converter_ICU_construct(&filler, fh, "UTF8", target_charset, RawMemorySize);
+    buffer.filler = (quex::__QuexBufferFiller_tag*)&filler;
+    QuexBufferMemory_init(&(buffer._memory), MemoryManager_get_BufferMemory(5), 5);      
+    QuexBuffer_init(&buffer, /* OnlyResetF */false);
+
+    test_move_forward(&buffer, StepSize); 
+    fclose(fh); /* this deletes the temporary file (see description of 'tmpfile()') */
+}
+
