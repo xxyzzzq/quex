96056a24db75fbc2a265add1b022662e080755db
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-03-19 07:19:51 +0000

    Update

--- quex/TEST/token-policies/TEST/lexer.cpp
+++ quex/TEST/token-policies/TEST/lexer.cpp
@@ -7,98 +7,127 @@
 
 using namespace std;
 
-void pseudo_analysis(quex::QuexAnalyser* me);
-void test();
-
-const char* choice = "";
+void                pseudo_analysis(quex::QuexAnalyser* me);
+QUEX_TYPE_TOKEN_ID  test_core(lexer&, const char*);
 
 int 
 main(int argc, char** argv) 
 {
     if( argc > 1 && strcmp(argv[1], "--hwut-info") == 0 ) {
-        printf("Token Policy: Queue");
-        printf("CHOICES: 1, Many;\n");
+#       if   defined( QUEX_OPTION_TOKEN_POLICY_QUEUE )
+        printf("Token Policy: Queue;\n");
+        printf("CHOICES: receive-1, receive-2;\n");
+        printf("SAME;\n");
+#       elif defined( QUEX_OPTION_TOKEN_POLICY_USERS_TOKEN )
+        printf("Token Policy: UsersToken;\n");
+        printf("CHOICES: receive-1, receive-2;\n");
+        printf("SAME;\n");
+#       elif defined( QUEX_OPTION_TOKEN_POLICY_USERS_QUEUE )
+        printf("Token Policy: UsersQueue;\n");
+#       endif
+        return 0;
     }
     cout << "NOTE: The production of an assertion error might be part of the test.\n";
     cout << "---------------------------------------------------------------------\n";
     stderr = stdout;
+#   if defined(QUEX_OPTION_TOKEN_POLICY_QUEUE) || defined(QUEX_OPTION_TOKEN_POLICY_USERS_TOKEN)
     if( argc < 2 ) return 0;
-    choice = argv[1];
-    test();
-    return 0;
-}
+#   endif
 
-
-void test()
-{        
-    quex::Token   Token;
     lexer         qlex("example.txt");
-
-    int number_of_tokens = 0;
 
     ((quex::QuexAnalyser*)&qlex)->current_analyser_function = pseudo_analysis;
 
-    do {
-        qlex.receive(&Token);
-
-        cout << "received: " << Token.type_id_name() << endl;
-
-        ++number_of_tokens;
-
-    } while( Token.type_id() != QUEX_TKN_TERMINATION );
-
-    cout << "Number of Tokens: " << number_of_tokens << endl;
+    while( test_core(qlex, argv[1]) != QUEX_TKN_TERMINATION );
 }
+
+#if   defined( QUEX_OPTION_TOKEN_POLICY_QUEUE )
+QUEX_TYPE_TOKEN_ID test_core(lexer& qlex, const char* Choice)
+{
+    quex::Token   token;
+    quex::Token*  token_p;
+    if( strcmp(Choice, "receive-1") == 0 ) {
+        qlex.receive(&token);
+        cout << "received: " << token.type_id_name() << endl;
+        return token.type_id();
+    }
+    else {
+        qlex.receive(&token_p);
+        cout << "received: " << token_p->type_id_name() << endl;
+        return token_p->type_id();
+    }
+}
+
+#elif defined( QUEX_OPTION_TOKEN_POLICY_USERS_TOKEN )
+QUEX_TYPE_TOKEN_ID test_core(lexer& qlex, const char* Choice)
+{        
+    quex::Token Token;
+    if( strcmp(Choice, "receive-1") == 0 ) { qlex.token = &Token; qlex.receive(); }
+    else                                   qlex.receive(&Token);
+    cout << "received: " << Token.type_id_name() << endl;
+    return Token.type_id();
+}
+
+#elif defined( QUEX_OPTION_TOKEN_POLICY_USERS_QUEUE )
+QUEX_TYPE_TOKEN_ID test_core(lexer& qlex, const char* Choice)
+{        
+    quex::Token   MyArray[5];
+    quex::Token*  water_mark = qlex.receive(MyArray, MyArray + 5);
+
+    for(quex::Token* iterator = MyArray; iterator != water_mark; ++iterator) {
+        cout << "    received: " << iterator->type_id_name() << endl;
+    }
+    cout << "---- \n";
+    return (water_mark-1)->type_id();
+}
+
+#endif
 
 void pseudo_analysis(quex::QuexAnalyser* me)
 {
     lexer&     self = *((lexer*)me);
     static int i = 0;
 
-    if( strcmp(choice, "1") == 0 ) {
-        switch( i++ ) {
-        default: self.send(QUEX_TKN_TERMINATION); break;
-        case 0:  self.send(QUEX_TKN_ONE);         break;
-        case 1:  self.send(QUEX_TKN_TWO);         break;
-        case 2:  self.send(QUEX_TKN_THREE);       break;
-        case 3:  self.send(QUEX_TKN_FOUR);        break;
-        case 4:  self.send(QUEX_TKN_FIVE);        break;
-        }
-    }
-    else if( strcmp(choice, "Many") == 0 ) {
-        switch( i++ ) {
-        default: self.send(QUEX_TKN_TERMINATION); break;
-        case 0:  
-                 self.send(QUEX_TKN_ONE);
-                 self.send(QUEX_TKN______NEXT_____);
-                 break;
-        case 1:
-                 self.send(QUEX_TKN_ONE);
-                 self.send(QUEX_TKN_TWO);
-                 self.send(QUEX_TKN______NEXT_____);
-                 break;
-        case 2:
-                 self.send(QUEX_TKN_ONE);
-                 self.send(QUEX_TKN_TWO);
-                 self.send(QUEX_TKN_THREE);
-                 self.send(QUEX_TKN______NEXT_____);
-                 break;
-        case 3:
-                 self.send(QUEX_TKN_ONE);
-                 self.send(QUEX_TKN_TWO);
-                 self.send(QUEX_TKN_THREE); 
-                 self.send(QUEX_TKN_FOUR);   
-                 self.send(QUEX_TKN______NEXT_____);
-                 break;
-        case 4:
-                 self.send(QUEX_TKN_ONE);
-                 self.send(QUEX_TKN_TWO);
-                 self.send(QUEX_TKN_THREE); 
-                 self.send(QUEX_TKN_FOUR); 
-                 self.send(QUEX_TKN_FIVE); 
-                 self.send(QUEX_TKN______NEXT_____);
-                 break;
-        }
+    switch( i++ ) {
+    default: self.send(QUEX_TKN_TERMINATION); break;
+    case 0:  self.send(QUEX_TKN_ONE);         break;
+    case 1:  self.send(QUEX_TKN_TWO);         break;
+    case 2:  self.send(QUEX_TKN_THREE);       break;
+    case 3:  self.send(QUEX_TKN_FOUR);        break;
+    case 4:  self.send(QUEX_TKN_FIVE);        break;
+    case 5:  self.send(QUEX_TKN______NEXT_____); break;
+    case 6:  self.send(QUEX_TKN______NEXT_____); break;
+    case 7:  self.send(QUEX_TKN______NEXT_____); break;
+    case 8:  
+             self.send(QUEX_TKN_ONE);
+             self.send(QUEX_TKN______NEXT_____);
+             break;
+    case 9:
+             self.send(QUEX_TKN_ONE);
+             self.send(QUEX_TKN_TWO);
+             self.send(QUEX_TKN______NEXT_____);
+             break;
+    case 10:
+             self.send(QUEX_TKN_ONE);
+             self.send(QUEX_TKN_TWO);
+             self.send(QUEX_TKN_THREE);
+             self.send(QUEX_TKN______NEXT_____);
+             break;
+    case 11:
+             self.send(QUEX_TKN_ONE);
+             self.send(QUEX_TKN_TWO);
+             self.send(QUEX_TKN_THREE); 
+             self.send(QUEX_TKN_FOUR);   
+             self.send(QUEX_TKN______NEXT_____);
+             break;
+    case 12:
+             self.send(QUEX_TKN_ONE);
+             self.send(QUEX_TKN_TWO);
+             self.send(QUEX_TKN_THREE); 
+             self.send(QUEX_TKN_FOUR); 
+             self.send(QUEX_TKN_FIVE); 
+             self.send(QUEX_TKN______NEXT_____);
+             break;
     }
 }
 
