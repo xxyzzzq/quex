c93ba31ea838fb4e8c3e9249257f9349cde09da7
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-04-19 08:45:09 +0000

    Daily Bump

---
+++ quex/input/TEST/token_type-3.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+import sys
+import os
+
+sys.path.append(os.environ["QUEX_PATH"])
+
+from quex.input.token_type import *
+from StringIO import StringIO
+
+
+if "--hwut-info" in sys.argv:
+    print "token_type: Union Members;"
+    print "CHOICES: None, One, One-2, Some, Some-2, Forbidden, Error, Twice, Twice-2;"
+    sys.exit(0)
+
+def test(Txt):
+    descr = TokenTypeDescriptor()
+    sh = StringIO("union " + Txt)
+    sh.name = "a string"
+    print "-----------------------------"
+    print "IN:"
+    print "    [" + Txt.replace("\n", "\n    ") + "]"
+    print 
+    print "OUT:"
+    print 
+    parse_section(sh, descr, [])
+    try:
+        pass
+    except Exception, inst:
+        print "Exception Caught: " + inst.__class__.__name__ 
+        pass
+    print descr
+
+
+if   "One" in sys.argv:
+    test("{ name : std::string; }")
+
+elif "One-2" in sys.argv:
+    test("""{ 
+              { 
+                 name : std::string; 
+                 value : int; 
+              }
+            }
+         """)
+
+elif "None" in sys.argv:
+    test("  {")
+
+elif "Some" in sys.argv:
+    test("{ name : std::string; number_list : std::vector<int>; }")
+
+elif "Some-2" in sys.argv:
+    test("""{ 
+              { 
+                 name : std::string; 
+                 value : int; 
+              }
+              namer : std::string; number_list : std::vector<int>; 
+            }""")
+
+elif "Forbidden"  in sys.argv:
+    test("{ id : uint8_t; }")
+
+elif "Error"  in sys.argv:
+    test("{ something : uint8_t }")
+
+elif "Twice"  in sys.argv:
+    test("{ name : std::string;\n name : std::vector<int>; }")
+
+elif "Twice-2"  in sys.argv:
+    test("""{ 
+              { 
+                 name : std::string; 
+                 value : int; 
+              }
+              name : std::string; number_list : std::vector<int>; 
+            }""")
+
+
+
