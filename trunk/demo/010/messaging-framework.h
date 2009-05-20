363a6295ec098f51b7a469c1cc6ec3463b2b5fb9
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-05-20 03:57:43 +0000

    Daily Bump

---
+++ trunk/demo/010/messaging-framework.h
@@ -0,0 +1,14 @@
+#ifndef __INCLUDE_GUARD__MESSAGING_FRAMEWORK__
+#define __INCLUDE_GUARD__MESSAGING_FRAMEWORK__
+
+/* Assume that some low level driver communicates the place where 
+ * input is placed via macros.                                     */
+#define  MESSAGING_FRAMEWORK_BUFFER_SIZE  ((size_t)(65536))
+extern QUEX_TYPE_CHARACTER   MESSAGING_FRAMEWORK_BUFFER[MESSAGING_FRAMEWORK_BUFFER_SIZE];
+
+extern size_t messaging_framework_receive(QUEX_TYPE_CHARACTER** buffer);
+extern size_t messaging_framework_receive_into_buffer(QUEX_TYPE_CHARACTER*, size_t);
+extern size_t messaging_framework_receive_to_internal_buffer();
+extern void   messaging_framework_release(uint8_t*);
+
+#endif // __INCLUDE_GUARD__MESSAGING_FRAMEWORK__
