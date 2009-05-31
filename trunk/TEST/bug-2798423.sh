8c6b539b0ad8f12a7f6dbc271a0195bcbf831baa
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-05-31 04:01:18 +0000

    Daily Bump

---
+++ trunk/TEST/bug-2798423.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+bug=2798423
+if [[ $1 == "--hwut-info" ]]; then
+    echo "sphericalcow: $bug 0.39.4 token_type default __copy function does not compile"
+    exit
+fi
+
+tmp=`pwd`
+cd $bug/ 
+quex -i simple.qx -o Simple --nsacc
+rm -f *.o
+echo "No error -- is just fine"
+gcc -c -Wall -I$QUEX_PATH -I. *.cpp
+ls *.o
+
+# cleansening
+rm -f Simple*
+cd $tmp
