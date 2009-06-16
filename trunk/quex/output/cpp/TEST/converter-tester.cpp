61abdadfec903a4de7b27342bab055d15b386b95
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-06-16 16:43:54 +0000

    Daily Bump

---
+++ trunk/quex/output/cpp/TEST/converter-tester.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "converter-tester.h"
+
+int
+main(int argc, char** argv)
+{
+    using namespace quex;
+
+    const size_t Start      = 0x0;
+    const size_t CharacterN = 255;
+    QUEX_TYPE_CHARACTER  source[256];
+    uint8_t              drain[4096];
+    uint8_t              drain_ref[4096];
+
+    /* Fill source buffer with all available characters */
+    for(int i=Start; i < Start + CharacterN; ++i) source[i-Start] = i;
+
+    /* Convert the whole array */
+    size_t Size = CONVERT_TO_UTF8(source, CharacterN, (char*)drain, 4095);
+    drain[Size] = '\0'; /* terminating zero */
+
+    printf("Result (%i):\n", (int)Size);
+    for(int i=0; i<Size; ++i) {
+        if( i % 16 == 0 ) printf("\n");
+        printf("%02X.", drain[i]);
+    }
+    printf("\n");
+    
+#   if 0
+    FILE* fhi = fopen("for-reference.txt", "wb");
+    fwrite(source, 1, CharacterN, fhi);
+    fclose(fhi);
+  
+    FILE* fh_out  = fopen("tmp.txt", "wb");
+    fwrite(drain, 1, Size, fh_out);
+    fclose(fh_out);
+#   endif
+
+    FILE* fh = fopen("reference-" __QUEX_CODEC "-to-utf8.txt", "rb");
+    fread(drain_ref, 1, 4096, fh);
+
+    /* Compare the output */
+    printf("Check result (no response == OK)\n");
+    for(int i=0; i<Size; ++i) {
+        if( drain[i] != drain_ref[i] ) {
+            printf("Error at position '%i'.\n", i);
+            break;
+        }
+    }
+    fclose(fh);
+}
