0efd683b7ef99b66463c08f4c51fdb45117a07de
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-07-25 09:23:23 +0000

    Daily Bump

--- trunk/TEST/bug-2001602.sh
+++ trunk/TEST/bug-2001602.sh
@@ -2,12 +2,17 @@
 bug=2001602
 if [[ $1 == "--hwut-info" ]]; then
     echo "attardi: $bug RE matching UTF8 l quote uno"
+    echo "CHOICES: iconv, icu, codec;"
+    echo "SAME;"
     exit
 fi
 
 tmp=`pwd`
 cd $bug/ 
-make
-./a.out test.utf8
-make clean
+make lex_$1 > tmp.txt
+cat tmp.txt | awk '(/[Ww][Aa][Rr][Nn][Ii][Nn][Gg]/ || /[Ee][Rr][Rr][Oo][Rr]/) && ! /ASSERTS/ '
+rm tmp.txt
+
+./lex_$1 test.utf8
+make clean >& /dev/null
 cd $tmp
