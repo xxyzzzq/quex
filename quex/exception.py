d1787e2dcf1a9b78189c8bd02d076c7e12cf7a7e
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-12-06 14:17:25 +0000

    Daily Bump

--- quex/exception.py
+++ quex/exception.py
@@ -1,8 +1,13 @@
 class RegularExpressionException(Exception):
+    def _get_message(self): return self._message
+    def _set_message(self, message): self._message
+
+    message = property(_get_message, _set_message)
+
     def __init__(self, InfoStr):
-        self.message = InfoStr
+        self._message = InfoStr
 
     def __repr__(self):
         txt = "QuexException:\n"
-        return txt + self.message
+        return txt + self._message
 
