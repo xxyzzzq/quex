f34c843588e3c6382143f94ea2c6f975905219fd
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-12-12 09:51:58 +0000

    Daily Bump

--- quex/output/cpp/TEST/test-token_class.py
+++ quex/output/cpp/TEST/test-token_class.py
@@ -10,6 +10,7 @@ import quex.input.setup
 quex.input.setup.setup.QUEX_TEMPLATE_DB_DIR = os.environ["QUEX_PATH"] + "/quex/code_base/"
 quex.input.setup.setup.bytes_per_ucs_code_point = 1
 quex.input.setup.setup.output_token_class_file = ""
+quex.input.setup.setup.token_class_name_space = ""
 
 if "--hwut-info" in sys.argv:
     print "Token Class Template"
