5b521d527aa4f135ad6f330fb9678cfb7638e4c8
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-12-13 15:11:36 +0000

    Daily Bump

--- demo/012/lexer.cpp
+++ demo/012/lexer.cpp
@@ -11,35 +11,35 @@ int
 main(int argc, char** argv) 
 {        
     // we want to have error outputs in stdout, so that the unit test could see it.
-    max::Lexer     max_lex("example.txt", "UTF8");
+    max::Lexer     max_lex("example-utf16.txt", "UTF16");
     max::Token     max_token;
-    moritz::Lexer  moritz_lex("example.txt", "UTF8");
+    moritz::Lexer  moritz_lex("example-ucs2.txt", "UCS-2");
     moritz::Token  moritz_token;
-    boeck::Lexer   boeck_lex("example.txt");
+    boeck::Lexer   boeck_lex("example-utf8.txt");
     boeck::Token   boeck_token;
 
 
-    /* Each lexer reads one token, since the grammars are similar the lexeme 
-     * is always the same.                                                    */
+    // Each lexer reads one token, since the grammars are similar the lexeme 
+    // is always the same.                                                    
     printf("                Max:        Moritz:      Boeck:\n");
+
     do {
         max_lex.receive(&max_token);
         moritz_lex.receive(&moritz_token);
         boeck_lex.receive(&boeck_token);
 
         /* Lexeme is same for all three. */
-#if 0
         char* lexeme = (char*)max_token.utf8_text().c_str();
         int   L      = (int)std::strlen(lexeme);
         printf(lexeme);
-            for(int i=0; i < 10 - L ; ++i) printf(" ");
-            printf("\t");
+        for(int i=0; i < 10 - L ; ++i) printf(" ");
+        printf("\t");
         printf("%s   %s   %s\n", 
                max_token.type_id_name().c_str(), 
                moritz_token.type_id_name().c_str(), 
                boeck_token.type_id_name().c_str());
-#endif
-    } while( max_token.type_id() != MAX_TKN_TERMINATION );
+
+    } while( boeck_token.type_id() != TKN_TERMINATION );
 
     return 0;
 }
