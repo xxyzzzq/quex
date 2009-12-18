d8520f3eac5d41b262b2e0df08f70697f0b737e3
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-12-18 05:39:20 +0000

    Release 0.48.1-pre-1

---
+++ tags/quex-0.48.1-pre-1/quex/code_base/analyzer/TEST/my_tester.h
@@ -0,0 +1,47 @@
+#ifndef __QUEX_INCLUDE_GUARD__ANALYZER__TEST__MY_TESTER_H
+#define __QUEX_INCLUDE_GUARD__ANALYZER__TEST__MY_TESTER_H
+#include <quex/code_base/compatibility/inttypes.h>
+
+#define QUEX_TYPE_CHARACTER           uint8_t  
+
+#include <cstdio>
+
+struct my_tester;
+namespace quex {
+typedef struct {} Token;
+typedef int       CounterLineColumnIndentation;
+}
+#define  QUEX_TYPE_ANALYZER my_tester
+#include <quex/code_base/analyzer/configuration/default>
+#include <quex/code_base/analyzer/counter/LineColumn>
+#include <quex/code_base/analyzer/Mode>
+#include <quex/code_base/analyzer/AnalyzerData>
+
+
+extern int  indentation[64];
+
+class my_tester : public quex::QUEX_NAME(AnalyzerData) {
+public:
+    my_tester();
+    quex::QUEX_NAME(CounterLineColumn)*   counter;
+    quex::QUEX_NAME(Mode)                 tester_mini_mode;
+};
+
+inline void 
+mini_mode_on_indentation(my_tester* x, int Indentation) 
+{
+    indentation[((my_tester*)x)->counter->base._line_number_at_end-1] = Indentation;
+    printf("indentation = %i\n", Indentation);
+}
+
+my_tester::my_tester() 
+{ 
+    tester_mini_mode.on_indentation = mini_mode_on_indentation;
+    __current_mode_p = &tester_mini_mode; 
+}
+
+#include <../counter/LineColumnIndentation.i>
+#include <../counter/LineColumn.i>
+#include <../../MemoryManager.i>
+
+#endif // __QUEX_INCLUDE_GUARD__ANALYZER__TEST__MY_TESTER_H
