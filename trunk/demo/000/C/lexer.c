ec7c472fb432581ed5f9f2288ca6f008358bdd9f
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2009-12-21 06:58:32 +0000

    Daily Bump

--- trunk/demo/000/C/lexer.c
+++ trunk/demo/000/C/lexer.c
@@ -2,42 +2,35 @@
 #include<iostream> 
 
 // (*) include lexical analyser header
-#include <./tiny_lexer>
-
-using namespace std;
+#include "tiny_lexer"
 
 int 
 main(int argc, char** argv) 
 {        
-    // (*) create token
-    quex::Token        Token;
-    // (*) create the lexical analyser
-    //     if no command line argument is specified user file 'example.txt'
-    quex::tiny_lexer   qlex(argc == 1 ? "example.txt" : argv[1]);
+    Token        Token;
+    tiny_lexer   qlex;
 
-    // (*) print the version 
-    // cout << qlex->version() << endl << endl;
+    tiny_lexer_construct(&qlex, "example.txt");
 
-    cout << ",------------------------------------------------------------------------------------\n";
-    cout << "| [START]\n";
+    fprintf(",------------------------------------------------------------------------------------\n");
+    fprintf("| [START]\n");
 
     int number_of_tokens = 0;
     // (*) loop until the 'termination' token arrives
     do {
         // (*) get next token from the token stream
-        qlex.receive(&Token);
+        tiny_lexer_receive(&qlex, &Token);
 
         // (*) print out token information
-        //     -- name of the token
-        cout << Token.type_id_name() << endl;
+        // cout << Token.type_id_name() << endl;
 
         ++number_of_tokens;
 
         // (*) check against 'termination'
-    } while( Token.type_id() != QUEX_TKN_TERMINATION );
+    } while( Token.type_id != QUEX_TKN_TERMINATION );
 
-    cout << "| [END] number of token = " << number_of_tokens << "\n";
-    cout << "`------------------------------------------------------------------------------------\n";
+    fprintf("| [END] number of token = %i\n", number_of_tokens);
+    fprintf("`------------------------------------------------------------------------------------\n");
 
     return 0;
 }
