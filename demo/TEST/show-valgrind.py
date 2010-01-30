d084ea8b3adf2d7d5bb99f124d3f511152609c61
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-01-30 21:35:36 +0000

    Daily Bump

--- demo/TEST/show-valgrind.py
+++ demo/TEST/show-valgrind.py
@@ -3,7 +3,7 @@ print_f = False
 for line in fh.readlines():
     if line.find("==") == 0:
         # 'valgrind' line
-        if   line.find("All heap blocks were freed") != -1: print "VALGRIND: ", line[8:].replace("=", ""), 
+        if   line.find("All heap blocks were freed") != -1: print "VALGRIND: ", line[8:].replace("=", "").replace(".", ""), 
         elif line.find(" definitely lost:") != -1: print "VALGRIND: ", line[8:],
         elif line.find(" indirectly lost:") != -1: print "VALGRIND: ", line[8:],
         elif line.find(" possibly lost:") != -1:   print "VALGRIND: ", line[8:],
