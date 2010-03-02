89d981044eb6175ffcb4315c40b7d8e8bbf14b8c
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-03-02 21:23:42 +0000

    Regression Tests

--- trunk/TEST/2078087/lexer.cpp
+++ trunk/TEST/2078087/lexer.cpp
@@ -9,14 +9,15 @@ int
 main(int argc, char** argv) 
 {        
     // we want to have error outputs in stdout, so that the unit test could see it.
-    quex::Token   Token;
+    quex::Token   token;
     quex::Simple  qlex(argc == 1 ? "example.txt" : argv[1]);
 
     cout << "## An Assert-Abortion might be an intended element of the experiment.\n";
+    qlex.token_p_set(&token);
     do {
-        qlex.receive(&Token);
-        cout << string(Token) << endl;
-    } while( Token.type_id() != QUEX_TKN_TERMINATION );
+        qlex.receive(&token);
+        cout << string(token) << endl;
+    } while( token.type_id() != QUEX_TKN_TERMINATION );
 
     return 0;
 }
