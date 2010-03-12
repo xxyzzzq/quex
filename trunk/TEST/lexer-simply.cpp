29459b5e84c5697590199511c73866c7ff976535
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-03-12 07:27:28 +0000

    Regression Test / Token Policy Cleanup

--- trunk/TEST/lexer-simply.cpp
+++ trunk/TEST/lexer-simply.cpp
@@ -9,24 +9,24 @@ int
 main(int argc, char** argv) 
 {        
     // we want to have error outputs in stdout, so that the unit test could see it.
-    quex::Token   token;
+    quex::Token*  token_p;
     quex::Simple  qlex(argc == 1 ? "example.txt" : argv[1]);
 
     cout << "## An Assert-Abortion might be an intended element of the experiment.\n";
 #   ifdef QUEX_OPTION_TOKEN_POLICY_SINGLE
-    qlex.token_p_set(&token);
+    token_p = qlex.token_p();
 #   endif
     do {
 #       ifdef QUEX_OPTION_TOKEN_POLICY_SINGLE
         qlex.receive();
 #       else
-        token = *(qlex.receive());
+        token_p = qlex.receive();
 #       endif
-        if( token.type_id() == QUEX_TKN_TERMINATION ) {
-            token.text = (QUEX_TYPE_CHARACTER*)"";
+        if( token_p->type_id() == QUEX_TKN_TERMINATION ) {
+            token_p->text = (QUEX_TYPE_CHARACTER*)"";
         }
-        cout << string(token) << endl;
-    } while( token.type_id() != QUEX_TKN_TERMINATION );
+        cout << string(*token) << endl;
+    } while( token_p->type_id() != QUEX_TKN_TERMINATION );
 
     return 0;
 }
