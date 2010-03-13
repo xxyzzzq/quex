8bfe7f0a7d20ca852e7a8f259037568370e958b0
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-03-13 15:29:40 +0000

    Regression Tests/Token Policies

--- TEST/lexer-simply.cpp
+++ TEST/lexer-simply.cpp
@@ -10,7 +10,11 @@ main(int argc, char** argv)
 {        
     // we want to have error outputs in stdout, so that the unit test could see it.
     quex::Token*  token_p;
+#   ifdef CONVERTER_ENCODING
+    quex::Simple  qlex(argc == 1 ? "example.txt" : argv[1], CONVERTER_ENCODING);
+#   else
     quex::Simple  qlex(argc == 1 ? "example.txt" : argv[1]);
+#   endif
 
     cout << "## An Assert-Abortion might be an intended element of the experiment.\n";
 #   ifdef QUEX_OPTION_TOKEN_POLICY_SINGLE
@@ -28,7 +32,10 @@ main(int argc, char** argv)
 #       ifdef PRINT_LINE_COLUMN
         cout << "(" << qlex.line_number() << ", " << qlex.column_number() << ")  \t";
 #       endif
-        cout << string(*token_p) << endl;
+        if( token_p->type_id() != QUEX_TKN_TERMINATION )
+            cout << string(*token_p) << endl;
+        else
+            cout << "<TERMINATION>\n";
     } while( token_p->type_id() != QUEX_TKN_TERMINATION );
 
     return 0;
