6d041ae8d9a4889f1f6a96e7348518c2be6e8096
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-03-25 20:21:59 +0000

    Regression Tests on Case Fold Feature

--- trunk/quex/core_engine/regular_expression/TEST/test-case_fold.py
+++ trunk/quex/core_engine/regular_expression/TEST/test-case_fold.py
@@ -5,6 +5,7 @@ import os
 sys.path.insert(0, os.environ["QUEX_PATH"])
 
 import quex.core_engine.regular_expression.core as core
+from   quex.exception import RegularExpressionException
 
 if "--hwut-info" in sys.argv:
     print "Case Folding;"
@@ -14,19 +15,30 @@ if "--hwut-info" in sys.argv:
 def test(TestString):
     print "-------------------------------------------------------------------"
     print "expression    = \"" + TestString + "\""
-    sm = core.do(TestString, {}, -1, )
-    print "state machine\n", sm 
+    try: 
+        sm = core.do(TestString, {}, -1, )
+        print "state machine\n", sm 
+
+    except RegularExpressionException, x:
+        print x._message
+    except:
+        pass
 
 if "set" in sys.argv:
     test('[:\\C{[ﬀİ]}:]')
-    test('[:\\C{[[a-z]}:]')
+    test('[:\\C{[a-z]}:]')
     test('[:\\C{[:union([a-z], [ﬀİ]):]}:]')
+    test('[:\\C(m){[:union([a-z], [ﬀİ]):]}:]')
+    test('[:\\C(st){[i]}:]')
+    test('[:\\C(st){[I]}:]')
+    test('[:\\C(st){}:]')
+    test('[:\\C{[]}:]')
+    test('[:\\C(d){[]}:]')
+    test('[:\\C( d ){[]}:]')
+    test('[:\\C( s ){[a]}:]')
 else:
     test('a\\C{[a-zﬀİ]}a')
+    test('a\\C(s){[a-zﬀİ]}a')
+    test('a\\C(t){[a-gk-zﬀİ]}a')
+    test('a\\C(t){[a-hj-zﬀİ]/}a')
 
-# test('[:\\C{[a-z]}:]')
-# test('(a|z)/c')
-# test('"aac"|"bad"/"z"|congo')
-# test('"aac"|"bad"|bcad')
-# test("you[a-b]|[a-e]|[g-m]you")    
-# test("a(a|b)*")
