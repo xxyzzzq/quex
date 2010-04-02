5cfaf6e111c85e466a1783e002ac18190bbb8044
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-04-02 19:51:08 +0000

    Path Compression/Unit Tests

--- quex/core_engine/state_machine/compression/TEST/paths-extract_admissible_single_transitions.py
+++ quex/core_engine/state_machine/compression/TEST/paths-extract_admissible_single_transitions.py
@@ -7,7 +7,8 @@ from   quex.core_engine.interval_handling import *
 import quex.core_engine.state_machine.compression.paths as paths 
 
 if "--hwut-info" in sys.argv:
-    print "Paths: find_skeleton;\n"
+    print "Paths: extract_admissible_single_transitions;\n"
+    print "CHOICES: single, more;"
     sys.exit(0)
     
 def number_set(IntervalList):
@@ -23,15 +24,17 @@ def get_map(*TriggerSetList):
     return db
 
 def test(A, B):
-    sA = A.items(); sA.sort()
-    sB = B.items(); sB.sort()
-    print "A", sA
-    print "B", sB
-    print paths.find_skeleton(A, B)
-    print paths.find_skeleton(B, A)
+    print "A", A
+    print "B", B
+    print paths.extract_admissible_single_transitions(B, A)
+    print paths.extract_admissible_single_transitions(A, B)
 
 
 if "single" in sys.argv:
+    A = get_map([0, [1, 2]])
+    B = get_map([0, [1, 2]])
+    test(A, B)
+
     A = get_map([0, [2, 3]])
     B = get_map([0, [1, 2]])
     test(A, B)
@@ -45,16 +48,12 @@ if "single" in sys.argv:
     test(A, B)
 
 else:
-    # A = get_map([1, [2, 3]], [2, [4, 5]])
-    # B = get_map([0, [1, 2]])
-    # test(A, B)
+    A = get_map([1, [2, 3]], [2, [4, 5]])
+    B = get_map([0, [1, 2]])
+    test(A, B)
 
-    # A = get_map([1, [2, 3]], [2, [4, 5]])
-    # B = get_map([0, [1, 2]], [2, [4, 5]])
-    # test(A, B)
-
-    A = get_map([1, [1, 3]],              [3, [5, 6]], [5, [6, 10]])
-    B = get_map([1, [1, 2]], [2, [2, 3]],              [5, [5, 10]])
+    A = get_map([1, [2, 3]], [2, [4, 5]])
+    B = get_map([2, [1, 2]], [0, [4, 5]])
     test(A, B)
 
 # A = get_map([0, [0, 1]], [1, [1, 2]], [2, [3, 4]])
