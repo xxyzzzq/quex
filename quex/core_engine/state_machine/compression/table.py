dc662ed3340d786717f6fe0ee2fb2a6360c172f0
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-04-16 04:44:46 +0000

    Template Compression

--- quex/core_engine/state_machine/compression/table.py
+++ quex/core_engine/state_machine/compression/table.py
@@ -45,7 +45,7 @@ def TEST_get_NAC_matrix(sm):
         for k in range(i + 1, L):
             state_index_1 = state_index_list[k]
             trigger_map_1 = trigger_map_db[state_index_1]
-            bn, nst, net = get_nac(trigger_map_0, trigger_map_1)
+            bn, nst, net = get_metric(trigger_map_0, trigger_map_1)
             sum    = len(trigger_map_0) + len(trigger_map_1)
             saving = sum - bn
             ratio  = 0.0
@@ -54,10 +54,10 @@ def TEST_get_NAC_matrix(sm):
             fh.write("%i %.2f %i %i %i %i %i %i\n" % (abs(len(trigger_map_0) - len(trigger_map_1)), ratio, saving, sum, nst, net, i, k))
     fh.close()
 
-def get_nac(TriggerMap0, TriggerMap1):
+def get_metric(TriggerMap0, TriggerMap1):
     """Assume that interval list 0 and 1 are sorted."""
-    L0 = len(TriggerMap0)
-    L1 = len(TriggerMap1)
+    Li = len(TriggerMap0)
+    Lk = len(TriggerMap1)
     # Count the number of additional intervals if list 0 is combined with list 1
     # Each intersection requires the setup of new intervals, e.g.
     #
@@ -74,32 +74,47 @@ def get_nac(TriggerMap0, TriggerMap1):
 
     i = 0 # iterator over interval list 0
     k = 0 # iterator over interval list 1
+    assert TriggerMap0[0][0].begin == -sys.maxint
+    assert TriggerMap1[0][0].begin == -sys.maxint
+
     focus_line     = - sys.maxint
     border_count_n = 0
 
-    while i != L0 and k != L1:
-        i_begin  = TriggerMap0[i][0].begin
-        i_end    = TriggerMap0[i][0].end
-        i_target = TriggerMap0[i][1]
+    # -- intervals in trigger map are always adjacent, so the '.end'
+    #    member is not required.
+    while i != Li and k != Lk:
+        i_trigger = TriggerMap0[i]
+        k_trigger = TriggerMap1[k]
 
-        k_begin  = TriggerMap1[k][0].begin
-        k_end    = TriggerMap1[k][0].end
-        k_target = TriggerMap1[k][1]
+        i_begin   = i_trigger[0].begin
+        i_target  = i_trigger[1]
 
-        if i_begin == k_begin: 
-            new_focus_line = i_begin
-            i += 1
-            k += 1 
-        else:    
-            if i_begin < k_begin: 
-                i += 1
-                new_focus_line = i_begin
-            else:
-                k += 1
-                new_focus_line = k_begin
+        k_begin   = k_trigger[0].begin
+        k_target  = k_trigger[1]
+
+        # Step to the next *lowest* border, i.e. increment the 
+        # interval line index with the lowest '.begin'. For example:
+        # 
+        #         0   1 2  3 4 5  6   7
+        #     i   |     |      |  |   |
+        #     k   |   |    | |        |
+        #         :   : :  : : :  :   :   (6 intervals, 6 borders)
+        #
+        #                         i_begin:     k_begin:
+        # Does:  (1) ++i, ++k -->    2            1
+        #        (2) ++k      -->    2            3
+        #        (3) ++i      -->    5            3
+        #        (4) ++k      -->    5            4
+        #        (5) ++k      -->    5            6
+        #        (6) ++i      -->    6            7
+        #        (6) ++i      -->    7            7
+        if   i_begin == k_begin:  i += 1; k += 1;
+        elif i_begin < k_begin:   i += 1;
+        else:                     k += 1;
 
         if i_target == k_target: 
             same_target_list[i_target] = True
+
         else:
             pair = (i_target, k_target)
             if pair not in equivalent_target_list:
@@ -107,7 +122,7 @@ def get_nac(TriggerMap0, TriggerMap1):
 
         border_count_n += 1
 
-    border_count_n += (L0 - i) + (L1 - k)
+    border_count_n += (Li - i) + (Lk - k)
 
     return border_count_n, \
            len(same_target_list), \
