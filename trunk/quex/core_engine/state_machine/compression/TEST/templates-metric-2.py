459ad094594bcd88e4dc1d64173b1bbab27ca8c5
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-04-18 17:23:00 +0000

    Template Compression

--- trunk/quex/core_engine/state_machine/compression/TEST/templates-metric-2.py
+++ trunk/quex/core_engine/state_machine/compression/TEST/templates-metric-2.py
@@ -9,7 +9,7 @@ import quex.core_engine.state_machine.compression.templates as templates
 
 
 if "--hwut-info" in sys.argv:
-    print "Transition Map Templates: Single Target Idx Metric"
+    print "Transition Map Templates: Target Idx Combination Metric"
     print "CHOICES: 1, 2, 2b, 3, 4;"
     sys.exit(0)
 
@@ -26,7 +26,8 @@ def print_tm(TM):
 
     txt = ""
     for info in TM:
-        txt += "%i, " % info[1]
+        if type(info[1]) != list: txt += "%i, " % info[1]
+        else:                     txt += "%s, " % repr(info[1])
     txt = txt[:-2] + ";"
     print "   " + txt
 
@@ -53,48 +54,48 @@ def test(TMa, TMb):
     print
 
 tm0 = [ 
-        (Interval(-sys.maxint, 10), 1L),
-        (Interval(10, sys.maxint),  2L),
+        (Interval(-sys.maxint, 10), [10L, 11L]),
+        (Interval(10, sys.maxint),  [20L, 21L]),
       ]
 
 if "1" in sys.argv:
     tm1 = [ 
-            (Interval(-sys.maxint, 30), 1L),
-            (Interval(30, sys.maxint),  2L),
+            (Interval(-sys.maxint, 30), [10L, 11L]),
+            (Interval(30, sys.maxint),  [20L, 21L]),
           ]
     test(tm0, tm1)
 
 elif "2" in sys.argv:
     tm1 = [ 
-            (Interval(-sys.maxint, 10), 2L),
-            (Interval(10, sys.maxint),  1L),
+            (Interval(-sys.maxint, 10), [20L, 21L]),
+            (Interval(10, sys.maxint),  [10L, 11L])
           ]
     test(tm0, tm1)
 
 elif "2b" in sys.argv:
     tm1 = [ 
-            (Interval(-sys.maxint, 10), 1L),
-            (Interval(10, sys.maxint),  2L),
+            (Interval(-sys.maxint, 10), [10L, 11L]),
+            (Interval(10, sys.maxint),  [20L, 21L]),
           ]
     test(tm0, tm1)
 
 elif "3" in sys.argv:
     tm1 = [ 
-            (Interval(-sys.maxint, 5),  2L),
-            (Interval(5, 15),           3L),
-            (Interval(20, 25),          4L),
-            (Interval(25, 30),          5L),
-            (Interval(35, sys.maxint),  1L),
+            (Interval(-sys.maxint, 5),  [20L, 21L]),
+            (Interval(5, 15),           [30L, 31L]),
+            (Interval(20, 25),          [40L, 41L]),
+            (Interval(25, 30),          [50L, 51L]),
+            (Interval(35, sys.maxint),  [10L, 20L]),
           ]
     test(tm0, tm1)
 
 elif "4" in sys.argv:
     tm0 = [ 
-            (Interval(-sys.maxint, sys.maxint), 2L),
+            (Interval(-sys.maxint, sys.maxint), [20L, 21L]),
           ]
     tm1 = [ 
-            (Interval(-sys.maxint, 20), 2L),
-            (Interval(20, sys.maxint),  1L),
+            (Interval(-sys.maxint, 20), [20L, 21L]), 
+            (Interval(20, sys.maxint),  [10L, 11L]), 
           ]
     test(tm0, tm1)
 
