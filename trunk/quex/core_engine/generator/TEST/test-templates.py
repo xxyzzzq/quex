ae450903a011cc4dfec86bc86b2906f4bbb061de
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-05-18 05:03:36 +0000

    Template Compression Code Generation

--- trunk/quex/core_engine/generator/TEST/test-templates.py
+++ trunk/quex/core_engine/generator/TEST/test-templates.py
@@ -6,11 +6,16 @@ import quex.core_engine.state_machine.compression.templates as templates
 import quex.core_engine.generator.template_coder            as coder
 from   quex.core_engine.state_machine.core                  import StateMachine, State
 from   quex.core_engine.generator.state_machine_decorator   import StateMachineDecorator
+from   quex.input.setup import setup as Setup
 
 if "--hwut-info" in sys.argv:
-    print "Basic Template Compression Test"
+    print "Basic Template Compression Test;"
+    print "CHOICES: 1, 2;"
     sys.exit(0)
 
+# Buffer limit code is required in order to choose between
+# 'direct drop out' and 'drop out' in state transition map.
+Setup.buffer_limit_code = 0
 
 # A state machine decorator is required for the code generation.
 # Create a default one
@@ -31,18 +36,36 @@ for state_index in sm.states.keys():
 DSM = StateMachineDecorator(sm, "Test", [], False, False)
 # print DSM.sm().states.keys()
 
-combination = templates.TemplateCombination([100L], [200L, 202L])
 
-combination.append(-sys.maxint, 10, [1L, 3L])
-combination.append(10, 11,          100L)
-combination.append(11, 12,          -2)
-combination.append(10, sys.maxint,  [2L, 3L])
+if "1" in sys.argv:
+    combination = templates.TemplateCombination([100L], [200L, 202L])
+    combination.append(-sys.maxint, 10, None)
+    combination.append(8,   9,          [1L, None, 2L])
+    combination.append(9,  10,          [1L, 3L,   2L])
+    combination.append(10, 11,          100L)
+    combination.append(11, 12,          -2)
+    combination.append(12, 13,          None)
+    combination.append(14, sys.maxint,  [2L, 3L,   1L])
 
-print coder.do([combination], DSM)
+else:
+    combination = templates.TemplateCombination([100L], [200L, 202L, 300L, 400L, 500L])
+    combination.append(-sys.maxint, 10, [1L, None, 1L, 2L,   2L, 2L])
+    combination.append(10,  sys.maxint, [2L, 3L,   1L, None, 2L, 2L])
+
+x = coder.do([combination], DSM)
+
+print "--(Transition Targets)--------------------------------------------------"
+print
+print "".join(x[0])
+print
+print "--(Template Code)-------------------------------------------------------"
+print
+print "".join(x[1])
+print
+print "--(State Router)--------------------------------------------------------"
+print
+print "".join(x[2])
+print
 
 sys.exit(0)
 
-combination.append(10, 20, [2L, 3L, 4L])
-combination.append(20, 30, 2L)
-combination.append(30, 40, -1)
-combination.append(40, 50, 100L)
