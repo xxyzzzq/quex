882acfab54dd3c426f21d5fa6b7d4ec3be5419b1
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-05-20 05:55:17 +0000

    Unit Tests: Single State Transitions

--- quex/core_engine/generator/TEST/test-single-state-transitions.py
+++ quex/core_engine/generator/TEST/test-single-state-transitions.py
@@ -13,6 +13,7 @@
 ################################################################################
 import sys
 import os
+import random
 sys.path.insert(0, os.environ["QUEX_PATH"])
 from   quex.input.setup import setup as Setup
 import quex.core_engine.generator.languages.core as languages
@@ -26,37 +27,82 @@ import quex.core_engine.generator.state_coder.transition_block as transition_blo
 from   quex.core_engine.generator.state_machine_decorator import StateMachineDecorator
 
 if "--hwut-info" in sys.argv:
-    print "Single State: Transition Code Generation"
+    print "Single State: Transition Code Generation;"
+    print "CHOICES: A, B, C;"
     sys.exit(0)
 
 state = State()
-state.add_transition(NumberSet([Interval(10,20),    Interval(195,196)]),  1L)
-state.add_transition(NumberSet([Interval(51,70),    Interval(261,280)]),  2L)
-state.add_transition(NumberSet([Interval(90,100),   Interval(110,130)]),  3L)
-state.add_transition(NumberSet([Interval(150,151),  Interval(151,190)]),  4L)
-state.add_transition(NumberSet([Interval(190,195),  Interval(21,30)]),    5L) 
-state.add_transition(NumberSet([Interval(197, 198), Interval(198, 198)]), 6L)
-state.add_transition(NumberSet([Interval(200,230),  Interval(231,240)]),  7L)
-state.add_transition(NumberSet([Interval(250,260),  Interval(71,80), Interval(71,71)]),  8L)
+
+if "A" in sys.argv:
+    state.add_transition(NumberSet([Interval(10,20),    Interval(195,196)]),  1L)
+    state.add_transition(NumberSet([Interval(51,70),    Interval(261,280)]),  2L)
+    state.add_transition(NumberSet([Interval(90,100),   Interval(110,130)]),  3L)
+    state.add_transition(NumberSet([Interval(150,151),  Interval(151,190)]),  4L)
+    state.add_transition(NumberSet([Interval(190,195),  Interval(21,30)]),    5L) 
+    state.add_transition(NumberSet([Interval(197, 198), Interval(198, 198)]), 6L)
+    state.add_transition(NumberSet([Interval(200,230),  Interval(231,240)]),  7L)
+    state.add_transition(NumberSet([Interval(250,260),  Interval(71,80), Interval(71,71)]),  8L)
+    
+    interval_end = 300
+
+elif "B" in sys.argv:
+    interval_start = 0
+    interval_end   = -1
+    # initialize pseudo random generator: produces always the same numbers.
+    random.seed(110270)   # must set the seed for randomness, otherwise system time
+    #                     # is used which is no longer deterministic.
+    max_number = 300
+    for i in range(4000):
+        interval_size      = int(random.random() * 4) + 1
+        target_state_index = long(random.random() * 10)
+        interval_end = interval_start + interval_size
+        state.add_transition(Interval(interval_start, interval_end), target_state_index)
+        interval_start = interval_end
+
+elif "C" in sys.argv:
+    interval_start = 0
+    interval_end   = -1
+    # initialize pseudo random generator: produces always the same numbers.
+    random.seed(110270)   # must set the seed for randomness, otherwise system time
+    #                     # is used which is no longer deterministic.
+    target_state_index = long(0)
+    for i in range(1000):
+        if random.random() > 0.75:
+            interval_size      = int(random.random() * 3) + 1
+            target_state_index = long(random.random() * 5)
+
+            interval_end = interval_start + interval_size
+            state.add_transition(Interval(interval_start, interval_end), target_state_index)
+            interval_start = interval_end
+        else:
+            target_state_index = long(random.random() * 5)
+            for i in range(0, int(random.random() * 5) + 2):
+                state.add_transition(Interval(interval_start, interval_start + 1), target_state_index)
+                interval_start += 1 + int(random.random() * 2)
 
 # Make sure, that the 'goto state' is transformed into 'return index of target state'
 languages.db["Python"]["$goto"] = lambda x, y: "return %s" % repr(y)   
 
 dsm = StateMachineDecorator(StateMachine(), "UnitTest", [], False, False)
-function = "def example_func(input):\n" + "".join(transition_block.do(state.transitions().get_trigger_map(), -1, False, dsm))
-function = function.replace("_-1_", "_MINUS_1_")
+function  = "def example_func(input):\n"
+function += "".join(transition_block.do(state.transitions().get_trigger_map(), 
+                                        None, False, dsm))
+function  = function.replace("_-1_", "_MINUS_1_")
+
 # line_n = 0
 # for line in function.split("\n"):
 #     print "##%i" % line_n, line
 #     line_n += 1
-exec(function)
 
+exec(function)
 differences = []    
-for number in range(300):
-    result_state_idx = state.transitions().get_resulting_target_state_index(number)
-    if result_state_idx == None: result_state_idx = -1
-    sys.stdout.write("%i %s %s\n" % (number, repr(example_func(number)), repr(result_state_idx)))
-    if example_func(number) != result_state_idx:
+for number in range(interval_end):
+    expected_state_index = state.transitions().get_resulting_target_state_index(number)
+    if expected_state_index == None: expected_state_index = -1
+    actual_state_index = example_func(number)
+    if actual_state_index == None: actual_state_index = -1
+    sys.stdout.write("%i %s %s\n" % (number, repr(actual_state_index), repr(expected_state_index)))
+    if actual_state_index != expected_state_index:
         differences.append(number)
 
 print "# errors = ", differences    
