158ebe60642c0193c2d9294f30089009aab6cdd4
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-05-20 05:55:17 +0000

    Unit Tests: Single State Transitions

--- trunk/quex/core_engine/generator/TEST/test-single-state-transitions-y.py
+++ trunk/quex/core_engine/generator/TEST/test-single-state-transitions-y.py
@@ -56,7 +56,7 @@ for i in range(1000):
 languages.db["Python"]["$goto"] = lambda x, y: "return %s" % repr(y)   
 
 dsm = StateMachineDecorator(StateMachine(), "UnitTest", [], False, False)
-function = "def example_func(input):\n" + "".join(transition_block.do(state.transitions().get_trigger_map(), -1, False, dsm))
+function = "def example_func(input):\n" + "".join(transition_block.do(state.transitions().get_trigger_map(), None, False, dsm))
 function = function.replace("_-1_", "_MINUS_1_")
 
 #line_n = 0
