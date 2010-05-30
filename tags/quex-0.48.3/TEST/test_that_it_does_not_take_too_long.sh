1fc1dfbc01eb4f4ebcb0261069fefce3faadb98e
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-05-30 05:20:30 +0000

    Release 0.48.3

---
+++ tags/quex-0.48.3/TEST/test_that_it_does_not_take_too_long.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+#
+# $1 input file to quex
+#
+file=$1 # `pwd`/error.qx
+max_time=$2
+quex_application=$QUEX_PATH/quex-exe.py
+
+# Start the process _________________________________________________________________
+$quex_application -i $file &
+
+# Give it a couple of seconds _______________________________________________________
+time=0
+while (( $time < $max_time )); do
+    sleep 1  # Sleep one second
+    time=$(($time+1))
+    echo "## $time seconds"
+    process=`ps aux | grep $file | grep $quex_application`
+    if [[ -z $process ]]; then
+        echo "Oll Korrekt"
+        # Show the content of this directory
+        ls
+        rm lexer lexer.cpp lexer-token_ids lexer-configuration
+        exit
+    fi
+done
+
+# get the process id ________________________________________________________________
+echo "Error, generating process took to long"
+i=0
+for word in $process; do 
+    i=$((i+1))
+    if [[ $i == 2 ]]; then
+        process_id=$word
+        break;
+    fi
+done
+echo $process_id
+kill -15 $process_id
---
+++ tags/quex-0.48.3/doc/mindmap.mm
@@ -0,0 +1,126 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1234536159006" ID="Freemind_Link_1923209599" MODIFIED="1234536173208" TEXT="Quex Documentation">
+<node CREATED="1234536199057" ID="_" MODIFIED="1234536203686" POSITION="right" TEXT="Introduction">
+<node CREATED="1234536208763" ID="Freemind_Link_1477919346" MODIFIED="1234536231108" TEXT="Lexical Analysis (What)"/>
+<node CREATED="1234536267564" ID="Freemind_Link_1499368297" MODIFIED="1234536272487" TEXT="Motivation">
+<node CREATED="1234702943328" ID="Freemind_Link_874222312" MODIFIED="1234702948546" TEXT="Name of the Game"/>
+</node>
+<node CREATED="1234536236208" ID="Freemind_Link_36571826" MODIFIED="1234536266184" TEXT="Features of Quex">
+<node CREATED="1234536352116" ID="Freemind_Link_154933604" MODIFIED="1234536359558" TEXT="Generating Lexical Analysers"/>
+<node CREATED="1234536367802" ID="Freemind_Link_458380402" MODIFIED="1234536378122" TEXT="Generate State Machine Plots"/>
+</node>
+<node CREATED="1234536958987" ID="Freemind_Link_115425247" MODIFIED="1234536978033" TEXT="Installation"/>
+</node>
+<node CREATED="1234802356785" ID="Freemind_Link_664518655" MODIFIED="1234849166373" POSITION="right" TEXT="Concepts">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1234802363633" ID="Freemind_Link_81943491" MODIFIED="1234849162155" TEXT="Pattern-Action Pairs">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234802372142" ID="Freemind_Link_1918811577" MODIFIED="1234849171157" TEXT="Modes">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234802374870" ID="Freemind_Link_739174156" MODIFIED="1234849174325" TEXT="Event Handlers">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1234536297522" ID="Freemind_Link_1083441308" MODIFIED="1234849280917" POSITION="right" TEXT="Usage">
+<node CREATED="1234536527249" ID="Freemind_Link_1510982525" MODIFIED="1234849178822" TEXT="Minimalist Example">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234536405508" ID="Freemind_Link_1417286731" MODIFIED="1234850859909" TEXT="Patterns">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1234802529280" ID="Freemind_Link_1329991873" MODIFIED="1234850851317" TEXT="The pattern section">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234802502093" ID="Freemind_Link_456096713" MODIFIED="1234850855501" TEXT="Pattern Syntax">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1234849364977" ID="Freemind_Link_1341936574" MODIFIED="1234849369259" TEXT="Actions">
+<node CREATED="1234536466039" ID="Freemind_Link_732173939" MODIFIED="1234936568106" TEXT="Token ID Definitions">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1234802538812" ID="Freemind_Link_5308471" MODIFIED="1234936575026" TEXT="The token section">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234802546058" ID="Freemind_Link_418927031" MODIFIED="1234936592170" TEXT="External token-id definition">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1234802723407" ID="Freemind_Link_1870949265" MODIFIED="1235310773031" TEXT="The Token Stack">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1234536895359" ID="Freemind_Link_789617465" MODIFIED="1235290971444" TEXT="Shortcuts">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1234802442649" ID="Freemind_Link_1296899785" MODIFIED="1235290968012" TEXT="Token Sending Short Cuts">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234802454463" ID="Freemind_Link_483890134" MODIFIED="1235290975292" TEXT="Mode Transition Short Cuts">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1234536900598" ID="Freemind_Link_49173035" MODIFIED="1235297475802" TEXT="C/C++ Code">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1234536434066" ID="Freemind_Link_92294574" MODIFIED="1234536437192" TEXT="Modes">
+<node CREATED="1234703061638" ID="Freemind_Link_1148571035" MODIFIED="1235310763966" TEXT="Concept (Pattern Scope)">
+<edge WIDTH="thin"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234536504286" ID="Freemind_Link_1317280454" MODIFIED="1235310747095" TEXT="Options">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234851511311" ID="Freemind_Link_1821927613" MODIFIED="1235310756103" TEXT="Skippers">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234536495429" ID="Freemind_Link_667410840" MODIFIED="1235310779351" TEXT="Transitions">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234536725853" ID="Freemind_Link_1143153944" MODIFIED="1235310743263" TEXT="Events">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234536489062" ID="Freemind_Link_1403963919" MODIFIED="1235383493264" TEXT="Inheritance">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1234702979415" ID="Freemind_Link_311623292" MODIFIED="1235383483736" TEXT="Pattern Inheritance">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1234703031029" ID="Freemind_Link_1748195068" MODIFIED="1235383433616" TEXT="PRIORITY-MARK">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234703038358" ID="Freemind_Link_1500784948" MODIFIED="1235383455840" TEXT="DELETION">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1234703005575" ID="Freemind_Link_1468485606" MODIFIED="1235375414835" TEXT="Attribute Inheritance">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1234703016829" ID="Freemind_Link_1195468841" MODIFIED="1235375420211" TEXT="Handler Inheritance">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1234536566606" ID="Freemind_Link_411649199" MODIFIED="1234536573839" POSITION="right" TEXT="Character Encodings">
+<node CREATED="1234536578307" ID="Freemind_Link_88566471" MODIFIED="1234536594355" TEXT="The Analysing Process">
+<node CREATED="1234536595463" ID="Freemind_Link_1036612572" MODIFIED="1234536610549" TEXT="Explains: BufferFiller, Filler, and Analyser"/>
+</node>
+<node CREATED="1234536616256" ID="Freemind_Link_363208015" MODIFIED="1234536627563" TEXT="BufferFiller">
+<node CREATED="1234536633560" ID="Freemind_Link_1355593892" MODIFIED="1234536640103" TEXT="Converter: ICU"/>
+<node CREATED="1234536641025" ID="Freemind_Link_75071026" MODIFIED="1234536647818" TEXT="Converter: IConv"/>
+<node CREATED="1234536649132" ID="Freemind_Link_493049835" MODIFIED="1234536655301" TEXT="User defined Converters"/>
+</node>
+<node CREATED="1234536658556" ID="Freemind_Link_966317140" MODIFIED="1234536670045" TEXT="Engine Encoding"/>
+</node>
+<node CREATED="1234536680100" ID="Freemind_Link_117177678" MODIFIED="1234536685152" POSITION="right" TEXT="Advanced Features">
+<node CREATED="1234536689321" ID="Freemind_Link_1465952565" MODIFIED="1234536701128" TEXT="Buffer based analysers"/>
+<node CREATED="1234536702283" ID="Freemind_Link_553207768" MODIFIED="1234536708543" TEXT="Memory Management"/>
+<node CREATED="1234536710018" ID="Freemind_Link_567494029" MODIFIED="1234536716172" TEXT="Include Stacks"/>
+<node CREATED="1234536784567" ID="Freemind_Link_257062751" MODIFIED="1234536788493" TEXT="Token Stack"/>
+<node CREATED="1234536822881" ID="Freemind_Link_744668705" MODIFIED="1234536839613" TEXT="Tuning for Speed"/>
+<node CREATED="1234703128306" ID="Freemind_Link_1914313703" MODIFIED="1234703136064" TEXT="User Defined Token Class"/>
+<node CREATED="1234703137123" ID="Freemind_Link_617387967" MODIFIED="1234703145186" TEXT="User Defined Lexer Class"/>
+</node>
+<node CREATED="1234703209853" ID="Freemind_Link_1888311760" MODIFIED="1234703235852" POSITION="right" TEXT="Appendix A: Unicode Properties"/>
+<node CREATED="1234703236958" ID="Freemind_Link_1967212443" MODIFIED="1234703336469" POSITION="right" TEXT="Appendix B: The demo"/>
+</node>
+</map>
