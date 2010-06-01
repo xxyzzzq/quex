c45e83459d604246c8580a463268e0c50d7323d6
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-06-01 04:42:49 +0000

    Release 0.48.3d

---
+++ tags/quex-0.48.3d/demo/011/utf8-lexer.cpp
@@ -0,0 +1,30 @@
+#include<cstdio> 
+
+// (*) include lexical analyser header
+#include "UTF8Lex"
+
+using namespace std;
+
+int 
+main(int argc, char** argv) 
+{        
+    using namespace quex;
+
+    Token*   token;
+    UTF8Lex  qlex("example-utf8.txt");
+    
+
+    // (*) loop until the 'termination' token arrives
+    do {
+        // (*) get next token from the token stream
+        token = qlex.receive();
+
+        // (*) print out token information
+        printf("%s\t", (char*)token->type_id_name().c_str());
+        printf("%s\n", (char*)(token->get_text()).c_str());
+
+        // (*) check against 'termination'
+    } while( token->type_id() != TKN_TERMINATION );
+
+    return 0;
+}
---
+++ tags/quex-0.48.3d/demo/TEST/011.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+if [[ $1 == "--hwut-info" ]]; then
+    echo "demo/011: Engine Encoding (Example ISO8859-7)"
+    echo "CHOICES:  iso8859-7, utf8;"
+    echo "SAME;"
+    exit
+fi
+cd $QUEX_PATH/demo/011
+make clean >& /dev/null
+make $1-lexer >& tmp.txt
+cat tmp.txt | awk ' ! /g\+\+/ ' | awk '/[Ww][Aa][Rr][Nn][Ii][Nn][Gg]/ { print; } /[Ee][Rr][Rr][Oo][Rr]/ { print; }'
+rm tmp.txt
+valgrind --leak-check=full ./$1-lexer >& tmp.txt
+python ../TEST/show-valgrind.py
+rm -f tmp.txt
