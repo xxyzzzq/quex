2a2e9b18abadcb16bdf677ba0da6a5de209ae063
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-06-10 05:31:21 +0000

    Path Compression; Adding Uniformity Check;

--- trunk/quex/core_engine/state_machine/compression/TEST/paths-find_paths-2.py
+++ trunk/quex/core_engine/state_machine/compression/TEST/paths-find_paths-2.py
@@ -52,7 +52,7 @@ def test(Skeleton, AddTransitionList, *StringPaths):
     # print Skeleton
     print sm.get_graphviz_string(NormalizeF=False)
     print
-    result = paths.find_paths(sm)
+    result = paths.find_paths(sm, UniformityF=False)
     for path in result:
         print "# " + repr(path).replace("\n", "\n# ")
 
