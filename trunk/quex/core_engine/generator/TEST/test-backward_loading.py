15e2358e43b8b7a34222650e660216ade8d74f9e
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-06-14 06:23:18 +0000

    Code Generation Unit Tests

--- trunk/quex/core_engine/generator/TEST/test-backward_loading.py
+++ trunk/quex/core_engine/generator/TEST/test-backward_loading.py
@@ -4,17 +4,15 @@ import os
 sys.path.insert(0, os.environ["QUEX_PATH"])
 import generator_test
 
-if "--hwut-info" in sys.argv:
-    print "Buffer Reload: Backwards;"
-    print "CHOICES: ANSI-C-PlainMemory, ANSI-C, Cpp, Cpp_StrangeStream, Cpp-Template;"
-    print "SAME;"
-    sys.exit(0)
+choice = generator_test.hwut_input("Buffer Reload: Backwards",
+                                   "SAME;",
+                                   ["Cpp-ASSERTS", "ANSI-C-ASSERTS"])
 
-if sys.argv[1].find("ASSERTS") != -1: ASSERT_str = "-DQUEX_OPTION_ASSERTS"
-else:                                 ASSERT_str = ""
-if sys.argv[1].find("ANSI-C") != -1: BufferType = "ANSI-C"
-else:                                BufferType = "Cpp"
-
+if choice.find("-ASSERTS") != -1: 
+    choice     = choice.replace("-ASSERTS", "")
+    ASSERT_str = "-DQUEX_OPTION_ASSERTS"
+else: 
+    ASSERT_str = ""
 
 pattern_action_pair_list = [
     # keyword (needs to come before identifier, because otherwise they would be overruled by it.)
@@ -29,7 +27,7 @@ pattern_action_pair_list = [
 test_str = "   0xxxxxxalola 0xxxxxxxa"
 
 
-generator_test.do(pattern_action_pair_list, test_str, {}, Language=BufferType, 
+generator_test.do(pattern_action_pair_list, test_str, {}, Language=choice, 
                   QuexBufferSize=11, QuexBufferFallbackN=2, ShowBufferLoadsF=True,
                   AssertsActionvation_str=ASSERT_str)
     
--- trunk/quex/core_engine/generator/TEST/test-position_adaption.py
+++ trunk/quex/core_engine/generator/TEST/test-position_adaption.py
@@ -4,16 +4,16 @@ import os
 sys.path.append("../../../../")
 import generator_test
 
-if "--hwut-info" in sys.argv:
-    print "Buffer Reload: Forward Position Adaption"
-    print "CHOICES: Cpp_ASSERTS, Cpp, ANSI-C-ASSERTS, ANSI-C, Cpp_StrangeStream, Cpp-Template;"
-    print "SAME;"
-    exit(0)
+choice = generator_test.hwut_input("Buffer Reload: Forward Position Adaption", 
+                                   "SAME;",
+                                   ["Cpp-ASSERTS", "ANSI-C-ASSERTS"])
 
-if sys.argv[1].find("ASSERTS") != -1: ASSERT_str = "-DQUEX_OPTION_ASSERTS"
-else:                                 ASSERT_str = ""
-if sys.argv[1].find("ANSI-C") != -1: BufferType = "ANSI-C"
-else:                                BufferType = "Cpp"
+if choice.find("-ASSERTS") != -1: 
+    choice     = choice.replace("-ASSERTS", "")
+    ASSERT_str = "-DQUEX_OPTION_ASSERTS"
+else: 
+    ASSERT_str = ""
+
 
 pattern_action_pair_list = [
     # keyword (needs to come before identifier, because otherwise they would be overruled by it.)
@@ -55,6 +55,6 @@ A
 XXXXXXXXXA
 """
 
-generator_test.do(pattern_action_pair_list, test_str, {}, BufferType,
+generator_test.do(pattern_action_pair_list, test_str, {}, choice,
                   AssertsActionvation_str=ASSERT_str)
     
