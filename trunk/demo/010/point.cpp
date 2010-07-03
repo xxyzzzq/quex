da0587e1153b5ba3142e50d7f437476c6af9899d
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-07-03 06:27:33 +0000

    Unit Tests

--- trunk/demo/010/point.cpp
+++ trunk/demo/010/point.cpp
@@ -10,32 +10,36 @@ main(int argc, char** argv)
 {        
     using namespace std;
 
-    quex::Token*       token = (quex::Token*)0x0;
+    quex::Token        token;           
     quex::tiny_lexer   qlex(MESSAGING_FRAMEWORK_BUFFER, 
                             MESSAGING_FRAMEWORK_BUFFER_SIZE,
                             MESSAGING_FRAMEWORK_BUFFER + 1); 
 
-    // -- Call the low lever driver to fill the fill region
-    size_t receive_n = messaging_framework_receive_to_internal_buffer();
+    // Iterate 3 times doing the same thing in order to illustrate
+    // the repeated activation of the same chunk of memory.
+    for(int i = 0; i < 3; ++i ) {
+        // -- Call the low lever driver to fill the fill region
+        size_t receive_n = messaging_framework_receive_to_internal_buffer();
 
-    // -- Inform the buffer about the number of loaded characters NOT NUMBER OF BYTES!
-    qlex.buffer_fill_region_finish(receive_n-1);
-    // -- Each time the buffer is filled, the input pointer must be reset
-    //    (Here, it is just to display the principle, ...)
-    qlex.buffer_input_pointer_set(MESSAGING_FRAMEWORK_BUFFER_SIZE + 1);
+        // -- Inform the buffer about the number of loaded characters NOT NUMBER OF BYTES!
+        qlex.buffer_fill_region_finish(receive_n-1);
+        // -- Each time the buffer is filled, the input pointer must be reset
+        //    (Here, it is just to display the principle, ...)
+        qlex.buffer_input_pointer_set(MESSAGING_FRAMEWORK_BUFFER + 1);
 
-    // -- Loop until the 'termination' token arrives
-    token = qlex.token_p();
-    do {
-        qlex.receive();
+        // -- Loop until the 'termination' token arrives
+        (void)qlex.token_p_switch(&token);
+        do {
+            qlex.receive();
 
-        if( token->type_id() == QUEX_TKN_BYE ) 
-            cout << "## ";
+            if( token.type_id() != QUEX_TKN_TERMINATION )
+                cout << "Consider: " << string(token) << endl;
 
-        if( token->type_id() != QUEX_TKN_TERMINATION )
-            cout << "Consider: " << string(*token) << endl;
-        
-    } while( token->type_id() != QUEX_TKN_TERMINATION );
+            if( token.type_id() == QUEX_TKN_BYE ) 
+                cout << "##\n";
+
+        } while( token.type_id() != QUEX_TKN_TERMINATION );
+    }
 
     return 0;
 }
