2f724f9a77bb78fd060f20be1d646a1efa4b2712
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-08-04 05:07:36 +0000

    Feature 3039173

--- trunk/TEST/feature-3039173.sh
+++ trunk/TEST/feature-3039173.sh
@@ -8,9 +8,11 @@ fi
 tmp=`pwd`
 cd $bug/ 
 
-quex -i simple.qx -o Simple
-g++ ../lexer.cpp Simple.cpp -I. -I$QUEX_PATH -o lexer
-./lexer example.txt 
+quex -i simple.qx -o Simple --language C
+gcc ../lexer.c Simple.c -I. -I$QUEX_PATH -o lexer
+./lexer example.txt 2> tmp.txt
+cat tmp.txt
+rm -f tmp.txt
 rm -f Simple* 
 rm -f lexer
 
