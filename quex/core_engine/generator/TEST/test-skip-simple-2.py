06c016141dfaea844d322a3311b02fb2271a5bab
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-08-16 06:36:07 +0000

    Indentation Handling

--- quex/core_engine/generator/TEST/test-skip-simple-2.py
+++ quex/core_engine/generator/TEST/test-skip-simple-2.py
@@ -2,8 +2,7 @@
 import sys
 import os
 sys.path.insert(0, os.environ["QUEX_PATH"])
-from   generator_test import create_main_function, \
-                             create_skipper_code, \
+from   generator_test import create_range_skipper_code, \
                              compile_and_run
 
 if "--hwut-info" in sys.argv:
@@ -30,14 +29,14 @@ end_sequence = map(ord, SEP)
 
 TestStr  = "abcdefg" + SEP + "hijklmnop" + SEP + "qrstuvw" + SEP + "xyz" + SEP + "ok"
 
-compile_and_run(Language, create_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True))
+compile_and_run(Language, create_range_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True))
 
 TestStr  = SEP + "hijklmnop" + SEP + "qrstuvw" + SEP + "xyz" + SEP
 
-compile_and_run(Language, create_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True))
+compile_and_run(Language, create_range_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True))
 
 TestStr  = "a" + SEP + "h" + SEP + SEP + SEP
 
-compile_and_run(Language, create_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True))
+compile_and_run(Language, create_range_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True))
 
 
