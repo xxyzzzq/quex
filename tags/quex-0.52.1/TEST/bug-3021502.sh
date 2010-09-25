5959cbc98c999b7d1ce3b294fc138b28fd95d00a
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-09-25 07:39:25 +0000

    Release 0.52.1

---
+++ tags/quex-0.52.1/TEST/bug-3021502.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+bug=3021502
+if [[ $1 == "--hwut-info" ]]; then
+    echo "the-user-k: $bug 0.49.2 Buffer-size gets ignored"
+    exit
+fi
+
+tmp=`pwd`
+cd $bug/ 
+make  >& tmp.txt
+cat tmp.txt | awk '(/[Ww][Aa][Rr][Nn][Ii][Nn][Gg]/ || /[Ee][Rr][Rr][Oo][Rr]/) && ! /ASSERTS/ '
+./a.out
+
+# cleansening
+make clean >& /dev/null
+cd $tmp
---
+++ tags/quex-0.52.1/demo/Cpp/010/point.cpp
@@ -0,0 +1,46 @@
+#include<fstream>    
+#include<iostream> 
+#include<sstream> 
+
+#include "tiny_lexer"
+#include "messaging-framework.h"
+
+int 
+main(int argc, char** argv) 
+{        
+    using namespace std;
+
+    quex::Token        token;           
+    quex::tiny_lexer   qlex(MESSAGING_FRAMEWORK_BUFFER, 
+                            MESSAGING_FRAMEWORK_BUFFER_SIZE,
+                            MESSAGING_FRAMEWORK_BUFFER + 1); 
+
+    // Iterate 3 times doing the same thing in order to illustrate
+    // the repeated activation of the same chunk of memory.
+    for(int i = 0; i < 3; ++i ) {
+        // -- Call the low lever driver to fill the fill region
+        size_t receive_n = messaging_framework_receive_to_internal_buffer();
+
+        // -- Inform the buffer about the number of loaded characters NOT NUMBER OF BYTES!
+        qlex.buffer_fill_region_finish(receive_n-1);
+        // -- Each time the buffer is filled, the input pointer must be reset
+        //    (Here, it is just to display the principle, ...)
+        qlex.buffer_input_pointer_set(MESSAGING_FRAMEWORK_BUFFER + 1);
+
+        // -- Loop until the 'termination' token arrives
+        (void)qlex.token_p_switch(&token);
+        do {
+            qlex.receive();
+
+            if( token.type_id() != QUEX_TKN_TERMINATION )
+                cout << "Consider: " << string(token) << endl;
+
+            if( token.type_id() == QUEX_TKN_BYE ) 
+                cout << "##\n";
+
+        } while( token.type_id() != QUEX_TKN_TERMINATION );
+    }
+
+    return 0;
+}
+
