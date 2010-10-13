19b0252f743fc2f88e61cd2f36ebd3b6e53dc0f9
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-10-13 05:25:15 +0000

    Unit Tests Feature 3079986

--- trunk/TEST/feature-3079986.sh
+++ trunk/TEST/feature-3079986.sh
@@ -2,14 +2,20 @@
 bug=3079986
 if [[ $1 == "--hwut-info" ]]; then
     echo "rmanoj-oss: $bug Allow namespace delimiter in --token-prefix for C++"
+    echo "CHOICES: nnn, nnc, ncc, ccc;"
+    echo "SAME;"
     exit
 fi
 
 tmp=`pwd`
 cd $bug/ 
 
+testcase=$1
+
+sed "s/Parserbase-XXX/Parserbase-$testcase/" simple-XXX.qx > simple.qx
+
 echo "(0) quexify -- no output is good output"
-quex -i simple.qx --token-prefix ParserBase::TKN_ --foreign-token-id-file Parserbase.h >& tmp.txt
+quex -i simple.qx --token-prefix scope1::scope2::scope3::TKN_ --foreign-token-id-file Parserbase-$testcase.h >& tmp.txt
 cat tmp.txt
 
 echo "(1) check out generated sources"
@@ -20,9 +26,9 @@ echo "(2) compile -- no output is good output"
 g++ -I$QUEX_PATH -c lexer.cpp -I. -Wall -Wconversion >& tmp.txt
 cat tmp.txt
 
-rm tmp.txt
-
-# cleansening
+# cleanup
+rm -f simple.qx tmp.txt
 rm -rf lexer*
 rm -rf tmp.txt
+
 cd $tmp
