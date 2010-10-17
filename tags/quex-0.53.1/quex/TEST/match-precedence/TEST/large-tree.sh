be7d5bbe8a6ef39fb4fdbde30100cf6ca8c09ddb
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-10-17 12:37:11 +0000

    Release 0.53.1

---
+++ tags/quex-0.53.1/quex/TEST/match-precedence/TEST/large-tree.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+if [[ $1 == "--hwut-info" ]]; then
+    echo "Precedence: Larger Inheritance Tree;"
+    exit
+fi
+quex -i data/large-tree.qx -o Simple --token-prefix T_
+g++ -I$QUEX_PATH -I. \
+    -DQUEX_OPTION_ASSERTS_WARNING_MESSAGE_DISABLED \
+    $QUEX_PATH/TEST/lexer.cpp Simple.cpp -o lexer  \
+    -DQUEX_TKN_TERMINATION=T_TERMINATION
+./lexer data/large-tree.txt
+rm -f ./lexer
+rm -f Simple*
