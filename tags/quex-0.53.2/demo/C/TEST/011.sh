63512171cdb192967391b453a3afb42d393a7a6c
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-10-17 14:07:33 +0000

    Release 0.53.2

---
+++ tags/quex-0.53.2/demo/C/TEST/011.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+if [[ $1 == "--hwut-info" ]]; then
+    echo "demo/011: Engine Encoding (Example ISO8859-7)"
+    echo "CHOICES:  iso8859-7, utf8;"
+    echo "SAME;"
+    exit
+fi
+cd ../011
+
+make clean >& /dev/null
+make $1-lexer >& tmp.txt
+cat tmp.txt | awk ' ! /g\+\+/ ' | awk '/[Ww][Aa][Rr][Nn][Ii][Nn][Gg]/ { print; } /[Ee][Rr][Rr][Oo][Rr]/ { print; }'
+rm tmp.txt
+valgrind --leak-check=full ./$1-lexer >& tmp.txt
+python $QUEX_PATH/TEST/show-valgrind.py
+rm -f tmp.txt
---
+++ tags/quex-0.53.2/quex/core_engine/state_machine/transformation.py
@@ -0,0 +1,83 @@
+# (C) 2009 Frank-Rene Schaefer
+import os
+import sys
+import codecs
+from copy import copy
+import quex.core_engine.state_machine.utf8_state_split  as utf8_state_split
+import quex.core_engine.state_machine.utf16_state_split as utf16_state_split
+from   quex.frs_py.file_in import error_msg
+from   quex.input.setup import setup as Setup
+
+sys.path.append(os.environ["QUEX_PATH"])
+
+def do(X, TrafoInfo=None, FH=-1, LineN=None):
+    """Transforms a given state machine from 'Unicode Driven' to another
+       character encoding type.
+    
+       RETURNS: True  transformation successful
+                False transformation failed, number set possibly in inconsistent state!
+
+       X = state machine or number set
+    """
+    assert X.__class__.__name__ in ["StateMachine", "NumberSet"]
+
+    if TrafoInfo == None:
+        TrafoInfo = Setup.engine_character_encoding_transformation_info
+        if TrafoInfo == None: return X
+
+    assert TrafoInfo != None
+    if X.__class__.__name__ == "NumberSet": return do_set(X, TrafoInfo, FH, LineN)
+
+    if type(TrafoInfo) == str:
+        if TrafoInfo == "utf8-state-split": 
+            return __split(X, utf8_state_split)
+
+        elif TrafoInfo == "utf16-state-split":
+            return __split(X, utf16_state_split)
+
+        # Other transformations are not supported
+        assert False
+    
+    # Pre-condition SM is transformed inside the member function
+    return X.transform(TrafoInfo)
+        
+def __split(sm, splitter_module):
+    """sm              -- the state machine of which the state shall be split.
+       splitter_module -- the mode with the 'do' function that 
+                          performs the state split.
+    """
+    result = splitter_module.do(sm)
+    pre_sm = sm.core().pre_context_sm()
+    if pre_sm == None: return result
+
+    # There is a pre-context, state machine needs to be adapted
+    new_pre_sm = splitter_module.do(pre_sm)
+    result.core().set_pre_context_sm(new_pre_sm)
+
+    # Adapt all origins that depend on the old pre-context to the new context
+    for state in result.states.values():
+        for origin in state.origins().get_list():
+            if origin.pre_context_id() != pre_sm.core().get_id(): continue
+            origin.set_pre_context_id(new_pre_sm.get_id())
+    return result
+
+def do_set(number_set, TrafoInfo, FH=-1, LineN=None):
+    """RETURNS: True  transformation successful
+                False transformation failed, number set possibly in inconsistent state!
+    """
+    assert TrafoInfo != None
+    assert TrafoInfo.__class__.__name__ == "NumberSet"
+
+    if type(TrafoInfo) == str:
+        if TrafoInfo == "utf8-state-split": 
+            result = utf8_state_split.do_set(number_set)
+            if result == None:
+                error_msg("Operation 'number set transformation' failed 'utf8'.\n" + \
+                          "The given number set results in a state sequence not a single transition.", FH, LineN) 
+            return result
+        # Other transformations are not supported
+        assert False
+    
+    return number_set.transform(TrafoInfo)
+        
+
