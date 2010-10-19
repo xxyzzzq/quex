46a35f32112608a78474ae4954071b4cc3866162
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-10-19 04:33:07 +0000

    Release 0.53.3

---
+++ tags/quex-0.53.3/TEST/feature-3079986.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+bug=3079986
+if [[ $1 == "--hwut-info" ]]; then
+    echo "rmanoj-oss: $bug Allow namespace delimiter in --token-prefix for C++"
+    echo "CHOICES: nnn, nnc, ncc, ccc;"
+    echo "SAME;"
+    exit
+fi
+
+tmp=`pwd`
+cd $bug/ 
+
+testcase=$1
+
+sed "s/Parserbase-XXX/Parserbase-$testcase/" simple-XXX.qx > simple.qx
+
+echo "(0) quexify -- no output is good output"
+quex -i simple.qx --token-prefix scope1::scope2::scope3::TKN_ --foreign-token-id-file Parserbase-$testcase.h >& tmp.txt
+cat tmp.txt
+
+echo "(1) check out generated sources"
+awk ' /TKN_/ { print; }' lexer.cpp >& tmp.txt
+cat tmp.txt
+
+echo "(2) compile -- no output is good output"
+g++ -I$QUEX_PATH -c lexer.cpp -I. -Wall -Wconversion >& tmp.txt
+cat tmp.txt
+
+# cleanup
+rm -f simple.qx tmp.txt
+rm -rf lexer*
+rm -rf tmp.txt
+
+cd $tmp
---
+++ tags/quex-0.53.3/demo/C/012/lexer.c
@@ -0,0 +1,70 @@
+#include <stdio.h> 
+
+#include "moritz_Lexer.h"
+#include "moritz_Lexer-token.c"
+#include "max_Lexer.h"
+#include "max_Lexer-token.c"
+#include "boeck_Lexer.h"
+#include "boeck_Lexer-token.c"
+
+int 
+main(int argc, char** argv) 
+{        
+    /* we want to have error outputs in stdout, so that the unit test could see it.  */
+    max_Lexer     max_lex;
+    moritz_Lexer  moritz_lex;
+    boeck_Lexer   boeck_lex;
+    max_Token*    max_token    = 0x0;
+    moritz_Token* moritz_token = 0x0;
+    boeck_Token*  boeck_token  = 0x0;
+
+    const size_t BufferSize = 1024;
+    char         buffer[1024];
+    char*        iterator = 0x0;
+    size_t       i = 0;
+
+    max_Lexer_construct_file_name(&max_lex,       "example-utf16.txt", "UTF16", false);
+    moritz_Lexer_construct_file_name(&moritz_lex, "example-ucs2.txt",  "UCS-2", false);
+    boeck_Lexer_construct_file_name(&boeck_lex,   "example-utf8.txt",  0x0,     false);
+
+    /* Each lexer reads one token, since the grammars are similar the lexeme 
+     * is always the same.                                                           */
+    printf("                Max:        Moritz:      Boeck:\n");
+
+    max_token    = max_Lexer_token_p(&max_lex);
+    moritz_token = moritz_Lexer_token_p(&moritz_lex);
+    boeck_token  = boeck_Lexer_token_p(&boeck_lex);
+
+    do {
+        (void)max_Lexer_receive(&max_lex);
+        (void)moritz_Lexer_receive(&moritz_lex);
+        (void)boeck_Lexer_receive(&boeck_lex);
+
+        /* Lexeme is same for all three. */
+        iterator = (char*)boeck_Lexer_utf8_to_utf8_string(boeck_token->text, 
+                                                          boeck_Lexer_strlen(boeck_token->text),
+                                                          (uint8_t*)buffer,
+                                                          BufferSize);
+        *iterator = '\0';
+        const char* lexeme = (const char*)buffer;
+        size_t      preL   = (size_t)strlen(lexeme);
+        size_t      L      = preL < 10 ? preL : 10;
+        printf("%s", lexeme);
+
+        for(i=0; i < 10 - L ; ++i) printf(" ");
+
+        printf("\t");
+        printf("%s   %s   %s\n", 
+               max_Token_map_id_to_name(max_token->_id),
+               moritz_Token_map_id_to_name(moritz_token->_id),
+               boeck_Token_map_id_to_name(boeck_token->_id));
+
+    } while( boeck_token->_id != TKN_TERMINATION );
+
+    boeck_Lexer_destruct(&boeck_lex);
+    max_Lexer_destruct(&max_lex);
+    moritz_Lexer_destruct(&moritz_lex);
+
+    return 0;
+}
+
