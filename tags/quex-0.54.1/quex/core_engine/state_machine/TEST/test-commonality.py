55ea98ce34aee21baebe99f725af651e9c8a1995
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-10-25 05:41:43 +0000

    Release 0.54.1

---
+++ tags/quex-0.54.1/quex/core_engine/state_machine/TEST/test-commonality.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+import sys
+import os
+sys.path.insert(0, os.environ["QUEX_PATH"])
+
+import quex.core_engine.regular_expression.core           as regex
+import quex.core_engine.state_machine.commonality_checker as commonality_checker
+
+if "--hwut-info" in sys.argv:
+    print "Commonality Checker"
+    # print "CHOICES: True, False, Pre-Post-Contexts-True, Pre-Post-Contexts-False;"
+    sys.exit(0)
+    
+def test(A, B):
+    def __core(Pattern0, Pattern1):
+        print ("Pattern A = " + Pattern0).replace("\n", "\\n").replace("\t", "\\t")
+        print ("Pattern B = " + Pattern1).replace("\n", "\\n").replace("\t", "\\t")
+        sm0 = regex.do(Pattern0, {})
+        sm1 = regex.do(Pattern1, {})
+        print "claim     = ", commonality_checker.name[commonality_checker.do(sm0, sm1)]
+    print "---------------------------"
+    __core(A, B)
+    print
+    __core(B, A)
+
+test("b", "ab")
+test("a", "a")
+test("a", "ab")
+test("a", "a{5}")
+test("albert", "a(de)?lbert")
+test("(alb)|(er)", "albert")
+test("(alb)+|(er)", "albert")
+test("(alfons)|(adelheid)|(adolf)|(arthur)|(arnheim)|(augsburg)|(frieda)", "albert")
+test("(alfons)|(adelheid)|(adolf)|(arthur)|(arnheim)|(augsburg)|(frieda)", "arthurius")
+test("(a+lfons)|(a{2}delheid)|(a+dolf)|(a+r+t+h{1,3}ur)|(a+r+n+heim)|(a{5,}ugsburg)|(f+rieda)", "arthurius")
