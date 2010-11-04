4919b7fe2447526da05c4c5ee4cd0daabbe4ea70
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-11-04 19:56:53 +0000

    Converter Unit Tests

--- quex/TEST/converter_helper/TEST/test.cpp
+++ quex/TEST/converter_helper/TEST/test.cpp
@@ -4,35 +4,60 @@
 #define ____QUEX_CONVERTER_HELPER(A, B) Testy_from_ ## A ## _to_ ## B
 #define __QUEX_CONVERTER_HELPER(A, B)   ____QUEX_CONVERTER_HELPER(A, B)
 
-#define QUEX_TYPE_CHARACTER   uint32_t
-
 #if   defined(TEST_UTF8)
 #  include <quex/code_base/converter_helper/utf8>
 #  include <quex/code_base/converter_helper/utf8.i>
-#  define  CODEC utf8
+#  define  FROM_CODEC           utf8
+#  define  QUEX_TYPE_CHARACTER  uint16_t
 #elif defined(TEST_UTF16)
 #  include <quex/code_base/converter_helper/utf16>
 #  include <quex/code_base/converter_helper/utf16.i>
-#  define  CODEC utf16
+#  define  FROM_CODEC           utf16
+#  define  QUEX_TYPE_CHARACTER  uint16_t
 #elif defined(TEST_UTF16)
 #elif defined(TEST_UNICODE)
 #  include <quex/code_base/converter_helper/unicode>
 #  include <quex/code_base/converter_helper/unicode.i>
-#  define  CODEC unicode
+#  define  FROM_CODEC           unicode
+#  define  QUEX_TYPE_CHARACTER  uint32_t
 #elif defined(TEST_CODEC)
 #else
 #  error "No test codec defined."
 #endif
 
-#define CONVERTER(OUTPUT)   Tester::__QUEX_CONVERTER_HELPER(CODEC, OUTPUT)
+#define CONVERTER(OUTPUT)   Tester::__QUEX_CONVERTER_HELPER(FROM_CODEC, OUTPUT)
 
 using namespace std;
 #include <iostream>
 
-template <class DrainT> void
-check_output(const DrainT* Drain, const DrainT* DrainEnd, const DrainT* ref_iterator)
+void
+check_wchar_output(const wchar_t* Drain, const wchar_t* DrainEnd, 
+                   const wchar_t* ref_iterator);
+
+void
+check_utf8_output(const uint8_t* Drain, const uint8_t* DrainEnd, 
+                  const uint8_t* ref_iterator);
+
+void
+check_utf8_output(const uint8_t* Drain, const uint8_t* DrainEnd, const uint8_t* ref_iterator)
 {
-    const DrainT* iterator = Drain;
+    const uint8_t* iterator = Drain;
+
+    if( iterator > DrainEnd ) { cout << "Drain Distorted\n"; return; }
+
+    for(; iterator != DrainEnd; ++iterator) {
+        if( *ref_iterator != *iterator ) { 
+            cout << "At element " << (size_t)(iterator - Drain) << ": ERROR!\n"; 
+            return; 
+        }
+    }
+    cout << "Checked " << (size_t)(iterator - Drain) << " elements: GOOD!\n"; 
+}
+
+void
+check_wchar_output(const wchar_t* Drain, const wchar_t* DrainEnd, const wchar_t* ref_iterator)
+{
+    const wchar_t* iterator = Drain;
 
     if( iterator > DrainEnd ) { cout << "Drain Distorted\n"; return; }
 
@@ -46,11 +71,11 @@ check_output(const DrainT* Drain, const DrainT* DrainEnd, const DrainT* ref_iter
 }
 
 void 
-output_utf8_string(const char*                 TestName, 
-                   const QUEX_TYPE_CHARACTER*  source_p,
-                   const QUEX_TYPE_CHARACTER*  SourceEnd,
-                   size_t                      DrainSize,
-                   const uint8_t*              reference)
+convert_utf8(const char*                 TestName, 
+             const QUEX_TYPE_CHARACTER*  source_p,
+             const QUEX_TYPE_CHARACTER*  SourceEnd,
+             size_t                      DrainSize,
+             const uint8_t*              reference)
 {
     cout << TestName << "____________________________________________________\n";
     cout << endl;
@@ -61,7 +86,7 @@ output_utf8_string(const char*                 TestName,
         const uint8_t*   DrainEnd = drain + DrainSize;
 
         CONVERTER(utf8_string)(&source_p, SourceEnd, &drain_p,  DrainEnd);
-        check_output(drain, drain_p, reference);
+        check_utf8_output(drain, drain_p, reference);
         delete drain;
     }
 
@@ -71,16 +96,8 @@ output_utf8_string(const char*                 TestName,
         std::string                             drain;
 
         drain = CONVERTER(utf8_string)(source);
-        check_output<char>(drain.c_str(), drain.c_str() + drain.length(), (const char*)reference);
+        check_wchar_output<char>(drain.c_str(), drain.c_str() + drain.length(), (const char*)reference);
     }
     cout << endl;
 }
 
-int
-main(int argc, char** argv)
-{
-    {
-        QUEX_TYPE_CHARACTER*  source = 0x0;
-        output_utf8_string("Empty Source", source, source, 1024, 0x0);
-    }
-}
