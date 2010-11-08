9e766a3e6b31a86872422c47c94561a0696ace12
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-11-08 20:43:27 +0000

    Converter Helper

--- quex/TEST/converter_helper/TEST/test-utf8.cpp
+++ quex/TEST/converter_helper/TEST/test-utf8.cpp
@@ -6,7 +6,7 @@
 #include <cassert>
 
 void
-test_this(const char* Name, 
+test_this(const char*                  Name, 
           const QUEX_TYPE_CHARACTER*   Source,
           const uint8_t*               UTF8_Expected,
           const size_t                 UTF8_DrainSize,
@@ -23,21 +23,11 @@ test_this(const char* Name,
 int
 main(int argc, char** argv)
 {
-    hwut_info("UTF8 to utf8 and wchar_t.;n"
-              "CHOICES: Normal, Source_Empty, Source_TestFile, Drain_ToSmall;n"); // Source_Incomplete
+    hwut_info("UTF8 to utf8 and wchar_t.;\n"
+              "CHOICES: Normal, Source_Empty, Source_TestFile, Drain_ToSmall;\n"); // Source_Incomplete
 
     assert( sizeof(QUEX_TYPE_CHARACTER) == 1 );
     
-#   if 0
-    hwut_if_choice("Normal") {
-        test_this(/* NAME         */ argv[1],
-                  /* SOURCE       */ (0xe4, 0x9c, 0x91 /* Unicode 0x4711 */, 0x0),
-                  /* REFERENCE    */ (0xe4, 0x9c, 0x91 /* Unicode 0x4711 */, 0x0),
-                  /* DRAIN_SIZE   */ 1024,
-                  /* W_REFERENCE  */ (0x4711, 0x0),
-                  /* W_DRAIN_SIZE */ 1024);
-    }
-#   endif
     hwut_if_choice("Normal") {
         QUEX_TYPE_CHARACTER  source[]         = { 0xe4, 0x9c, 0x91 /* Unicode 0x4711 */, 0x0 }; 
         uint8_t              utf8_expected[]  = { 0xe4, 0x9c, 0x91 /* Unicode 0x4711 */, 0x0 }; 
@@ -46,53 +36,38 @@ main(int argc, char** argv)
         test_this(argv[1], source, utf8_expected, 1024, wchar_expected, 1024);
     }
     hwut_if_choice("Source_Empty") {
-        QUEX_TYPE_CHARACTER  source = { 0x0 };
-        {
-            uint8_t  reference = { 0x0 };
-            test_utf8_string(argv[1], &source, &source, 1024, &reference);
-        }
-        {
-            wchar_t  wreference = { 0x0 };
-            test_wstring(argv[1], &source, &source, 1024, &wreference);
-        }
+        QUEX_TYPE_CHARACTER  source[]         = { 0x0 }; 
+        uint8_t              utf8_expected[]  = { 0x0 }; 
+        wchar_t              wchar_expected[] = { 0x0 };
+
+        test_this(argv[1], source, utf8_expected, 1024, wchar_expected, 1024);
     }
     hwut_if_choice("Source_Incomplete") {
-        QUEX_TYPE_CHARACTER  source[] = { 0xe4, 0x9c, /* 0x91 (Unicode 0x4711) */ 0x0, 0x0, 0x0, 0x0 }; 
-        {
-            uint8_t  reference[] = { 0xe1, 0x89, 0x0, };
-            test_utf8_string(argv[1], source, source + 2, 1024, reference);
-        }
-        {
-            wchar_t  wreference[] = { 0x4711, 0x0 };
-            test_wstring(argv[1], source, source + 2, 1024, wreference);
-        }
+        QUEX_TYPE_CHARACTER  source[]         = { 0xe4, 0x9c, /* 0x91 (Unicode 0x4711) */ 0x0, 0x0, 0x0, 0x0 }; 
+        uint8_t              utf8_expected[]  = { 0xe1, 0x89, 0x0, };
+        wchar_t              wchar_expected[] = { 0x4711, 0x0 };
+
+        test_this(argv[1], source, utf8_expected, 1024, wchar_expected, 1024);
     }
     hwut_if_choice("Drain_ToSmall") {
         QUEX_TYPE_CHARACTER  source[] = { 0xe4, 0x9c, 0x91, /* unicode 0x4711 */ 
                                           0xe0, 0xa0, 0x95, /* unicode 0x0815 */
                                           0x0 }; 
-        {
-            uint8_t  reference[] = { 0xe4, 0x9c, 0x91 /* unicode 0x4711 */, 0x0 }; 
-            test_utf8_string(argv[1], source, source + 6, 4, reference);
-        }
-        {
-            wchar_t  wreference[] = { 0x4711, 0x0 };
-            test_wstring(argv[1], source, source + 6, 1, wreference);
-        }
+        uint8_t              utf8_expected[]  = { 0xe4, 0x9c, 0x91 /* unicode 0x4711 */, 0x0 };
+        wchar_t              wchar_expected[] = { 0x4711, 0x0 };
+
+        test_this(argv[1], source, utf8_expected, 4, wchar_expected, 1);
     }
     hwut_if_choice("Source_TestFile") {
         QUEX_TYPE_CHARACTER  source[65536];
         QUEX_TYPE_CHARACTER* source_end = read_from_file(source, 65536, "example/utf8.txt");
-        {
-            uint8_t  reference[65536];
-            memcpy(reference, source, (source_end - source));
-            test_utf8_string(argv[1], source, source_end, 65536, reference);
-        }
-        {
-            wchar_t  wreference[65536];
-            read_from_file(wreference, 65536, "example/ucs4le.txt");
-            test_wstring(argv[1], source, source_end, 65536, wreference);
-        }
+        uint8_t              utf8_expected[65536];
+        wchar_t              wchar_expected[65536];
+
+        memcpy(utf8_expected, source, (source_end - source));
+        read_from_file(wchar_expected, 65536, "example/ucs4le.txt");
+
+        test_this(argv[1], source, utf8_expected, 65536, wchar_expected, 65536);
     }
 }
 
