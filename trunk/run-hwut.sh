88c3cd3df1b30cf8e56fea7528046073d6ead611
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-01-19 22:04:46 +0000

    Prepare Release 0.57.2

--- trunk/run-hwut.sh
+++ trunk/run-hwut.sh
@@ -1,9 +1,12 @@
 cd $QUEX_PATH/TEST
 hwut >& $QUEX_PATH/hwut-output-1.txt &
 
-cd $QUEX_PATH/demo/TEST
+cd $QUEX_PATH/demo/Cpp/TEST
 hwut >& $QUEX_PATH/hwut-output-2.txt &
 
+cd $QUEX_PATH/demo/C/TEST
+hwut >& $QUEX_PATH/hwut-output-3.txt &
+
 cd $QUEX_PATH/quex
-hwut >& $QUEX_PATH/hwut-output-3.txt
+hwut >& $QUEX_PATH/hwut-output-4.txt
 
