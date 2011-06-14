f1ee51349ee9fe75a39b9af96bd8da6b7ead5ff5
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-06-14 06:19:48 +0000

    Track Analysis

--- trunk/quex/engine/analyzer/TEST/track_analysis-pre-post.py
+++ trunk/quex/engine/analyzer/TEST/track_analysis-pre-post.py
@@ -95,7 +95,7 @@ state_machine_list = map(lambda x: regex.do(x, {}), pattern_list)
 
 sm  = get_combined_state_machine(state_machine_list, False) # May be 'True' later.
 
-if False:
+if True:
     fh = open("tmp.dot", "wb")
     fh.write( sm.get_graphviz_string() )
     fh.close()
