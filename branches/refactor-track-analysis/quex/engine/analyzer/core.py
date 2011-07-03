0723380b20b0659e9ecb22249e652987dc1b154c
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-07-03 05:29:16 +0000

    Daily Bump

--- branches/refactor-track-analysis/quex/engine/analyzer/core.py
+++ branches/refactor-track-analysis/quex/engine/analyzer/core.py
@@ -313,126 +313,29 @@ class Analyzer:
         # Checks (1), (2), and (3) did not find anything 'bad' --> uniform.
         return True
 
-    def get_position_register_map(self):
-        """RETURNS: 
-        
-           A dictionary that maps:
+    def _get_storing_state_set(self, PostContextID):
+        result = defaultdict(set)
+        for state in self.state_db.itervalues():
+            if state.entry.positioner.has_key(PostContextID):
+                result[PostContextID].add(state.index)
+        return result
 
-                   post-context-id --> position register index
-
-           where post-context-id == PostContextIDs.NONE means
-           'last_acceptance_position'.  The position register index starts from
-           0 and ends with N, where N-1 is the number of required position
-           registers. It can directly be used as index into an array of
-           positions.
-
-           -----------------------------------------------------------------------
-        
-           Under some circumstances it is necessary to store the acceptance
-           position or the position in the input stream where a post context
-           begins. For this an array of positions is necessary, e.g.
-
-               QUEX_POSITION_LABEL     positions[4];
-
-           where the last acceptance input position or the input position of
-           post contexts may be stored. The paths of a state machine, though,
-           may allow to store multiple positions in one array location, because
-           they never interact. 
-           
-           For example:                   . b .
-                                         /     \ 
-                                         \     /
-                    .-- a -->( 1 )-- b -->( 2 )-- c -->((3))
-                   /            S47                       R47 
-               ( 0 )
-                   \            S11                       R11
-                    '-- b -->( 4 )-- c -->( 5 )-- d -->((6))
-                                 \                     /
-                                  '-------- e --------'
-
-           The input position needs to be stored for post context 47 in state 1
-           and for post context 11 in state 4. Since the paths of the post contexts
-           do not cross it is actually not necessary to have to separate
-           array registers. One for post context 47 and 11 is enough.
-
-           This function determines what post contexts (together with the
-           'acceptance position register) can share a single register/array 
-           location. It does so by investigating the drop-out routers and how 
-           they use position registers: 
-           
-                Position registers that appear together in a drop-out router
-                CANNOT share a register. Otherwise their positions would not be
-                distinguishable. (Exception: if the two position registers are
-                applied in exactly the same set of states. This case is
-                not yet considered.)
-        """
-        # IMPORTANT: When we talk about 'post_context' this includes the 
-        #            last acceptance position. Indeed, the last acceptance
-        #            position is coded as 'PostContextIDs.NONE'.
-
-        # (1) Determine: -- Set of all existing post context ids.
+    def _get_position_storage_constellations(self):
+        # -- Set of all existing post context ids.
         all_post_context_set = set()
-        #                -- Set of constellations of post contexts as they 
-        #                   appear in the drop-out routers.
-        constellation_set    = set()
+        # -- Set of constellations of post contexts as they 
+        #    appear in the drop-out routers.
+        constellation_list   = []
 
-        #     Loop over all routers of the drop out handlers.
         for drop_out in imap(lambda state: state.drop_out, self.__state_db.itervalues()):
             if drop_out is None: continue
-            constellation = map(lambda x: x.post_context_id, 
-                                ifilter(lambda x: x.positioning is None, 
-                                        drop_out.router))
-            constellation = tuple(sorted(constellation))
-            constellation_set.add(constellation)
-            all_post_context_set.update(constellation)
+            related_post_context_id_set = set(map(lambda x: x.post_context_id, 
+                                                  ifilter(lambda x: x.positioning is None, 
+                                                  drop_out.router)))
+            all_post_context_set.update(related_post_context_id_set)
+            constellation_list.append(related_post_context_id_set)
 
-        # (2) Determine: For each post context the set of post context with which it 
-        #                can share the position register.
-
-        # -- First, assume for each post context that it can share with ALL others
-        allowed_db = dict([(register, copy(all_post_context_set)) for register in all_post_context_set])
-
-        # -- Second, delete for each post context the post contexts with which it 
-        #    appears together.
-        for post_context_id in all_post_context_set:
-            for constellation in ifilter(lambda x: register in x, constellation_set):
-                # Never remove a register itself from the combinable set
-                for other in ifilter(lambda x: x != post_context_id, constellation):
-                    allowed_db[post_context_id].discard(other)
-
-        # (3) Determine: For each post context the index in an array of stored 
-        #                input positions that it would use.
-        # -- allowed_db.values() lists all combinable sets of post context.
-        combinable_list = allowed_db.values()
-
-        result      = {}
-        array_index = 0
-        while len(combinable_list) != 0:
-            # Allways, try to combine the largest combinable set first.
-            k           = max(enumerate(combinable_list), key=itemgetter(1))[0]
-            combination = combinable_list.pop(k)
-
-            for post_context_id in combination:
-                result[post_context_id] = array_index
-                # Delete the post_context_id from all combinable sets, because it is done.
-                for combination in combinable_list:
-                    combination.discard(post_context_id)
-
-            # Since: -- The combinations only contain post_context_id's that have not been
-            #           mentioned before, and
-            #        -- all empty combinations are deleted from the combinable_list,
-            # Thus:  It is safe to assume that new entries were made for the current
-            #        array index. Thus, a new array index is required for the next turn.
-            array_index += 1
-
-            # Delete combinations that have become empty
-            size = len(combinable_list)
-            p    = 0
-            while p < size:
-                if len(combinable_list[p]) == 0: del combinable_list[p]; size -= 1
-                else:                            p += 1
-
-        return result
+        return all_post_context_set, constellation_list
 
 InputActions = Enum("INCREMENT_THEN_DEREF", "DEREF", "DECREMENT_THEN_DEREF")
 
@@ -638,7 +541,6 @@ class Entry(object):
                 if None not in pre_context_id_list:
                     txt.append("if %s: " % repr(pre_list)[1:-1])
                 txt.append("%s = input_p;\n" % repr_position_register(post_context_id))
-            
 
         return "".join(txt)
 
