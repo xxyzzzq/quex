46e5d6bdd20feaa15c66ea903cca9acc0fc2218e
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-07-12 00:40:47 +0000

    BugFix 3357694 -- Revise Unit Tests

--- trunk/TEST/bug-3357694.sh
+++ trunk/TEST/bug-3357694.sh
@@ -1,13 +1,21 @@
 #! /usr/bin/env bash
 bug=3357694
 if [[ $1 == "--hwut-info" ]]; then
-    echo "denkfix: $bug Matches too long when using certain post condition"
+    echo "denkfix: $bug Matches too long when using certain post condition;"
+    echo "CHOICES: Normal, TemplateCompression, PathCompression;"
+    echo "SAME;"
     exit
 fi
 
 tmp=`pwd`
 cd $bug/ 
-make test >& tmp.txt
+
+case $1 in 
+    Normal)              make test                                   >& tmp.txt;;
+    TemplateCompression) make test ADD_OPTION=--template-compression >& tmp.txt;;
+    PathCompression)     make test ADD_OPTION=--path-compression     >& tmp.txt;;
+esac
+
 cat tmp.txt | awk '(/[Ww][Aa][Rr][Nn][Ii][Nn][Gg]/ || /[Ee][Rr][Rr][Oo][Rr]/) && ! /ASSERTS/ '
 rm tmp.txt
 
