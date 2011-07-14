b84ecb42dfd4d747655a6ff14336a470374ca850
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-07-14 04:44:01 +0000

    Version Info / Prepare Release 0.59.6

--- quex-exe.py
+++ quex-exe.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
         print
         error_msg("#\n# Keyboard interrupt -- exiting while processing unfinished.\n#")
 
-    #except Exception, x:
-    #    on_exception(x, "Exception occured -- please, report a bug under\n" + \
-    #                    " https://sourceforge.net/tracker/?group_id=168259&atid=846112")
+    except Exception, x:
+        on_exception(x, "Exception occured -- please, report a bug under\n" + \
+                        " https://sourceforge.net/tracker/?group_id=168259&atid=846112")
 
--- quex/DEFINITIONS.py
+++ quex/DEFINITIONS.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-QUEX_VERSION = '0.59.5'
+QUEX_VERSION = '0.59.6'
 
 try:
     QUEX_INSTALLATION_DIR = os.environ["QUEX_PATH"]
