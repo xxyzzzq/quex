fb74b47deac37b5dbc84f67cdf3f443bc56f8871
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-08-21 09:46:16 +0000

    Clean Up

--- branches/track_analyzis/quex/engine/generator/state_machine_coder.py
+++ branches/track_analyzis/quex/engine/generator/state_machine_coder.py
@@ -1,77 +1,53 @@
-from   quex.engine.generator.state_machine_decorator import StateMachineDecorator
-
-import quex.engine.generator.state_coder.core as state_coder
+import quex.engine.generator.state_coder.core as     state_coder
+from   quex.blackboard                        import setup as Setup
 import quex.engine.generator.template_coder   as template_coder
 import quex.engine.generator.paths_coder      as paths_coder
-from   quex.blackboard                       import setup as Setup
 
-def do(SMD, TemplateHasBeenCodedBeforeF=False):
-    """Returns the program code implementing the StateMachine's behavior.
-       NOTE: This function should only be called on a DFA after the call
-             to 'filter_dominated_origins'. The latter is important
-             to ensure that for an acceptance state there is only one
-             related original state.
+from   collections import defaultdict
+from   itertools   import ifilter
 
-       The procedure for each state is the following:
-            i)  input <-- next character from stream 
-            ii) state transition code (include marking of last success state
-                and last success stream position).                  
+def do(TheAnalyzer):
+    """Generate source code for a given state machine 'SM'.
     """
-    assert isinstance(SMD, StateMachineDecorator)
+    txt = []
 
-    state_machine = SMD.sm()
-    
-    # Track what states are treated with different methods
-    remainder = set(state_machine.states.keys())
+    # Track what states are treated with different methods (see below)
+    remainder  = set(TheAnalyzer.state_db.keys())
 
     # (*) Init State
-    init_state = state_coder.do(state_machine.states[state_machine.init_state_index], 
-                                state_machine.init_state_index, 
-                                SMD)
-    remainder.discard(state_machine.init_state_index)
+    state_coder.do(txt, TheAnalyzer.state_db[TheAnalyzer.init_state_index], TheAnalyzer)
+    remainder.discard(TheAnalyzer.init_state_index)
 
     # (*) [Optional] Path-Compressed States
     path_compressed_states = []
     if Setup.compression_path_f or Setup.compression_path_uniform_f:
-        path_compressed_states, state_index_set = paths_coder.do(SMD, Setup.compression_path_uniform_f)
+        assert False # We cannot deal with that yet
+        path_compressed_states, state_index_set = paths_coder.do(TheAnalyzer, Setup.compression_path_uniform_f)
         remainder.difference_update(state_index_set)
     
     # (*) [Optional] Template-Compressed States
     template_compressed_states = []
     if Setup.compression_template_f:
-        template_compressed_states, state_index_set = template_coder.do(SMD, Setup.compression_template_coef)
+        template_compressed_states, state_index_set = template_coder.do(TheAnalyzer, Setup.compression_template_coef)
         remainder.difference_update(state_index_set)
     
-    # (*) All other (normal) states
-    normal_states = []
-    for state_index, state in get_sorted_state_list(state_machine.states, remainder):
-        normal_states.extend(state_coder.do(state, state_index, SMD))
+    # (*) All other (normal) states (sorted by their frequency of appearance
+    frequency_db = get_frequency_db(TheAnalyzer.state_db, remainder)
+    for state in sorted(ifilter(lambda x: x.index in remainder, TheAnalyzer.state_db.itervalues()), 
+                        key=lambda x: frequency_db[x.index], reverse=True):
+        state_coder.do(txt, state, TheAnalyzer) 
 
-    return init_state + \
-           normal_states + \
-           path_compressed_states + \
-           template_compressed_states 
+    return txt
 
-def get_sorted_state_list(StateDict, RemainderStateIndexList):
+def get_frequency_db(StateDict, RemainderStateIndexList):
     """Sort the list in a away, so that states that are used more
        often appear earlier. This happens in the hope of more 
        cache locality. 
     """
     # Count number of transitions to a state: frequency_db
-    frequency_db = {}
+    frequency_db = defaultdict(int)
     for state in StateDict.itervalues():
-        for target_index in state.transitions().get_map().iterkeys():
-            if frequency_db.has_key(target_index): frequency_db[target_index] += 1
-            else:                                  frequency_db[target_index]  = 1
-
-    # Only list states that remain to be coded.
-    state_list = []
-    for index, state in StateDict.iteritems():
-        if index not in RemainderStateIndexList: continue
-        state_list.append((index, state))
-
-    # x[0]               -- state index; 
-    # frequency_db[x[0]] -- frequency that state 'state_index' is entered.
-    state_list.sort(key=lambda x: frequency_db[x[0]], reverse=True)
-    return state_list
+        for interval, target_index in state.transition_map:
+            frequency_db[target_index] += 1
+    return frequency_db
 
