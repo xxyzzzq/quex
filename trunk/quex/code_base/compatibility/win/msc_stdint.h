f23c6f4d26f61a2e6246b2c9399cbc390786413d
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-10-11 05:31:19 +0000

    quick

--- trunk/quex/code_base/compatibility/win/msc_stdint.h
+++ trunk/quex/code_base/compatibility/win/msc_stdint.h
@@ -1,21 +1,5 @@
 #ifndef __QUEX_INCLUDE_GUARD__COMPATIBILITY__WIN__MSC_STDINT_H
 #define __QUEX_INCLUDE_GUARD__COMPATIBILITY__WIN__MSC_STDINT_H
-
-#ifndef _MSC_VER 
-#   error "Use this header only with Microsoft Visual C++ compilers!"
-#endif          
-
-#if _MSC_VER >= 1600
-// VisualStudio(tm) 2010 _MSC_VER = 1600
-// Since this version, Visual Studio(tm) contains "stdint.h"
-#   ifdef __cplusplus
-extern "C" {
-#   endif
-#   include "stdint.h"
-#   ifdef __cplusplus
-}
-#   endif
-#else
 // ISO C9x  compliant stdint.h for Microsoft Visual Studio
 // Based on ISO/IEC 9899:TC2 Committee draft (May 6, 2005) WG14/N1124 
 // 
@@ -46,6 +30,22 @@ extern "C" {
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // 
 ///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _MSC_VER 
+#   error "Use this header only with Microsoft Visual C++ compilers!"
+#endif          
+
+#if _MSC_VER >= 1600
+// VisualStudio(tm) 2010 _MSC_VER = 1600
+// Since this version, Visual Studio(tm) contains "stdint.h"
+#   ifdef __cplusplus
+extern "C" {
+#   endif
+#   include "stdint.h"
+#   ifdef __cplusplus
+}
+#   endif
+#else
 
 #if _MSC_VER > 1000
 #pragma once
