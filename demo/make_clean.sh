284039c17bf886bf19c5f69894b1582da9f844f1
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2011-10-31 07:31:35 +0000

    Cleanup

--- demo/make_clean.sh
+++ demo/make_clean.sh
@@ -4,5 +4,5 @@ for x in `find -maxdepth 3 -mindepth 1 -type f -or -path "*.svn*" -prune -or -pr
     make clean; 
     rm -f tmp.txt
     rm -f tmp[10].txt
-    cd $q/demo; 
+    cd $QUEX_PATH/demo; 
 done
