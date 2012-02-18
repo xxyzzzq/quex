079b9f9141af9ca9279067c50a3790f3b64a9858
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2012-02-18 00:48:03 +0000

    Unfinished: Entry Action Unification.

--- trunk/quex/TEST/misc/TEST/debug_show.sh
+++ trunk/quex/TEST/misc/TEST/debug_show.sh
@@ -6,14 +6,14 @@ case $1 in
 
     *)
         quex -i debug_show.qx -o Simple \
-             --path-compression \
+             --path-compression-uniform \
              --template-compression \
              --template-compression-min-gain 0 -\
              --language C --debug-exception \
              --token-policy single
         gcc -I$QUEX_PATH Simple.c $q/TEST/lexer.c -I. -DQUEX_OPTION_DEBUG_SHOW -DQUEX_SETTING_BUFFER_SIZE=15 -o debug_show
         ./debug_show debug_show.txt 2>&1 | sed -e 's/:[0-9]\+:/:LineNumber:/g'
-        rm ./debug_show Simple*
+        echo rm ./debug_show Simple*
         ;;
 esac
 
