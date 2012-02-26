4f68966e04d3c9aafef45decd63a659f20cecdcf
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2012-02-26 07:34:22 +0000

    Refactoring Directory Structure

--- quex/engine/analyzer/mega_state/path/TEST/select_longest_paths.py
+++ quex/engine/analyzer/mega_state/path/TEST/select_longest_paths.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.environ["QUEX_PATH"])
 from   quex.engine.interval_handling    import *
 import quex.engine.state_machine.core   as     core
 from   quex.engine.analyzer.state.core  import AnalyzerState
-from   quex.engine.analyzer.path.core   import __select_longest_intersecting_paths, CharacterPath     
+from   quex.engine.analyzer.mega_state.path.core   import __select_longest_intersecting_paths, CharacterPath     
 from   quex.blackboard                  import E_EngineTypes
 
 if "--hwut-info" in sys.argv:
--- quex/engine/analyzer/mega_state/path/core.py
+++ quex/engine/analyzer/mega_state/path/core.py
@@ -1,6 +1,6 @@
 # (C) 2010 Frank-Rene Schaefer
-from   quex.engine.analyzer.path.path        import CharacterPath
-import quex.engine.analyzer.path.path_walker as     path_walker
+from   quex.engine.analyzer.mega_state.path.path        import CharacterPath
+import quex.engine.analyzer.mega_state.path.path_walker as     path_walker
 from   quex.blackboard                       import E_Compression
 
 from   copy        import deepcopy
