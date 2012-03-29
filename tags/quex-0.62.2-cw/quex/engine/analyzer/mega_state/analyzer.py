6a36400287cfa6570bddfa09c65b13263869df57
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2012-03-29 21:24:34 +0000

    Release 0.62.2-cw

---
+++ tags/quex-0.62.2-cw/quex/engine/analyzer/mega_state/analyzer.py
@@ -0,0 +1,67 @@
+import quex.engine.analyzer.mega_state.template.core    as     template_analyzer
+import quex.engine.analyzer.mega_state.path_walker.core as     path_analyzer
+from   quex.blackboard                                  import E_Compression
+from   quex.blackboard                                  import setup as Setup
+
+def do(TheAnalyzer):
+    # Track what states are treated with different methods (see below)
+    remainder  = set(TheAnalyzer.state_db.keys())
+    # The init state shall never be part of a mega state
+    remainder.discard(TheAnalyzer.init_state_index)
+
+    mega_state_list = []
+    remainder       = set(TheAnalyzer.state_db.keys())
+    remainder.remove(TheAnalyzer.init_state_index)
+    for ctype in Setup.compression_type_list:
+        # -- Path-Compression
+        if ctype in (E_Compression.PATH, E_Compression.PATH_UNIFORM):
+            done_state_index_list, \
+            new_mega_state_list    = path_analyzer.do(TheAnalyzer, ctype, 
+                                                      remainder, mega_state_list)
+    
+        # -- Template-Compression
+        elif ctype in (E_Compression.TEMPLATE, E_Compression.TEMPLATE_UNIFORM):
+            done_state_index_list, \
+            new_mega_state_list    = template_analyzer.do(TheAnalyzer, 
+                                                          Setup.compression_template_min_gain, ctype, 
+                                                          remainder, mega_state_list)
+        else:
+            assert False
+
+        __transition_adaption(TheAnalyzer, new_mega_state_list, mega_state_list)
+
+        remainder.difference_update(done_state_index_list)
+        mega_state_list.extend(new_mega_state_list)
+
+    TheAnalyzer.non_mega_state_index_set = remainder
+    TheAnalyzer.mega_state_list          = mega_state_list
+
+def __transition_adaption(TheAnalyzer, NewMegaStateList, OldMegaStateList):
+    """Some AnalyzerState objects have been implemented in Mega States that 
+       contain multiple states in once. Thus, the transitions to those AnalyzerState-s
+       must be related to doors in the Mega State. This is what happens inside
+       the present function.
+    """
+    # For the states that the new mega states implement, the doors need to 
+    # be redirected from the original states to doors of the mega states.
+    door_id_replacement_db = {}
+    for mega_state in NewMegaStateList:
+        door_id_replacement_db.update(mega_state.door_id_replacement_db)
+
+    # All template states must set the databases about 'door-id' and 'transition-id'
+    # in the states that they implement.
+    for state in NewMegaStateList:
+        state.replace_door_ids_in_transition_map(door_id_replacement_db)
+
+    for state in OldMegaStateList:
+        state.replace_door_ids_in_transition_map(door_id_replacement_db)
+
+    # We must leave the databases in place, until the replacements are made
+    for mega_state in NewMegaStateList:
+        for state in (TheAnalyzer.state_db[i] for i in mega_state.implemented_state_index_list()):
+            # Make sure, that the databases which are referenced for transition addresses
+            # are updated, i.e. we use the ones of the template state.
+            state.entry.set_door_db(mega_state.entry.door_db)
+            state.entry.set_transition_db(mega_state.entry.transition_db)
+            state.entry.set_door_tree_root(mega_state.entry.door_tree_root)
+    return
---
+++ tags/quex-0.62.2-cw/quex/engine/state_machine/TEST/test-count_newline_n.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+import sys
+import os
+sys.path.insert(0, os.environ["QUEX_PATH"])
+
+import quex.input.regular_expression.engine         as regex
+import quex.engine.state_machine.character_counter as counter
+
+if "--hwut-info" in sys.argv:
+    print "Predetermined Character Count: Newlines"
+    sys.exit(0)
+    
+def test(TestString):
+    TestString = TestString.replace("\n", "\\n").replace("\t", "\\t")
+    print "expression           = " + TestString
+    sm = regex.do(TestString, {}).sm
+    print "fixed newline number = ", counter.get_newline_n(sm)
+
+test('[0-9]+')
+test('"1\n\n\n3"')
+test('"1\n\n\n3"|"A\n\n\nC"')
+test('"1\n\n\n34"|"A\n\n\nC"')
+test('"1\n\n\n34"|"A\n\n\nC\n"')
+test('"1\n\n\n3\n"|"A\n\n\nC\n"')
+test('"1\n"("1\n"?)')
+test('"1\n"')
+test('[ \t\n]')
+test('[ \t\n]+')
+test('"\n\n"')
+test('(\n\n)+')
+test('(\n|\n)+')
+test('X(\n|\n)*')
+test('X(\n|\n)?')
+test('"1\n"+')
+test('X"1\n"*')
+test('X"1\n"?')
+
+test('"1\n\n\n3"+')
+test('X"1\n\n\n3"?')
+test('X"1\n\n\n3"*')
+
+test('a\n\n\nc("1\n\n\n3"+)')
+test('a\n\n\nc("1\n\n\n3"?)')
+test('a\n\n\nc("1\n\n\n3"*)')
+
+test('a\n\n\nc("1\n\n\n3"+)x\n\n\nz')
+test('a\n\n\nc("1\n\n\n3"?)x\n\n\nz')
+test('a\n\n\nc("1\n\n\n3"*)x\n\n\nz')
+
+test('a\n\n\nc("1\n\n\n3"|"A\n\n\nC")x\n\n\nz')
+test('a\n\n\nc("1\n\n\n3"|"A\n\n\nC\n")x\n\n\nz')
+test('a\n\n\nc("1\n\n\n3"|"A\n\n\nC")+x\n\n\nz')
+test('a\n\n\nc("1\n\n\n3"|"A\n\n\nC")?x\n\n\nz')
+test('\na\n\n\nc\n("1\n\n\n3"|"A\n\nC\n")*\nx\n\n\nz\n')
+
+test('"a"|"c"|"e"|"g"')
+test('"a"|"\n"|"e"|"g"')
+test('X("a"|"\n"*|"e"|"g")')
+test('"a\ne"|"\n\n"')
+
+test('A\n\n\nC("12\n\n\n"|("A\n\n\nC"|"X\n\n\nZ"))"12\n\n\n"("\n\n\nAA"|"\n\n\nBB"|"CC\n\n\n")X\n\n\nZ')
+test('A\n\n\nC("12\n\n\n"|("A\n\n\nCD"|"X\n\n\nZ"))"12\n\n\n"("\n\n\nAA"|"\n\n\nBB"|"CC\n\n\n")X\n\n\nZ')
+test('"\n"{4}("a\n\ne"|"\n\n")')
+
+# quex version >= 0.49.1: only treat core pattern; no pre and post-conditions
+if False:
+    test('"12\n"/"Z"')
+    test('"1\n3"/"Z"')
+    test('"12\n"+/"Z"')
+    test('"1\n3"+/"Z"')
+    test('("1\n23"|"A\nC")/"Z"')
+    test('"12\n"/"ABC"|"XYZ"')
+    test('"12\n"/"ABC"|"X"*')
+    test('"12\n"/"ABC"|"X"?')
+    test('"12\n"/"ABC"|""')
+    test('"12\n"/"X\nZ"+')
+
+    test('"a"/"\n"/"b"')
+    test('x/(y|\n)/z')
+    test('"a"/b/c|\n')
+    test('"a\n"/b\nc|\n+')
+
+
