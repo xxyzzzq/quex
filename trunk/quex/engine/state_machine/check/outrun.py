178821c4482456f04ee1f1774a45ab52005fc4a7
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2012-04-27 06:12:25 +0000

    Clean Up

--- trunk/quex/engine/state_machine/check/outrun.py
+++ trunk/quex/engine/state_machine/check/outrun.py
@@ -9,29 +9,19 @@ def do(High, Low):
        EXAMPLE: High:  print
                 Low:   [a-z]+
 
-          If a stream contains "printer", then 'Low' would match even 
-          though 'High' could have matched if 'Low' was not there. The
-          writer of 'High' might be surprised if he was not aware of 
-          'Low'.
-       
+       If a stream contains "printer", then 'Low' would match even though
+       'High' could have matched if 'Low' was not there. The writer of 'High'
+       might be surprised if he was not aware of 'Low'.
+
+       Less obvious is:   High: alb|albertikus
+                          Low:  albert
+
+       where the low priority pattern may match on "albert" after the high-prio
+       pattern has matched on "alb". If the lexeme does not complete
+       "albertikus", then the low priority pattern wins.
+
        RETURNS: True, if 'Low' might outrun a lexeme that matches 'High'.
                 False, 'Low' can never outrun a lexeme matched by 'High'.
-       ________________________________________________________________________
-
-       ASSUMPTION: 
-       
-       It is assumed, that both machines are HOPCROFT-MINIMIZED! That is,
-       there MUST NOT BE A SUPERFLOUS FORK such as in
-
-                      .-- 'a'--->( 2 )-- 'c' --.
-                     /                          \
-          ... --->( 1 )                        ( 4 )--- ...
-                     \                          /
-                      '-- 'b'--->( 3 )-- 'c' --'
-
-       which needs to be minized to
-
-          ... --->( 1 )--- 'a' | 'b' --->( 2 )-- 'c' --->( 4 )--- ...
     """
     # Step 1: Find acceptance states which are reached while walking
     #         along paths of 'High' that are also inside 'Low'.
@@ -47,6 +37,7 @@ def do(High, Low):
     # Step 2: Detect paths in Low that divert from High starting from
     #         the acceptance states collected in step 1.
     detector = Step2_Walker(High, Low)
+
     # Start searching for diversion from the critical acceptance states in High.
     detector.do(collector.result)
 
@@ -63,66 +54,9 @@ def do(High, Low):
     #
     # False   Else.
     #
-    return walker.result
+    return detector.result
 
-class Base_TunnelWalker(TreeWalker):
-    """Walks along paths of 'A' if they are covered by 'B'. In a sense, it
-       walks the paths of 'A' in the tunnels of 'B'.
-    
-       If an acceptance state of 'A' is reached, then function 
-       
-                    self.on_acceptance_state_reached(...)
-
-       is executed. Its implemented in the derived class. When a step in 'A'
-       is detected that is not covered by 'B', then function 
-
-                    self.on_not_covered()
-
-       is executed which is also implemented in a derived class.
-    """
-    def __init__(self, A, B):
-        self.a_sm     = A
-        self.b_am     = B
-        self.result   = False
-        self.done_set = set()
-        TreeWalker.__init__(self)
-
-    def on_enter(self, Args):
-        # (*) Update the information about the 'trace of acceptances'
-        A_StateIndex, B_StateIndex = Args
-        if A_StateIndex in self.done_set: return None
-        else:                             self.done_set.add(A_StateIndex)
-        A_State = self.a_sm.states[A_StateIndex]
-
-        # Follow the path of common trigger sets
-        if A_State.is_acceptance():
-            self.on_acceptance_state_reached(A_StateIndex, B_StateIndex)
-            return None
-
-        B_State       = self.b_sm.states[B_StateIndex]
-        A_TriggerMap  = A_State.transitions().get_map()
-        B_TriggerMap  = B_State.transitions().get_map()
-        sub_node_list = []
-        for a_target, a_trigger_set in A_TriggerMap.iteritems():
-            for b_target, b_trigger_set in B_TriggerMap.iteritems():
-                if not b_trigger_set.is_superset(a_trigger_set): continue
-                # The transition in 'A' is covered by a transition in 'B'.
-                sub_node_list.append( (a_target, b_target) )
-                break
-            else:
-                # There is no transition in 'B' that covers the step in 'A'
-                # => B does not cover the given path in 'A'. 
-                # => Further investigation of the current path not necessary.
-                self.on_not_covered()
-                return None
-
-        # (*) Recurse to all sub nodes
-        return sub_node_list
-
-    def on_finished(self, Args):
-        pass
-
-class Step1_Walker(Base_TunnelWalker):
+class Step1_Walker(TreeWalker):
     """Find acceptance states of 'High' which are reachable
        by walking along possible paths in 'Low'. 
        
@@ -136,25 +70,47 @@ class Step1_Walker(Base_TunnelWalker):
        from these detected states to see whether it diverts.
     """
     def __init__(self, High, Low):
-        Base_TunnelWalker.__init__(self, SM_A=High, SM_B=Low)
-        self.result = []
+        self.high     = High # State Machine of the higher priority pattern
+        self.low      = Low  # State Machine of the lower priority pattern
+        self.result   = []
+        self.done_set = set()
+        TreeWalker.__init__(self)
 
-    def on_acceptance_state_reached(self, High_StateIndex, Low_StateIndex):
-        # A = High, B = Low
-        self.result.append( (Low_StateIndex, High_StateIndex) )
+    def on_enter(self, Args):
+        # (*) Update the information about the 'trace of acceptances'
+        High_StateIndex, Low_StateIndex = Args
+        if High_StateIndex in self.done_set: return None
+        else:                                self.done_set.add(High_StateIndex)
+        High_State = self.high.states[High_StateIndex]
 
-    def on_not_covered(self):
+        # Register any acceptance state reached in the high-prio pattern while
+        # walking also along paths in 'low'. Need to specify the associated
+        # state index of the low-prio pattern, so that the subsequent search
+        # knows where to start.
+        if High_State.is_acceptance():
+            self.result.append( (High_StateIndex, Low_StateIndex) )
+            return None
+
+        # Follow the path of common trigger sets
+        Low_State     = self.low.states[Low_StateIndex]
+        sub_node_list = []
+        for a_target, a_trigger_set in High_State.transitions().get_map().iteritems():
+            for b_target, b_trigger_set in Low_State.transitions().get_map().iteritems():
+                if b_trigger_set.has_intersection(a_trigger_set): 
+                    # Some of the transition in 'High' is covered by a transition in 'Low'.
+                    sub_node_list.append( (a_target, b_target) )
+
+        return sub_node_list
+
+    def on_finished(self, Args):
         pass
 
-class Step2_Walker(Base_TunnelWalker):
+class Step2_Walker(TreeWalker):
     """Starts at the acceptance states of 'High' that can be walked
        along in 'Low'. It then checks whether 'Low' can walk paths from
        there which are not covered by 'High'.
 
-       Use the algorithm provided by 'Base_TunnelWalker' where
-       "A = Low" and "B = High".
-
-       -- If an acceptance state in Low ('A') is reached while High does
+       -- If an acceptance state in Low is reached while High does
           not accept, then Low has outrun High after match. 
           
           Set 'result = True' and abort.
@@ -165,16 +121,51 @@ class Step2_Walker(Base_TunnelWalker):
           Set 'result = True' and abort.
     """
     def __init__(self, High, Low):
-        Base_TunnelWalker.__init__(self, SM_A=Low, SM_B=High)
-        self.result = False
+        self.high     = High  # State Machine of the higher priority pattern
+        self.low      = Low   # State Machine of the lower priority pattern
+        self.result   = False # Low cannot outrun High
+        self.done_set = set()
+        TreeWalker.__init__(self)
 
-    def on_acceptance_state_reached(self, Low_StateIndex, High_StateIndex):
-        # A = Low, B = High
-        if self.sm_b[High_StateIndex].is_acceptance(): 
-            return # High still wins, continue
-        self.result  = True
-        self.abort_f = True
+    def on_enter(self, Args):
+        # (*) Update the information about the 'trace of acceptances'
+        High_StateIndex, Low_StateIndex = Args
+        if Low_StateIndex in self.done_set: return None
+        else:                               self.done_set.add(Low_StateIndex)
 
-    def on_not_covered(self):
-        self.result  = True
-        self.abort_f = True
+        Low_State  = self.low.states[Low_StateIndex]
+        High_State = self.high.states[High_StateIndex]
+
+        # When the low prio pattern reaches an acceptance state where the high
+        # does not, then it outrun the high pattern. Note, that here we are in
+        # states *after* a matching high-prio pattern (after the high-prio 
+        # acceptance states).
+        if Low_State.is_acceptance() and not High_State.is_acceptance():
+            self.result  = True # Low outruns High
+            self.abort_f = True
+            return None
+
+        sub_node_list = []
+        for b_target, b_trigger_set in Low_State.transitions().get_map().iteritems():
+            b_remaining_triggers = b_trigger_set
+            for a_target, a_trigger_set in High_State.transitions().get_map().iteritems():
+                if b_trigger_set.has_intersection(a_trigger_set): 
+                    # The transition in 'A' is covered by a transition in 'B'.
+                    sub_node_list.append( (a_target, b_target) )
+                    b_remaining_triggers.subtract(a_trigger_set)
+
+            if not b_remaining_triggers.is_empty():
+                # Not all triggers in the state of the low-prio pattern where 
+                # present in the high-prio state. Thus, the low-prio pattern 
+                # has already outrun the high-prio pattern.
+                self.result  = True # Low outruns High
+                self.abort_f = True
+                return None
+
+        # (*) Recurse to all sub nodes
+        return sub_node_list
+
+    def on_finished(self, Args):
+        pass
+
+
