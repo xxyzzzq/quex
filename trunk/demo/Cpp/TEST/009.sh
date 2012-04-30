1544f47a89c59fffa4f2389943fc3f99d920e715
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2012-04-30 23:27:09 +0000

    Clean Up

--- trunk/demo/Cpp/TEST/009.sh
+++ trunk/demo/Cpp/TEST/009.sh
@@ -1,14 +1,22 @@
 #! /usr/bin/env bash
 if [[ $1 == "--hwut-info" ]]; then
     echo "demo/009: Strings of 'char' and 'wchar_t'"
-    echo "CHOICES:  lexer, wlexer;"
+    echo "CHOICES:  lexer, wlexer, stdinlexer;"
     echo "SAME;"
     exit
 fi
 cd $QUEX_PATH/demo/Cpp/009
 make $1 >& /dev/null
-echo "Normal FILE input"
-./$1 FILE
-echo "Normal stringstream input"
-./$1 stringstream
-make clean >& /dev/null
+
+if [[ $1 == "stdinlexer" ]]; then
+    echo -e "Hello World\nbye\n" | valgrind --leak-check=full ./stdinlexer &>  tmp.txt
+    echo -e "212 W32orld\nbye\n" | valgrind --leak-check=full ./stdinlexer &>> tmp.txt
+else
+    echo "Normal FILE input"                     &>  tmp.txt 
+    valgrind --leak-check=full ./$1 FILE         &>> tmp.txt
+    echo "Normal stringstream input"             &>> tmp.txt
+    valgrind --leak-check=full ./$1 stringstream &>> tmp.txt
+fi
+python $QUEX_PATH/TEST/show-valgrind.py 
+
+make clean &> /dev/null
