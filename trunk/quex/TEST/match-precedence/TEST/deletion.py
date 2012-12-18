703df3cbb8fd0ac540812c618e3949b4acf26d0f
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2012-12-18 21:51:19 +0000

    Counter Setup Parsing

--- trunk/quex/TEST/match-precedence/TEST/deletion.py
+++ trunk/quex/TEST/match-precedence/TEST/deletion.py
@@ -9,7 +9,8 @@ import match_precedence_test as test
 if "--hwut-info" in sys.argv:
     print "Checking DELETE marks internally.;"
     print "CHOICES: 3-begin, 3-mid, 3-end, 2-begin, 2-end, 1, Nonsense," \
-                   "derived-3-begin, derived-3-mid, derived-3-end, derived-2-begin, derived-2-end, derived-1, derived-Nonsense;"
+                   "derived-3-begin, derived-3-mid, derived-3-end, derived-2-begin, derived-2-end, derived-1, derived-Nonsense," \
+                   "before, derived-before;"
     sys.exit()
 
 derived = None
@@ -48,6 +49,11 @@ elif "derived-1" in sys.argv:
 elif "derived-Nonsense" in sys.argv:
     base    = "X { A {} }"
     derived = "Y : X { Nonsense DELETION; }"
+if   "before" in sys.argv:
+    base = "X { A {} B {} C DELETION; C {} }"
+if   "derived-before" in sys.argv:
+    base = "X { A {} B {} C DELETION; }"
+    derived = "Y : X { C {} }"
 
 if derived is not None:
     test.do([base, derived], "DELETION")
