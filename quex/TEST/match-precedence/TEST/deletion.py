9e98e0c2f2437ad8c4a5d1c16c4a739c95247b42
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2012-12-18 06:08:15 +0000

    Counter Setup Parsing

---
+++ quex/TEST/match-precedence/TEST/deletion.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+import os
+import sys
+sys.path.append(os.environ["QUEX_PATH"])
+sys.path.insert(0, os.getcwd())
+
+import match_precedence_test as test
+
+if "--hwut-info" in sys.argv:
+    print "Checking DELETE marks internally.;"
+    print "CHOICES: 3-begin, 3-mid, 3-end, 2-begin, 2-end, 1, Nonsense," \
+                   "derived-3-begin, derived-3-mid, derived-3-end, derived-2-begin, derived-2-end, derived-1, derived-Nonsense;"
+    sys.exit()
+
+derived = None
+if   "3-begin" in sys.argv:
+    base = "X { A {} B {} C {} A DELETION; }"
+elif "3-mid" in sys.argv:
+    base = "X { A {} B {} C {} B DELETION; }"
+elif "3-end" in sys.argv:
+    base = "X { A {} B {} C {} C DELETION; }"
+elif "2-begin" in sys.argv:
+    base = "X { A {} B {} A DELETION; }"
+elif "2-end" in sys.argv:
+    base = "X { A {} B {} B DELETION; }"
+elif "1" in sys.argv:
+    base = "X { A {} A DELETION; }"
+elif "Nonsense" in sys.argv:
+    base = "X { A {} Nonsense DELETION; }"
+elif   "derived-3-begin" in sys.argv:
+    base    = "X { A {} B {} C {} }"
+    derived = "Y : X { A DELETION; }"
+elif "derived-3-mid" in sys.argv:
+    base    = "X { A {} B {} C {} }"
+    derived = "Y : X { B DELETION; }"
+elif "derived-3-end" in sys.argv:
+    base    = "X { A {} B {} C {} }"
+    derived = "Y : X { C DELETION; }"
+elif "derived-2-begin" in sys.argv:
+    base    = "X { A {} B {} }"
+    derived = "Y : X { A DELETION; }"
+elif "derived-2-end" in sys.argv:
+    base    = "X { A {} B {} }"
+    derived = "Y : X { B DELETION; }"
+elif "derived-1" in sys.argv:
+    base    = "X { A {} }"
+    derived = "Y : X { A DELETION; }"
+elif "derived-Nonsense" in sys.argv:
+    base    = "X { A {} }"
+    derived = "Y : X { Nonsense DELETION; }"
+
+if derived is not None:
+    test.do([base, derived], "DELETION")
+else:
+    test.do([base], "DELETION")
+
+
