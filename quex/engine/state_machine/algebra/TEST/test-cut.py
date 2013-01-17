bb6447a630929297ecd4d3b77392fbc24cbf2c4d
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2013-01-17 20:03:10 +0000

    Prepare Release 0.64.1

--- quex/engine/state_machine/algebra/TEST/test-cut.py
+++ quex/engine/state_machine/algebra/TEST/test-cut.py
@@ -5,12 +5,22 @@ sys.path.insert(0, os.environ["QUEX_PATH"])
 
 import quex.input.regular_expression.engine  as regex
 import quex.engine.state_machine.algebra.cut as cut
+import quex.engine.state_machine.algebra.union as union
+import quex.engine.state_machine.algebra.intersection as intersection
+import quex.engine.state_machine.check.special as special
+import quex.engine.state_machine.check.identity as identity
+import quex.engine.state_machine.check.superset as superset
 import quex.engine.state_machine.algorithm.beautifier as beautifier
 
 if "--hwut-info" in sys.argv:
     print "Cut"
     print "CHOICES: 0, 1, 2, 3, 4, 5;"
     sys.exit(0)
+
+def clean(SM):
+    result = beautifier.do(SM)
+    result.clean_up()
+    return result
     
 def test(A, B):
     def __core(Original, Cutter):
@@ -20,7 +30,18 @@ def test(A, B):
         cutter = regex.do(Cutter, {}).sm
         #print orig.get_string(NormalizeF=False)
         #print cutter.get_string(NormalizeF=False)
-        print "result = ", beautifier.do(cut.do(orig, cutter)).get_string(NormalizeF=True)
+        result = clean(cut.do(orig, cutter))
+        print
+        if not special.is_none(result):
+            print "superset(Original, result):           %s" % superset.do(orig, result)
+        if not special.is_none(result):
+            tmp = clean(intersection.do([cutter, result]))
+            print "intersection(Cutter, result) == None: %s" % special.is_none(tmp)
+        tmp = clean(union.do([orig, result]))
+        print "union(Original, result) == Original:  %s" % identity.do(tmp, orig)
+        print
+        print "result = ", result.get_string(NormalizeF=True)
+
     print "---------------------------"
     __core(A, B)
     print
@@ -69,11 +90,12 @@ elif "4" in sys.argv:
 elif "5" in sys.argv:
     test('X("a"|"x"?|"e"|"g")', 'X')
     test('X("a"|"x"?|"e"|"g")', 'Xx')
-    test('"a"|"x"+|"e"|"g"', 'x{20}')
+    test('"a"|"x"+|"e"|"g"',    'x{20}')
     test('X("a"|"x"*|"e"|"g")', 'X')
     test('X("a"|"x"*|"e"|"g")', 'Xx{20}')
-    test('abc("123"|("ABC"|"XYZ")+)+"123"("AAA"|"BBB"|"CCC")?xyz', 'abc123ABC123AAAxyz')
-    test('((((((((p+)r)+i)+)n)+t)+e)+r)+',        'priprinter')
-    test('(printer|rinter|inter|nter|ter|er|r)+', 'printerter')
-    test('(printer|rinter|inter|nter|ter|er|r)+', '((((((((p+)r)+i)+)n)+t)+e)+r)+')
+    # test('abc("123"|("ABC"|"XYZ")+)+"123"("AAA"|"BBB"|"CCC")?xyz', 'abc123ABC123AAAxyz')
+    test('ab("12"|("AB"|"XY")+)+"12"("AA"|"BB"|"CC")?yz', 'ab12AB12AAyz')
+    test('(((a+)b)+c)+', 'abcbc')
+    test('(pri|ri|i)+',  'priri')
+    test('(pri|ri|i)+',  '(((p+)r)+i)+')
 
