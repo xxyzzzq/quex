dc2a7b11ecf051f1bf9c25441d670adff2ef6d89
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2013-02-08 06:37:28 +0000

    Prepare Release 0.64.1

---
+++ TEST/bug-257.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+bug=257
+if [[ $1 == "--hwut-info" ]]; then
+    echo "enmarabrams: $bug Hang-up on recursive --foreign-token-id-file"
+    exit
+fi
+
+tmp=`pwd`
+cd $bug/ 
+quex -i nonsense.qx --foreign-token-id-file include_me.h 2>&1
+
+# cleansening
+cd $tmp
