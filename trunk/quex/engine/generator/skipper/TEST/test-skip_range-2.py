fe0d5bbacd587eda71a6f0a3486b2ec56b0b8eaa
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2013-03-02 07:33:58 +0000

    Clean Up

--- trunk/quex/engine/generator/skipper/TEST/test-skip_range-2.py
+++ trunk/quex/engine/generator/skipper/TEST/test-skip_range-2.py
@@ -16,26 +16,50 @@ if len(sys.argv) < 2 or not (sys.argv[1] in ["DL=1", "DL=2", "DL=3", "DL=4"]):
 
 Language = "Cpp"
 DL = sys.argv[1]
-if   DL == "DL=1": SEP = "*"; BS=3
-elif DL == "DL=2": SEP = "*/"; BS=4
-elif DL == "DL=3": SEP = "*/*"; BS=5
-elif DL == "DL=4": SEP = "*/*/"; BS=6
+if   DL == "DL=1": SEP = "*"
+elif DL == "DL=2": SEP = "*/"
+elif DL == "DL=3": SEP = "*/*"
+elif DL == "DL=4": SEP = "*/*/"
 else:
     print "Delimiter length argument '%s' not acceptable, use --hwut-info" % DL
     sys.exit(0)
 
-end_sequence = map(ord, SEP)
+BS = len(SEP) + 2
 
-TestStr  = "abcdefg" + SEP + "hijklmnop" + SEP + "qrstuvw" + SEP + "xyz" + SEP + "ok"
+def make(Language, CloserStr, BufferSize):
+    code = create_range_skipper_code(Language, "", CloserStr, BufferSize, CommentTestStrF=True)
+    exe_name, tmp_file_name = compile(Language, code)
+    return exe_name, tmp_file_name
 
-compile_and_run(Language, create_range_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True, QuexBufferSize=BS))
+def core(Executable, BufferSize, TestStr):
+    fh = open("test.txt", "wb")
+    fh.write(TestStr)
+    fh.close()
+    run_this("./%s test.txt %i" % (Executable, BufferSize))
 
-TestStr  = SEP + "hijklmnop" + SEP + "qrstuvw" + SEP + "xyz" + SEP
+exe_name, tmp_file = make("ANSI-C-from-file", map(ord, SEP), BS)
 
-compile_and_run(Language, create_range_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True, QuexBufferSize=BS))
+core(exe_name, BS, 
+     "abcdefg" + SEP + "hijklmnop" + SEP + "qrstuvw" + SEP + "xyz" + SEP + "ok")
 
-TestStr  = "a" + SEP + "h" + SEP + SEP + SEP
+core(exe_name, BS, 
+     SEP + "hijklmnop" + SEP + "qrstuvw" + SEP + "xyz" + SEP)
 
-compile_and_run(Language, create_range_skipper_code(Language, TestStr, end_sequence, CommentTestStrF=True, QuexBufferSize=BS))
+core(exe_name, BS, 
+     "a" + SEP + "h" + SEP + SEP + SEP)
+
+if DL != "DL=1":
+    Q = ""
+    for i in xrange(1, len(SEP)+1):
+        Q += "o" + SEP[:i]
+    BS = len(Q) + 2
+
+    core(exe_name, BS, 
+         "abcdefg" + Q + "hijklmnop" + Q + "qrstuvw" + Q + "xyz" + Q + "ab")
+    core(exe_name, BS, 
+         Q + "hijklmnop" + Q + "qrstuvw" + Q + "xyz" + Q)
+    core(exe_name, BS, 
+         "a" + Q + "h")
+
 
 
