e98fd7e20cc8956fe1c110cb74f85078653c853d
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2014-01-22 07:37:44 +0000

    Massive Refactoring --unstable

--- quex/engine/state_machine/setup_backward_input_position_detector.py
+++
@@ -1,9 +0,0 @@
-import quex.engine.state_machine.algorithm.beautifier as beautifier
-import quex.engine.state_machine.algebra.reverse      as reverse
-
-def do(BIPD_to_be_inverted):
-    # Only now, after the transformation we can safely get the inverse 
-    # state machine which is able to detect backwards.
-    # sm.mark_state_origins() # Origins tell about pattern that is served.
-    # We must communicate the transformed state machine back to the pattern
-    return sm
