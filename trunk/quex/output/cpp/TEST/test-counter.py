01b9b07f9b594ad753135850751ff858325a2153
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2014-04-08 07:06:19 +0000

    Massive Refactoring --unstable

--- trunk/quex/output/cpp/TEST/test-counter.py
+++ trunk/quex/output/cpp/TEST/test-counter.py
@@ -3,9 +3,14 @@
 # PURPOSE: ASCII - codec. Testing the basic character counting functionality.
 #
 # -- With and withour reference-pointer counting (column_n += (input_p - reference_p)
+#
 # -- Spaces, Lines, and Grids.
-# -- Basic: Dedicated Scenerios of Spaces, Grids, and Lines
-#    All:   Testing the reactions to all characters of the codec.       
+# 
+# -- *:               Dedicated Scenerios of Spaces, Grids, and Lines
+#    *-wo-ReferenceP: Testing the reactions to all characters of the codec.       
+#
+# -- All tests use exactly the same input, so that the 'SAME' label for hwut 
+#    tests can be applied.
 #______________________________________________________________________________
 import sys
 import os
@@ -32,7 +37,8 @@ if "--hwut-info" in sys.argv:
     # Information for HWUT
     #
     print "Character Counter: Default Implementation"
-    print "CHOICES: Basic, All;"  
+    print "CHOICES: ASCII, ASCII-wo-ReferenceP;"  
+    print "SAME;"
     sys.exit(0)
 
 test_list = [
@@ -71,12 +77,24 @@ def prepare_test_input_file(TestStr):
     fh.write(TestStr)
     fh.close()
 
-def get_test_application(counter_db):
+def get_test_application(counter_db, ReferenceP):
     # (*) Generate Code 
     counter_function_name, \
     counter_str            = counter.get(counter_db.get_factory(NumberSet_All(), Lng.INPUT_P()), 
                                          "TEST_MODE")
     counter_str = counter_str.replace("static void", "void")
+
+    # Make sure that the counter is implemented using reference pointer
+    found_f = False
+    for i, line in enumerate(counter_str.split("\n")):
+        if line.find("reference_p") != -1:
+            found_f = True
+            break
+
+    if ReferenceP:
+        assert found_f, "Counter has not been setup using a reference pointer."
+    else:
+        assert not found_f, "Counter has been setup using a reference pointer."
 
     open("./data/test.c", "wb").write("#include <data/check.h>\n\n" + counter_str)
 
@@ -103,10 +121,24 @@ def run():
         subprocess.call("./test")
 
 
-counter_db = None
-# Basic: With Reference Pointer Counting
-counter_db = CounterSetupLineColumn_Default()
+if "ASCII" in sys.argv:
+    counter_db  = None
+    # Basic: With Reference Pointer Counting
+    counter_db  = CounterSetupLineColumn_Default()
+    reference_p = True
+else:
+    # Reference counter is not possible, since we have two space types
+    spec_txt = """
+       [\\x0A] => newline 1;
+       [\\t]   => grid    4;
+       [0-9]   => space   10;
+       \else   => space   1;>
+    """
+    fh = StringIO(spec_txt)
+    fh.name = "<string>"
+    counter_db = counter_parser.parse_line_column_counter(fh)
+    reference_p = False
 
-get_test_application(counter_db)
+get_test_application(counter_db, reference_p)
 run()
     
