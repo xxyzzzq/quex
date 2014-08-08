b0612310ff6ddf7587fecd9a8047e3bd76bb6198
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2014-08-08 05:58:09 +0000

    Man Page

--- quex/TEST/verify_command_line_option_doc.py
+++ quex/TEST/verify_command_line_option_doc.py
@@ -1,11 +1,10 @@
-# PURPOSE:
+#! /usr/bin/env python
 #
-# This is a helper file to keep the current list of command line options 
-# up-to-date with the reported list of options in the documentation and 
-# man-page.
+# Check whether the documentation of the command line options in sphinx
+# and man page are up-to-date and consistent.
 #
-# (1) Collect all command line options recognized in current version
-#     of quex.
+# (C) Frank-Rene Schaefer
+#______________________________________________________________________________
 import os
 import sys
 sys.path.insert(0, os.environ["QUEX_PATH"])
@@ -13,6 +12,10 @@ import re
 
 from quex.input.setup import SETUP_INFO
 
+if "--hwut-info" in sys.argv:
+    print "Command Line Option Documentation;"
+    print "CHOICES: man, shinx;"
+    sys.exit()
 
 def get_option_db():
     result = {}
@@ -22,13 +25,20 @@ def get_option_db():
         option_list, default = value
         result.update((option, default) for option in option_list)
 
-    print "#odb:", sorted(result.keys())
     return result
 
 option_db = get_option_db()
 
+file_name = os.environ["QUEX_PATH"] + {
+    "sphinx": "/doc/source/invocation/command-line/intro.txt",
+    "man":    "/doc/man_page/quex.1",
+}[sys.argv[1]]
+marker = {
+    "sphinx": "cmdoption::",
+    "man":    ".SP"
+}[sys.argv[1]]
+
 # Verify that every option is documented.
-file_name        = os.environ["QUEX_PATH"] + "/doc/source/invocation/command-line/intro.txt"
 command_line_doc = open(file_name).read()
 for option in option_db:
     if command_line_doc.find(option) == -1:
@@ -37,7 +47,7 @@ for option in option_db:
 # Find things which are documented but do not exist
 option_re = re.compile(" \-[_a-zA-Z\-0-9]+", re.UNICODE)
 for line_i, line in enumerate(command_line_doc.split("\n")):
-    if line.find("cmdoption::") == -1: continue
+    if line.find(marker) == -1: continue
     for match in option_re.finditer(line):
         lexeme = match.group().strip()
         if lexeme in option_db: continue
