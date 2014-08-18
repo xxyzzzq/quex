f38f81354c80c5b1bf833c030e8e4a78d49547b9
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2014-08-18 05:52:32 +0000

    Unit Tests Transitions

--- trunk/quex/engine/generator/state/core.py
+++ trunk/quex/engine/generator/state/core.py
@@ -1,6 +1,6 @@
 from   quex.engine.analyzer.core                    import Analyzer
 from   quex.engine.analyzer.state.core              import AnalyzerState
-from   quex.engine.generator.state.transition.code  import relate_to_TransitionCode
+from   quex.engine.generator.state.transition_map.code  import relate_to_TransitionCode
 import quex.engine.generator.state.transition_map.core  as transition_block
 import quex.engine.generator.state.entry            as entry
 from   quex.engine.tools                            import none_isinstance, none_is_None
--- trunk/quex/engine/generator/state/transition_map/branch_table.py
+++ trunk/quex/engine/generator/state/transition_map/branch_table.py
@@ -1,41 +1,49 @@
 import quex.engine.generator.state.transition_map.transition as     transition
 from quex.blackboard import setup as Setup, \
                             Lng
-def do(txt, TriggerMap):
-    """Transitions of characters that lie close to each other can be very efficiently
-       be identified by a switch statement. For example:
+class BranchTable(object):
+    __slots__ = ("sub_map", "moat")
+    def __init__(self, SubMap, Moat):
+        Node.__init__(self, ImplementFunc)
+        self.sub_map = SubMap
+        self.moad    = Moat
 
-           switch( Value ) {
-           case 1: ..
-           case 2: ..
-           ...
-           case 100: ..
-           }
+    def implement(self):
+        """Transitions of characters that lie close to each other can be very efficiently
+           be identified by a switch statement. For example:
 
-       If SwitchFrameF == False, then no 'switch() { ... }' frame is produced.
+               switch( Value ) {
+               case 1: ..
+               case 2: ..
+               ...
+               case 100: ..
+               }
 
-       Is implemented by the very few lines in assembler (i386): 
+           If SwitchFrameF == False, then no 'switch() { ... }' frame is produced.
 
-           sall    $2, %eax
-           movl    .L13(%eax), %eax
-           jmp     *%eax
+           Is implemented by the very few lines in assembler (i386): 
 
-       where 'jmp *%eax' jumps immediately to the correct switch case.
-    
-       It is therefore of vital interest that those regions are **identified** and
-       **not split** by a bisection. To achieve this, such regions are made a 
-       transition for themselves based on the character range that they cover.
-    """
-    global Lng
+               sall    $2, %eax
+               movl    .L13(%eax), %eax
+               jmp     *%eax
 
-    case_code_list = []
-    for interval, target in TriggerMap:
-        target_code = []
-        transition.do(target_code, (interval, target))
-        case_code_list.append((range(interval.begin, interval.end), target_code))
+           where 'jmp *%eax' jumps immediately to the correct switch case.
+        
+           It is therefore of vital interest that those regions are **identified** and
+           **not split** by a bisection. To achieve this, such regions are made a 
+           transition for themselves based on the character range that they cover.
+        """
+        global Lng
 
-    txt.extend(Lng.SELECTION("input", case_code_list))
-    txt.append("\n")
-    return True
+        case_code_list = []
+        for interval, target in self.sub_map:
+            if target == self.moat: continue
+            target_code = transition.do(interval, target)
+            case_code_list.append((interval, target_code))
+
+        txt.extend(Lng.SELECTION("input", case_code_list,
+                                 DefaultConsequence=self.moat))
+        txt.append("\n")
+        return True
 
 
