1c59f58010cdc0b0742546e837896920f05c7da7
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2014-12-13 20:18:20 +0000

    New Analysis Approach

---
+++ quex/engine/analyzer/examine/TEST/test-accumulate.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+import os
+import sys
+sys.path.insert(0, os.environ["QUEX_PATH"])
+
+from quex.engine.state_machine.core                             import StateMachine
+from quex.engine.state_machine.TEST.helper_state_machine_shapes import *
+from quex.engine.analyzer.examine.TEST.helper                   import *
+from quex.engine.analyzer.examine.acceptance                    import RecipeAcceptance
+from quex.engine.analyzer.examine.core                          import Examiner, \
+                                                                       LinearStateWalker
+
+if "--hwut-info" in sys.argv:
+    print "Accumulation;"
+    print "CHOICES: linear, butterfly, long_loop, nested_loop, mini_loop, fork, fork2, fork3, fork4;"
+    sys.exit()
+
+sm, state_n, pic = get_sm_shape_by_name(sys.argv[1])
+
+print pic
+
+sm.get_init_state().single_entry.add(get_SeAccept(4711L, 33L))
+
+examiner        = Examiner(sm, RecipeAcceptance)
+examiner.categorize()
+springs         = examiner.setup_initial_springs()
+mouth_ready_set = examiner._accumulate(springs)
+
+def print_recipe(si, R):
+    if R is None: 
+        print "  %02i <void>" % si
+    else:
+        print "  %02i %s" % (si, str(R).replace("\n", "\n     "))
+
+print "Mouths ready for interference:"
+print "   %s" % sorted(list(mouth_ready_set))
+print
+print "Linear States:"
+for si, info in examiner.linear_db.iteritems():
+    print_recipe(si, info.recipe)
+
+print "Mouth States:"
+for si, info in examiner.mouth_db.iteritems():
+    print_recipe(si, info.recipe)
+
---
+++ quex/engine/analyzer/examine/TEST/test-interference.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+import os
+import sys
+sys.path.insert(0, os.environ["QUEX_PATH"])
+
+from quex.engine.state_machine.core                             import StateMachine
+from quex.engine.operations.se_operations                       import SeAccept
+from quex.engine.state_machine.TEST.helper_state_machine_shapes import *
+from quex.engine.analyzer.examine.TEST.helper                   import *
+from quex.engine.analyzer.examine.state_info                    import *
+from quex.engine.analyzer.examine.acceptance                    import RecipeAcceptance
+from quex.engine.analyzer.examine.core                          import Examiner, \
+                                                                       LinearStateWalker
+from quex.blackboard import E_PreContextIDs
+from copy import deepcopy
+
+if "--hwut-info" in sys.argv:
+    print "Interference: Homogeneous Acceptance;"
+    print "CHOICES: 2-entries, 3-entries;"
+    print "SAME;"
+    sys.exit()
+
+choice  = sys.argv[1].split("-")
+entry_n = int(choice[0])
+
+def print_recipe(si, R, UndeterminedSet):
+    if R is None: 
+        print "  %02i <void> (ERROR!)" % si
+    else:
+        if UndeterminedSet is None:
+            undetermined_str = "ALL(ERROR!)"
+        else:
+            undetermined_str = str(sorted(list(UndeterminedSet)))
+
+        print "  %02i %s    Undetermined: %s" % (si, str(R).replace("\n", "\n     "), 
+                                               undetermined_str)
+
+def get_array(EntryN, AcceptanceScheme):
+    return [ 
+        RecipeAcceptance(deepcopy(AcceptanceScheme), {}) 
+        for i in xrange(EntryN) 
+    ]
+
+def get_MouthStateInfo(EntryN, AcceptanceScheme):
+    info  = MouthStateInfo(FromStateIndexSet=set(xrange(entry_n)))
+    array = get_array(entry_n, AcceptanceScheme)
+    for i, recipe in enumerate(array):
+        info.entry_recipe_db[i] = recipe
+    return info
+
+scheme_restore  = None
+scheme_simple   = [ get_SeAccept(1111L, E_PreContextIDs.NONE, False) ]
+scheme_simple2  = [ get_SeAccept(2222L, 22L,                  True) ]
+scheme_list     = [ 
+    get_SeAccept(3333L, 33L, True), 
+    get_SeAccept(4444L, 44L, True), 
+    get_SeAccept(5555L, E_PreContextIDs.NONE, True) 
+]
+
+examiner = Examiner(StateMachine(), RecipeAcceptance)
+# For the test, only 'examiner.mouth_db' and 'examiner.recipe_type'
+# are important.
+examiner.mouth_db[1L] = get_MouthStateInfo(entry_n, scheme_restore)
+examiner.mouth_db[2L] = get_MouthStateInfo(entry_n, scheme_simple)
+examiner.mouth_db[3L] = get_MouthStateInfo(entry_n, scheme_simple2)
+examiner.mouth_db[4L] = get_MouthStateInfo(entry_n, scheme_list)
+
+examiner._interfere(set([1L, 2L, 3L, 4L]))
+
+
+print "Mouth States:"
+for si, info in examiner.mouth_db.iteritems():
+    print_recipe(si, info.recipe, info.undetermined_register_set)
+    print
+
