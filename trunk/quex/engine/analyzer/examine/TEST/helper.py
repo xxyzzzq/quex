024a0600e85c18ec273b226393ba7f71bc01ea85
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2014-12-14 09:37:45 +0000

    New Analysis Approach

--- trunk/quex/engine/analyzer/examine/TEST/helper.py
+++ trunk/quex/engine/analyzer/examine/TEST/helper.py
@@ -1,4 +1,5 @@
-from quex.engine.operations.se_operations                       import SeAccept
+from quex.engine.operations.se_operations import SeAccept, SeStoreInputPosition
+from quex.blackboard                      import E_PreContextIDs
 
 def get_SeAccept(AcceptanceId, PreContextId, RestorePositionF=False):
     cmd = SeAccept()
@@ -7,3 +8,14 @@ def get_SeAccept(AcceptanceId, PreContextId, RestorePositionF=False):
     if RestorePositionF:
         cmd.set_restore_position_register_f()
     return cmd
+
+def add_SeAccept(sm, StateIndex, AcceptanceId, PreContextId=E_PreContextIDs.NONE, RestorePositionF=False):
+    sm.states[StateIndex].single_entry.add(get_SeAccept(AcceptanceId, PreContextId, RestorePositionF))
+    accept_str  = "%s" % AcceptanceId
+    pre_str     = "%s/" % PreContextId if PreContextId != E_PreContextIDs.NONE else ""
+    restore_str = "R" if RestorePositionF else ""
+    print "(%i) Accept %s%s%s" % (StateIndex, pre_str, accept_str, restore_str)
+
+def add_SeStoreInputPosition(sm, StateIndex, RegisterId):
+    sm.states[StateIndex].single_entry.add(SeStoreInputPosition(RegisterId))
+    print "(%i) Store InputP %s" % (StateIndex, RegisterId)
