34be0be03ac9b5ee2aa05a3bbb1b1e11fa1ceaf1
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2015-02-20 07:46:19 +0000

    New Analysis Approach

--- quex/engine/analyzer/examine/TEST/test-prepare_cautious_recipes.py
+++ quex/engine/analyzer/examine/TEST/test-prepare_cautious_recipes.py
@@ -1,4 +1,30 @@
 #! /usr/bin/env python
+#
+# PURPOSE: Test the production of a cautious recipe for acceptance considerations.
+#
+# Details on 'cautious recipes' see [DOC]. Brief: A cautious recipe is a recipe
+# for a mouth state, that actually has undetermined entries. Thus, ordinary
+# interference cannot be applied. Cautious recipes take the place of
+# undetermined recipes. Since, then all entry recipes are defined, ordinary
+# interference produces a correct recipe that assumes that values are stored at
+# state entry.
+#
+# Components of the mouth state's 'op(i)' which are constant do not trigger a 
+# 'store/restore' behavior. The component can be overtaken to the output recipe.
+#
+# Core Test: 
+#
+#    -- Setup mouth state with undetermined entries in '.entry_recipe_db'.
+#
+#    -- Setup 'op(i)' for the mouth state.
+#
+#    -- ._prepare_cautious_recipe()
+#         => .entry_recipe_db[p] = cautious recipe
+#            for all predecessor state indices 'p'.
+#
+#    -- ordinary interference
+#         => A correct output recipe.
+#______________________________________________________________________________
 import os
 import sys
 sys.path.insert(0, os.environ["QUEX_PATH"])
@@ -19,7 +45,7 @@ if "--hwut-info" in sys.argv:
 
 # choice = sys.argv[1]
 
-def setup_mouth(examiner, StateIndex, EntryN, SomeRecipe0, SomeRecipe1):
+def setup_mouth_with_undetermined_entries(examiner, EntryN, SomeRecipe0, SomeRecipe1):
     EntryN = 4
     mouth  = MouthStateInfo(FromStateIndexSet=set(xrange(EntryN)))
     if EntryN > 0: mouth.entry_recipe_db[0] = None
@@ -28,29 +54,44 @@ def setup_mouth(examiner, StateIndex, EntryN, SomeRecipe0, SomeRecipe1):
     if EntryN > 3: mouth.entry_recipe_db[3] = SomeRecipe1
     if EntryN > 4: mouth.entry_recipe_db[4] = None
 
-    examiner.mouth_db[StateIndex] = mouth
+    return mouth
 
-def setup_state(sm, *OperationList):
-    state_index = long(len(sm.states))
-    sm.states[state_index] = State(SingleEntry.from_iterable(OperationList))
+def setup_state_operation(sm, CmdList):
+    state_index = max(set(sm.states.iterkeys())) + 1
+    state       = State()
+    for cmd in CmdList:
+        state.single_entry.add(cmd)
+    sm.states[state_index] = state
     return state_index
 
-def setup(examiner, sm, EntryN):
-    si       = setup_state(sm) #, SeAccept(4711L)) 
+def setup(EntryN, StateOperation):
+    sm        = StateMachine()
+    examiner  = Examiner(sm, RecipeAcceptance)
+
+    si        = setup_state_operation(sm, StateOperation) 
+    operation = sm.states[si].single_entry
+
     examiner.linear_db[sm.init_state_index] = LinearStateInfo()
 
-    recipe0  = RecipeAcceptance([SeAccept(0)], {si: 0}, {})
-    recipe1  = RecipeAcceptance([SeAccept(1)], {si: 0}, {})
+    predecessor0_recipe  = RecipeAcceptance([SeAccept(0)], {si: 0}, {})
+    recipe0              = examiner.recipe_type.accumulation(predecessor0_recipe, operation)
+    predecessor1_recipe  = RecipeAcceptance([SeAccept(1)], {si: 0}, {})
+    recipe1              = examiner.recipe_type.accumulation(predecessor1_recipe, operation)
 
-    setup_mouth(examiner, si, EntryN, recipe0, recipe1)
+    mouth    = setup_mouth_with_undetermined_entries(examiner, EntryN, recipe0, recipe1)
+    examiner.mouth_db[si] = mouth
 
-sm       = StateMachine()
-examiner = Examiner(sm, RecipeAcceptance)
+    examiner.determine_required_sets_of_variables()
+    assert set(examiner.mouth_db).isdisjoint(examiner.linear_db), \
+           "%s intersects with %s" % (set(examiner.mouth_db), set(examiner.linear_db))
+    return examiner, si
 
-setup(examiner, sm, 5)
+def test(StateOperation):
+    examiner, si = setup(5, StateOperation)
+    examiner._prepare_cautious_recipe(si)
+    examiner._interference([si])
+    print_interference_result(examiner.mouth_db)
 
-examiner.determine_required_sets_of_variables()
-
-examiner._prepare_cautious_recipe(1L)
-examiner._interference([1L])
-print_interference_result(examiner.mouth_db)
+# test([])
+# test([SeAccept(4711L)])
+test([SeStoreInputPosition(4711L)])
