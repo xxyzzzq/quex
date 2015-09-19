52f956a4b533a12683879c699c7c33ec534c7cc2
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2015-09-19 08:42:01 +0000

    NEW -- byte loader (Fails X/1509)

--- quex/code_base/buffer/plain/TEST/Buffer_tell_and_seek-wiostream.cpp
+++ quex/code_base/buffer/plain/TEST/Buffer_tell_and_seek-wiostream.cpp
@@ -1,5 +1,5 @@
 #define QUEX_SETTING_BUFFER_MIN_FALLBACK_N 0
-#include <quex/code_base/buffer/TEST/Buffer_test_common.i>
+#include <quex/code_base/buffer/TESTS/Buffer_test_common.i>
 #include "test-helper.h"
 
 #include <sstream>
