d5174596c52722833d38ad6c61d8dcb9c69a2735
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2015-10-04 13:13:00 +0000

    NEW -- byte loader (Fails X/1509)

---
+++ quex/code_base/buffer/loader/TEST/basic_functionality.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_GUARD_QUEX_BASIC_FUNCTIONALITY_H
+#define INCLUDE_GUARD_QUEX_BASIC_FUNCTIONALITY_H
+
+extern void verify_basic_functionality(ByteLoader* me);
+
+#endif  /* INCLUDE_GUARD_QUEX_BASIC_FUNCTIONALITY_H */
+
