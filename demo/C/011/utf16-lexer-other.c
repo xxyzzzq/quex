3b641104e1f9cdce1436cc855c8c530408e5dd37
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2015-10-31 07:05:06 +0000

    Work towards next release (Fails 239/1530)

--- demo/C/011/utf16-lexer-other.c
+++ demo/C/011/utf16-lexer-other.c
@@ -6,22 +6,24 @@
 int 
 main(int argc, char** argv) 
 {        
-    quex_Token*      token_p    = 0x0;
-    bool             BigEndianF = (argc < 2 || (strcmp(argv[1], "BE") == 0)); 
-    const char*      file_name  = BigEndianF ? "example-other-utf16be.txt" : "example-other-utf16le.txt";
-    quex_UTF16Lex    qlex;
+    quex_Token*                 token_p     = 0x0;
+    bool                        BigEndianF  = (argc < 2 || (strcmp(argv[1], "BE") == 0)); 
+    const char*                 file_name   = BigEndianF ? "example-other-utf16be.txt" : "example-other-utf16le.txt";
+    ByteLoader*                 byte_loader = ByteLoader_FILE_new_from_file_name(file_name);
+    QUEX_NAME(BufferFiller)*    filler      = QUEX_NAME(BufferFiller_new)(byte_loader, 0, 0);
+    quex_UTF16Lex               qlex;
     const QUEX_TYPE_CHARACTER*  iterator = 0x0;
 
     if( argc == 1 ) {
         printf("Required at least one argument: 'LE' or 'BE'.\n");
         return -1;
     }
-
    
-    /* NOTE: On a big endian machine (e.g. PowerPC) the byte reversion flag
-     *       might be set to 'not BigEndianF'                                */
-
-    QUEX_NAME(from_file_name)(&qlex, file_name, 0x0, BigEndianF);
+    /* System's endianness is 'little' => reversion if 'big'
+     *                     is 'big'    => reversion if 'little' (not 'big'). */
+    filler->_byte_order_reversion_active_f = QUEXED(system_is_little_endian)() ? 
+                                             BigEndianF : ! BigEndianF;
+    QUEX_NAME(from_BufferFiller)(&qlex, filler);
 
     printf("## input file           = %s\n", file_name);
     printf("## byte order reversion = %s\n", QUEX_NAME(byte_order_reversion)(&qlex) ? "true" : "false");
@@ -39,6 +41,8 @@ main(int argc, char** argv)
     } while( token_p->_id != TKN_TERMINATION );
 
     QUEX_NAME(destruct)(&qlex);
+    filler->delete_self(filler);
+    byte_loader->delete_self(byte_loader);
 
     return 0;
 }
--- demo/Cpp/011/utf16-lexer.cpp
+++ demo/Cpp/011/utf16-lexer.cpp
@@ -15,19 +15,24 @@ main(int argc, char** argv)
        return -1;
     }
 
-    Token*       token;
-    bool         BigEndianF = (strcmp(argv[1], "BE") == 0); 
-    const char*  file_name = BigEndianF ? "example-utf16be.txt" : "example-utf16le.txt";
-   
-    /* NOTE: On a big endian machine (e.g. PowerPC) the byte reversion flag
-     *       might be set to 'not BigEndianF'                                */
-    UTF16Lex     qlex(file_name, 0x0, /* Byte Order Reversion */ BigEndianF);
+    Token*                   token;
+    bool                     BigEndianF  = (strcmp(argv[1], "BE") == 0); 
+    const char*              file_name   = BigEndianF ? "example-utf16be.txt" : "example-utf16le.txt";
+    ByteLoader*              byte_loader = ByteLoader_FILE_new_from_file_name(file_name);
+    QUEX_NAME(BufferFiller)* filler      = QUEX_NAME(BufferFiller_new)(byte_loader, 0, 0);
+    UTF16Lex*                qlex;
+
+    /* System's endianness is 'little' => reversion if 'big'
+     *                     is 'big'    => reversion if 'little' (not 'big'). */
+    filler->_byte_order_reversion_active_f = QUEXED(system_is_little_endian)() ? 
+                                             BigEndianF : ! BigEndianF;
+    qlex = new UTF16Lex(filler);
 
     printf("## input file           = %s\n", file_name);
-    printf("## byte order reversion = %s\n", qlex.byte_order_reversion() ? "true" : "false");
+    printf("## byte order reversion = %s\n", qlex->byte_order_reversion() ? "true" : "false");
     
     do {
-        qlex.receive(&token);
+        qlex->receive(&token);
 
         /* Print the lexeme in utf8 format. */
         printf("%s\n", (char*)(string(*token).c_str()));
@@ -35,5 +40,8 @@ main(int argc, char** argv)
         // (*) check against 'termination'
     } while( token->type_id() != TKN_TERMINATION );
 
+    delete qlex;
+    filler->delete_self(filler);
+    byte_loader->delete_self(byte_loader);
     return 0;
 }
