6086a20b8ad70a4337f4fb52f0f9659f95f6e8ec
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2015-11-12 23:46:52 +0000

    Demo 010; Manual Buffer Filling Example

--- trunk/demo/C/010/messaging_framework.h
+++ trunk/demo/C/010/messaging_framework.h
@@ -1,18 +1,17 @@
 #ifndef __INCLUDE_GUARD__MESSAGING_FRAMEWORK__
 #define __INCLUDE_GUARD__MESSAGING_FRAMEWORK__
 
-
-#if ! defined( __QUEX_OPTION_MESSAGE_UTF8)
-#   include "tiny_lexer.h"
-#   define ELEMENT_TYPE QUEX_TYPE_CHARACTER
-#else
-#   include "tiny_lexer_utf8.h"
+#ifdef QUEX_EXAMPLE_WITH_CONVERTER 
 #   define ELEMENT_TYPE uint8_t
+#   include "tiny_lexer_utf8.h"
+#else
+#   define ELEMENT_TYPE QUEX_TYPE_CHARACTER
+#   include "tiny_lexer.h"
 #endif
 
 typedef struct {
-    QUEX_TYPE_CHARACTER* begin_p;
-    QUEX_TYPE_CHARACTER* end_p;
+    ELEMENT_TYPE* begin_p;
+    ELEMENT_TYPE* end_p;
 } MemoryChunk;
 
 /* Assume that some low level driver communicates the place where 
@@ -20,13 +19,12 @@ typedef struct {
 #define  MESSAGING_FRAMEWORK_BUFFER_SIZE  ((size_t)(512))
 extern ELEMENT_TYPE   MESSAGING_FRAMEWORK_BUFFER[MESSAGING_FRAMEWORK_BUFFER_SIZE];
 
-extern size_t messaging_framework_receive(ELEMENT_TYPE** buffer);
-extern size_t messaging_framework_receive_whole_characters(ELEMENT_TYPE** rx_buffer);
-extern size_t messaging_framework_receive_syntax_chunk(ELEMENT_TYPE** buffer);
-extern size_t messaging_framework_receive_into_buffer(ELEMENT_TYPE*, size_t);
-extern size_t messaging_framework_receive_into_buffer_syntax_chunk(ELEMENT_TYPE* BufferBegin, 
-                                                                   size_t BufferSize);
-extern size_t messaging_framework_receive_to_internal_buffer();
-extern void   messaging_framework_release(ELEMENT_TYPE*);
+extern size_t receiver_fill(ELEMENT_TYPE** buffer);
+extern size_t receiver_fill_syntax_chunk(ELEMENT_TYPE** buffer);
+extern size_t receiver_fill_whole_characters(ELEMENT_TYPE** rx_buffer);
+extern size_t receiver_fill_to_internal_buffer();
+extern size_t receiver_copy(ELEMENT_TYPE*, size_t);
+extern size_t receiver_copy_syntax_chunk(ELEMENT_TYPE* BufferBegin, 
+                                         size_t        BufferSize);
 
 #endif /*_INCLUDE_GUARD__MESSAGING_FRAMEWORK_*/
