586ffa209587574fdb705f24a209e8e1c8e6bb63
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2015-11-19 21:20:18 +0000

    Towards Release (64/1484 fails)

--- trunk/quex/output/core/skipper/TEST/test-skip.py
+++ trunk/quex/output/core/skipper/TEST/test-skip.py
@@ -31,7 +31,7 @@ TestStr  = "abcdefg_HIJKLMNOP-qrstuvw'XYZ12ok3"
 compile_and_run(Language, 
                 create_character_set_skipper_code(Language, TestStr, trigger_set),
                 StrangeStream_str=StrangeStream_str)
-x = 1 / 0
+# x = 1 / 0
 
 TestStr  = "-hijklmnop_qrstuvw#xyz9"
 
